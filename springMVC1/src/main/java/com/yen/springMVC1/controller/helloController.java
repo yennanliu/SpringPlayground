d1e5ec7cd394cf6a9bae7578b7b09e77a0364ecb
yennanliu <f339339@gmail.com>
2022-01-31 15:16:40 +0800

    update

--- springMVC1/src/main/java/com/yen/springMVC1/controller/helloController.java
+++ springMVC1/src/main/java/com/yen/springMVC1/controller/helloController.java
@@ -4,7 +4,16 @@ package com.yen.springMVC1.controller;
 // https://www.youtube.com/watch?v=f-mn8mC6nTE&list=PLmOn9nNkQxJE3V_Eev79ao-g3a6BplSQG&index=10
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller // label as controller class, so springMVC can scan and use it
 public class helloController {
+
+    // request : "/"  -> /WEB-INF/templates/index.html
+    @RequestMapping(value="/") // will map request with path = "/" to below method
+    public String index(){
+        //return view name (/WEB-INF/templates/index.html remove prefix, suffix, so only need to return "index"
+        return "index";
+    }
+
 }
