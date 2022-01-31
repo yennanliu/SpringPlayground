3e0a66852743edb3abc84f341831298b71be82b1
yennanliu <f339339@gmail.com>
2022-01-31 10:52:24 +0800

    add springMVC1/, update gitignore

---
+++ springMVC1/src/main/java/com/yen/springMVC1/SpringMvc1Application.java
@@ -0,0 +1,13 @@
+package com.yen.springMVC1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvc1Application.class, args);
+	}
+
+}
---
+++ springMVC1/src/test/java/com/yen/springMVC1/SpringMvc1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.springMVC1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
