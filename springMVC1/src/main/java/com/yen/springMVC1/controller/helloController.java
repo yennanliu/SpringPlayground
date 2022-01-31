1fba6fee27a6943991609c36adcf04a0f6ff7583
yennanliu <f339339@gmail.com>
2022-01-31 14:58:48 +0800

    fix /webapp/WEB-INF/web.xml, /resources/springMVC.xml, add springMVC1/controller/

---
+++ springMVC1/src/main/java/com/yen/springMVC1/controller/helloController.java
@@ -0,0 +1,10 @@
+package com.yen.springMVC1.controller;
+
+// https://www.youtube.com/watch?v=S3So1anM_6w&list=PLmOn9nNkQxJE3V_Eev79ao-g3a6BplSQG&index=10
+// https://www.youtube.com/watch?v=f-mn8mC6nTE&list=PLmOn9nNkQxJE3V_Eev79ao-g3a6BplSQG&index=10
+
+import org.springframework.stereotype.Controller;
+
+@Controller // label as controller class, so springMVC can scan and use it
+public class helloController {
+}
