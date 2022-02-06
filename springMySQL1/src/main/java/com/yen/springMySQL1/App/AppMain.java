84564ba33d3bf5a78f5a1885fd15b78e0e996ab6
yennanliu <f339339@gmail.com>
2022-02-06 23:18:03 +0800

    add code, doc, clean .idea

---
+++ springMySQL1/src/main/java/com/yen/springMySQL1/App/AppMain.java
@@ -0,0 +1,11 @@
+package com.yen.springMySQL1.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMain {
+    public static void main(String[] args) {
+        SpringApplication.run(AppMain.class, args);
+    }
+}
---
+++ springMySQL1/src/main/java/com/yen/springMySQL1/Controller/AppController.java
@@ -0,0 +1,27 @@
+package com.yen.springMySQL1.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.yen.springMySQL1.model.Product;
+import com.yen.springMySQL1.Service.ProductService;
+
+import java.util.List;
+
+@Controller
+public class AppController {
+
+    @Autowired
+    private ProductService service;
+
+    // method
+    @RequestMapping("/")
+    public String viewHomePage(Model model){
+        List<Product> listProducts = service.listAll();
+        model.addAttribute("listProducts", listProducts);
+
+        return "index";
+    }
+}
