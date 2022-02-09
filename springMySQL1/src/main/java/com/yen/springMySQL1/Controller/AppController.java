867323b91b598be16ea134c8616511aae96bff55
yennanliu <f339339@gmail.com>
2022-02-09 23:05:49 +0800

    update controller, progress

--- springMySQL1/src/main/java/com/yen/springMySQL1/Controller/AppController.java
+++ springMySQL1/src/main/java/com/yen/springMySQL1/Controller/AppController.java
@@ -3,10 +3,12 @@ package com.yen.springMySQL1.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.yen.springMySQL1.model.Product;
 import com.yen.springMySQL1.Service.ProductService;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -31,5 +33,12 @@ public class AppController {
         model.addAttribute("product", product);
 
         return "new_product";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveProduct(@ModelAttribute("product") Product product) {
+        service.save(product);
+
+        return "redirect:/";
     }
 }
