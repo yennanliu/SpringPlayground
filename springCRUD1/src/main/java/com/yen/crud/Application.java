f574b74648bc01cc700554062cdb50f6663d54bc
yennanliu <f339339@gmail.com>
2022-02-16 23:13:28 +0800

    update

---
+++ springCRUD1/src/main/java/com/yen/crud/Application.java
@@ -0,0 +1,15 @@
+package com.yen.crud;
+
+// https://www.baeldung.com/spring-boot-crud-thymeleaf
+// https://github.com/eugenp/tutorials/blob/master/spring-boot-modules/spring-boot-crud/src/main/java/com/baeldung/crud/Application.java
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
