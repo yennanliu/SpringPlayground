e9aa666a1592e12930e257e8108bf4748c34cf7f
yennanliu <f339339@gmail.com>
2022-04-16 09:46:53 +0800

    update

--- springBootPOC1/src/main/java/com/yen/SpringBootPOC1/MainApplication.java
+++ springBootPOC1/src/main/java/com/yen/SpringBootPOC1/MainApplication.java
@@ -4,6 +4,9 @@ package com.yen.SpringBootPOC1;
 // https://www.youtube.com/watch?v=dJIksiVQDj4&list=PLmOn9nNkQxJFKh2PMfWbGT7RVuMowsx-u&index=8
 // https://www.youtube.com/watch?v=GGWMK2BJs7E&list=PLmOn9nNkQxJFKh2PMfWbGT7RVuMowsx-u&index=9
 
+import com.yen.SpringBootPOC1.bean.Pet;
+import com.yen.SpringBootPOC1.bean.User;
+import com.yen.SpringBootPOC1.config.MyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -29,7 +32,23 @@ public class MainApplication {
         }
 
         // 3. get component from container
+        Pet tom01 = run.getBean("tom", Pet.class);
+        Pet tom02  = run.getBean("tom", Pet.class);
 
+        System.out.println("component tom01 == tom02 ? : " + (tom01 == tom02) );
+
+        // 4. com.yen.SpringBootPOC1.config.MyConfig$$EnhancerBySpringCGLIB$$3f2ef3de@28cb3a25
+        MyConfig bean = run.getBean(MyConfig.class);
+        System.out.println(bean);
+
+        // 5. if @Configuration(proxyBeanMethods=true) in Myconfig
+        //    -> will use "proxy" call method
+        //    (spring boot will always check above)
+        //    -> keep component singleton
+        User user = bean.user01();
+        User user1 = bean.user01();
+
+        System.out.println("component user == user1 ? : " + (user == user1) );
 
         //SpringApplication.run(MainApplication.class, args);
     }
--- springBootPOC1/src/main/java/com/yen/SpringBootPOC1/config/MyConfig.java
+++ springBootPOC1/src/main/java/com/yen/SpringBootPOC1/config/MyConfig.java
@@ -7,14 +7,28 @@ import com.yen.SpringBootPOC1.bean.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/** tell spring boot, this is a config class. same as config file */
-@Configuration
+/**
+ *  tell spring boot, this is a config class. same as config file
+ *
+ *  1) we can use @Bean
+ *  2) component instance is singleton (單例) by default
+ *  3) conf class (MyConfig) is also a component
+ *  4) we have
+ *          Full (proxyBeanMethods=true)
+ *          Lite (proxyBeanMethods=false)
+ *      mode in config setting
+ *      -> for dealing with component dep.
+ */
+
+@Configuration(proxyBeanMethods=true)  // proxyBeanMethods default = true
 public class MyConfig {
 
     /**
      *  1) add component to container, use method name as component id
      *  2) returned type is component type
      *  3) returned value is component instance in container
+     *  4) no matter how many times we grab component instance in app,
+     *      -> it ALWAYS uses the same registered one (instance) (e.g.  singleton (單例))
      */
     @Bean
     public User user01(){
