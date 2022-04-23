b620921d2bc4577ed54e20fc5590076aca984a73
yennanliu <f339339@gmail.com>
2022-04-23 09:50:41 +0800

    add application.yml, add/update bean

--- springBootPOC1/src/main/java/com/yen/SpringBootPart1/bean/Pet.java
+++ springBootPOC1/src/main/java/com/yen/SpringBootPart1/bean/Pet.java
@@ -1,10 +1,17 @@
 package com.yen.SpringBootPart1.bean;
 
 // https://www.youtube.com/watch?v=GGWMK2BJs7E&list=PLmOn9nNkQxJFKh2PMfWbGT7RVuMowsx-u&index=9
+// https://www.youtube.com/watch?v=s5GTuFsCSWw&list=PLmOn9nNkQxJFKh2PMfWbGT7RVuMowsx-u&index=21
 
+import lombok.Data;
+import lombok.ToString;
+
+@ToString
+@Data
 public class Pet {
 
     private String name;
+    private Double weight;
 
     public Pet(){
 
