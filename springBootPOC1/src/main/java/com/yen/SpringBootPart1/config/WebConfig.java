79023eff3c97af13cc4d34ca39ff4318108cf38e
yennanliu <f339339@gmail.com>
2022-04-27 08:39:55 +0800

    add webConfig

---
+++ springBootPOC1/src/main/java/com/yen/SpringBootPart1/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.yen.SpringBootPart1.config;
+
+// https://www.youtube.com/watch?v=QpZEkzjit7o&list=PLmOn9nNkQxJFKh2PMfWbGT7RVuMowsx-u&index=27
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+
+@Configuration(proxyBeanMethods = false)
+public class WebConfig {
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+        HiddenHttpMethodFilter methodFilter = new HiddenHttpMethodFilter();
+        //methodFilter.setMethodParam("_m"); // NOTE !! here we'll override default "_method" to "_m" (in filer). please check index.html
+        //methodFilter.setMethodParam("_method");
+        return methodFilter;
+    }
+}
