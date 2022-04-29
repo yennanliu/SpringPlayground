ec6bb70d70885c9afdeca95cb55afbf299ebbbde
yennanliu <f339339@gmail.com>
2022-04-29 10:27:17 +0800

    update paramController

--- springBootPOC1/src/main/java/com/yen/SpringBootPart1/controller/ParamController.java
+++ springBootPOC1/src/main/java/com/yen/SpringBootPart1/controller/ParamController.java
@@ -4,6 +4,7 @@ package com.yen.SpringBootPart1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -13,11 +14,14 @@ import java.util.Map;
 public class ParamController {
 
     //@GetMapping("/car?id=1")  // this one is OK as well
-    @GetMapping("/car/{id}/owner/{userName}")    // REST style
+    @GetMapping("/car/{id}/owner/{userName}")                       // REST style
     public Map<String, Object> getCar(
-                    @PathVariable("id") Integer id,  // get id from path variable
-                    @PathVariable("userName") String userName// get userName from path variable
-                    //@PathVariable("pv") Map<String, String> pv // get ALL path variables and put into pv
+                    @PathVariable("id") Integer id,                 // get id from path variable
+                    @PathVariable("userName") String userName,      // get userName from path variable
+                    // Fix this
+                    //@PathVariable("pv") Map<String, String> pv    // get ALL path variables and put into pv
+                    @RequestHeader("User-Agent") String userAgent,  // get "User-Agent" info from header
+                    @RequestHeader Map<String, String> header       // get all info from header
             ){
 
         Map<String, Object> map = new HashMap<>();
@@ -25,6 +29,8 @@ public class ParamController {
         map.put("id", id);
         map.put("userName", userName);
         //map.put("pv", pv);
+        map.put("userAgent", userAgent);
+        map.put("header", header);
 
         return map;
     }
