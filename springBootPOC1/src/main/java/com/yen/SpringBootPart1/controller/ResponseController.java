95efbc44a27d421e3b97ad0dfc0556bc4e729a43
yennanliu <f339339@gmail.com>
2022-05-07 15:39:51 +0800

    add ResponseController.java

---
+++ springBootPOC1/src/main/java/com/yen/SpringBootPart1/controller/ResponseController.java
@@ -0,0 +1,24 @@
+package com.yen.SpringBootPart1.controller;
+
+// https://www.youtube.com/watch?v=s9NuhNSD4Mo&list=PLmOn9nNkQxJFKh2PMfWbGT7RVuMowsx-u&index=38
+
+import com.yen.SpringBootPart1.bean.Person2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+@Controller
+public class ResponseController {
+
+    @ResponseBody
+    @GetMapping("/test/person2")
+    public Person2 getPerson2(){
+        Person2 person = new Person2();
+        person.setAge(20);
+        person.setBirth(new Date());
+        person.setUserName("Abby");
+        return person;
+    }
+}
