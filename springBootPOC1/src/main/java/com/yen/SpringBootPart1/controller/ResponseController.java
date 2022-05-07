04e7f4a2dd6f4a5e18171028db8dd36e39c992a9
yennanliu <f339339@gmail.com>
2022-05-07 16:59:10 +0800

    update /ResponseController.java

--- springBootPOC1/src/main/java/com/yen/SpringBootPart1/controller/ResponseController.java
+++ springBootPOC1/src/main/java/com/yen/SpringBootPart1/controller/ResponseController.java
@@ -3,6 +3,7 @@ package com.yen.SpringBootPart1.controller;
 // https://www.youtube.com/watch?v=s9NuhNSD4Mo&list=PLmOn9nNkQxJFKh2PMfWbGT7RVuMowsx-u&index=38
 
 import com.yen.SpringBootPart1.bean.Person2;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,4 +22,13 @@ public class ResponseController {
         person.setUserName("Abby");
         return person;
     }
+
+    @ResponseBody // RequestResponseBodyMethodProducer -> messageConverter
+    @GetMapping("/test/file_resource")
+    public FileSystemResource file(){
+        // should return "doc" format (processed by messageConverter), instead of json format
+        // TODO : implement this
+        return null;
+    }
+
 }
