1c786e23bfa965406d7b64457235b1c3d77684dc
yennanliu <f339339@gmail.com>
2022-05-10 18:26:16 +0800

    add attr via Resource, add new endpoint (helloController

--- springBootPOC3/src/main/java/com/yen/springBootPOC3/controller/HelloController.java
+++ springBootPOC3/src/main/java/com/yen/springBootPOC3/controller/HelloController.java
@@ -2,12 +2,20 @@ package com.yen.springBootPOC3.controller;
 
 /** book p.18 */
 
+import com.yen.springBootPOC3.properties.MysqlProperties;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class HelloController {
+
+    // p.23
+    @Resource // attr, TODO : check this
+    private MysqlProperties mysqlProperties;
+
 
     // inject attr val
     // book p.20
@@ -25,6 +33,17 @@ public class HelloController {
     @GetMapping("/showJDBC")
     public String showJDBC(){
         String msg = "jdbcName = " + jdbcName + "<br/>" + " dburl = " + dburl;
+        return msg;
+    }
+
+
+    @GetMapping("/showJDBC2")
+    public String showJDBC2(){
+
+        mysqlProperties.setJdbcName("com.mysql.jdbc.Driver");
+        mysqlProperties.setDbUrl("jdbc:mysql://localhost:3036/mytest");
+        String msg = "jdbcName = " + mysqlProperties.getJdbcName() + "<br/>" + " dburl = " + mysqlProperties.getDbUrl();
+
         return msg;
     }
 
