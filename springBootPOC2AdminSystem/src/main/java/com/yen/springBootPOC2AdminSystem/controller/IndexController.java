fd07416798dc17d4bb92293632ef66e46843b126
yennanliu <f339339@gmail.com>
2022-05-10 16:04:09 +0800

    add IndexController.java

--- springBootPOC2AdminSystem/src/main/java/com/yen/springBootPOC2AdminSystem/controller/IndexController.java
+++ springBootPOC2AdminSystem/src/main/java/com/yen/springBootPOC2AdminSystem/controller/IndexController.java
@@ -2,5 +2,16 @@ package com.yen.springBootPOC2AdminSystem.controller;
 
 // https://www.youtube.com/watch?v=O8WUR5aSt8U&list=PLmOn9nNkQxJFKh2PMfWbGT7RVuMowsx-u&index=44
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class IndexController {
+
+    /**
+     *  request to endpoint = "/" or "/login"
+     *  will be passed to login.html
+     */
+    @GetMapping(value = {"/", "/login"})
+    public String loginPage(){
+        return "login";
+    }
 }
