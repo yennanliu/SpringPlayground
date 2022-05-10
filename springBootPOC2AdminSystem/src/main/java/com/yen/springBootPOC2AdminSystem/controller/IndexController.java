cb9b01741daefa81bbaab19933ae9eb9ad038ce8
yennanliu <f339339@gmail.com>
2022-05-10 15:57:51 +0800

    add static, templates.. files, add controller class

---
+++ springBootPOC2AdminSystem/src/main/java/com/yen/springBootPOC2AdminSystem/controller/IndexController.java
@@ -0,0 +1,6 @@
+package com.yen.springBootPOC2AdminSystem.controller;
+
+// https://www.youtube.com/watch?v=O8WUR5aSt8U&list=PLmOn9nNkQxJFKh2PMfWbGT7RVuMowsx-u&index=44
+
+public class IndexController {
+}
