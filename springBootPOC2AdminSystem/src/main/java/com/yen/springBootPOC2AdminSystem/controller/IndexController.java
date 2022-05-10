55a8e883eca7f2f7e08606e6d7e6bf08dd914e48
yennanliu <f339339@gmail.com>
2022-05-10 16:38:07 +0800

    add login in controller, add comment in html, code

--- springBootPOC2AdminSystem/src/main/java/com/yen/springBootPOC2AdminSystem/controller/IndexController.java
+++ springBootPOC2AdminSystem/src/main/java/com/yen/springBootPOC2AdminSystem/controller/IndexController.java
@@ -2,14 +2,23 @@ package com.yen.springBootPOC2AdminSystem.controller;
 
 // https://www.youtube.com/watch?v=O8WUR5aSt8U&list=PLmOn9nNkQxJFKh2PMfWbGT7RVuMowsx-u&index=44
 
+/** controller for landing page
+ *
+ *  - login page (login.html)
+ *  - main page (main.html)
+ */
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 // NOTE !!! don't forget @Controller annotation for a controller
 @Controller
 public class IndexController {
 
     /**
+     *  Non - login page
+     *
      *  request to endpoint = "/" or "/login"
      *  will be passed to login.html
      */
@@ -17,4 +26,14 @@ public class IndexController {
     public String loginPage(){
         return "login";
     }
+
+    /**
+     *  already-login page
+     *
+     */
+    @PostMapping("/login")
+    public String main(String username, String password){
+        return "main";
+    }
+
 }
