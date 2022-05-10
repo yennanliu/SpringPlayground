82646862cbc7d4b9a3e4b8870102ab7821da4939
yennanliu <f339339@gmail.com>
2022-05-10 18:14:14 +0800

    update helloController

--- springBootPOC3/src/main/java/com/yen/springBootPOC3/controller/HelloController.java
+++ springBootPOC3/src/main/java/com/yen/springBootPOC3/controller/HelloController.java
@@ -2,15 +2,30 @@ package com.yen.springBootPOC3.controller;
 
 // book p.18
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    // inject attr val
+    // book p.20
+    @Value("${mysql.jdbcName}")
+    private String jdbcName;
+
+    @Value("${mysql.dburl}")
+    private String dburl;
+
     @GetMapping("/hello")
     public String hello(){
         return "hello world!!!";
     }
+
+    @GetMapping("/showJDBC")
+    public String showJDBC(){
+        String msg = "jdbcName = " + jdbcName + "<br/>" + " dburl = " + dburl;
+        return msg;
+    }
+
 }
