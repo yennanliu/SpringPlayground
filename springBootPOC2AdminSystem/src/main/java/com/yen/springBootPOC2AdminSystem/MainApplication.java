f4fa7db35616bb3077f0784bb41253d75ee7644d
yennanliu <f339339@gmail.com>
2022-05-10 16:22:28 +0800

    fix IndexController, comment MainApplication, make login page work

--- springBootPOC2AdminSystem/src/main/java/com/yen/springBootPOC2AdminSystem/MainApplication.java
+++ springBootPOC2AdminSystem/src/main/java/com/yen/springBootPOC2AdminSystem/MainApplication.java
@@ -1,16 +1,16 @@
-package com.yen.springBootPOC2AdminSystem;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-@SpringBootApplication(scanBasePackages = "com.yen")
-public class MainApplication {
-    public static void main(String[] args) {
-
-        System.out.println("-------------------- START");
-        ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class, args);
-
-        System.out.println("-------------------- END");
-    }
-}
+//package com.yen.springBootPOC2AdminSystem;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.ConfigurableApplicationContext;
+//
+//@SpringBootApplication(scanBasePackages = "com.yen")
+//public class MainApplication {
+//    public static void main(String[] args) {
+//
+//        System.out.println("-------------------- START");
+//        ConfigurableApplicationContext run = SpringApplication.run(MainApplication.class, args);
+//
+//        System.out.println("-------------------- END");
+//    }
+//}
--- springBootPOC2AdminSystem/src/main/java/com/yen/springBootPOC2AdminSystem/controller/IndexController.java
+++ springBootPOC2AdminSystem/src/main/java/com/yen/springBootPOC2AdminSystem/controller/IndexController.java
@@ -2,8 +2,11 @@ package com.yen.springBootPOC2AdminSystem.controller;
 
 // https://www.youtube.com/watch?v=O8WUR5aSt8U&list=PLmOn9nNkQxJFKh2PMfWbGT7RVuMowsx-u&index=44
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+// NOTE !!! don't forget @Controller annotation for a controller
+@Controller
 public class IndexController {
 
     /**
