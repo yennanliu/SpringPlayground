1a60031f134f165b5ea3199b8a1b1b940903b5ec
yennanliu <f339339@gmail.com>
2022-05-17 22:59:31 +0800

    add more examples

--- springBootPOC3/src/main/java/com/yen/springBootPOC3/SpringBootPoc3Application.java
+++ springBootPOC3/src/main/java/com/yen/springBootPOC3/SpringBootPoc3Application.java
@@ -9,16 +9,18 @@ package com.yen.springBootPOC3;
 import com.yen.springBootPOC3.servlet.MyServlet1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@ServletComponentScan
 public class SpringBootPoc3Application {
 
-	@Bean
-	public ServletRegistrationBean MyServlet1(){
-		return new ServletRegistrationBean(new MyServlet1(), "/myServlet/**");
-	}
+//	@Bean
+//	public ServletRegistrationBean MyServlet1(){
+//		return new ServletRegistrationBean(new MyServlet1(), "/myServlet/**");
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPoc3Application.class, args);
