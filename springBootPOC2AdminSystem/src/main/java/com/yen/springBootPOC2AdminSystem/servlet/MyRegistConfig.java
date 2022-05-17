c1d99ed754c0df786013ba1d4e29b3c2c2572cee
yennanliu <f339339@gmail.com>
2022-05-17 08:31:24 +0800

    fix

--- springBootPOC2AdminSystem/src/main/java/com/yen/springBootPOC2AdminSystem/servlet/MyRegistConfig.java
+++ springBootPOC2AdminSystem/src/main/java/com/yen/springBootPOC2AdminSystem/servlet/MyRegistConfig.java
@@ -2,10 +2,23 @@ package com.yen.springBootPOC2AdminSystem.servlet;
 
 // https://www.youtube.com/watch?v=oi6ChwpC6rc&list=PLmOn9nNkQxJFKh2PMfWbGT7RVuMowsx-u&index=57
 
+/**  MyRegistConfig : A config file
+ *
+ *    1) same purpose as MyFilter, MyServlet, MyServletContextListener
+ *    2) NOTE !!!
+ *      -> since it's a config file, we need below annotations:
+ *          @Configuration
+ *          @Bean
+ *      ref :  https://github.com/yennanliu/SpringPlayground/blob/main/springBootPOC1/src/main/java/com/yen/SpringBootPart1/config/MyConfig.java
+ *
+ */
+
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
 
 @Configuration
 public class MyRegistConfig {
@@ -21,7 +34,14 @@ public class MyRegistConfig {
     public FilterRegistrationBean myFilter(){
 
         MyFilter myFilter = new MyFilter();
-        return  new FilterRegistrationBean(myFilter, myServlet());
+
+        // method 1
+        //return new FilterRegistrationBean(myFilter, myServlet());
+
+        // method 2
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(myFilter);
+        filterRegistrationBean.setUrlPatterns(Arrays.asList("/my", "/my01"));
+        return filterRegistrationBean;
     }
 
 }
