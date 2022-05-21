27b42c579b13d761d41885dd6b0a384451fe5965
yennanliu <f339339@gmail.com>
2022-05-21 19:35:59 +0800

    add mybatis plus dep, add bean, ddl

---
+++ springBootPOC2AdminSystem/src/main/java/com/yen/springBootPOC2AdminSystem/bean/User2.java
@@ -0,0 +1,20 @@
+package com.yen.springBootPOC2AdminSystem.bean;
+
+// https://www.youtube.com/watch?v=njvVPhCFH6o&list=PLmOn9nNkQxJFKh2PMfWbGT7RVuMowsx-u&index=66
+
+/**
+ *  User2 Bean (myBatis plus test)
+ */
+
+
+import lombok.Data;
+
+@Data
+public class User2 {
+
+    // attr
+    private Long id;
+    private String name;
+    private int age;
+    private String email;
+}
