9c869c71c3d3321f06a1531d66bcb80c0e024d39
yen.liu <yen.l@binance.com>
2022-05-23 18:33:10 +0800

    update

--- ref_project/markdown-blog-main/src/main/java/np/com/roshanadhikary/mdblog/MdBlogApplication.java
+++ ref_project/markdown-blog-main/src/main/java/np/com/roshanadhikary/mdblog/MdBlogApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MdBlogApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MdBlogApplication.class, args);
 	}
-
 }
