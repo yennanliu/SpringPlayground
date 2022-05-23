6924989fe2e23719585b70e0dd782870066007c8
yennanliu <f339339@gmail.com>
2022-05-23 21:52:29 +0800

    update

--- springBootPOC3/src/main/java/com/yen/springBootPOC3/controller/UserController.java
+++ springBootPOC3/src/main/java/com/yen/springBootPOC3/controller/UserController.java
@@ -1,12 +1,13 @@
 package com.yen.springBootPOC3.controller;
 
 import com.sun.org.apache.bcel.internal.generic.PUSH;
-import com.yen.springBootPOC3.dao.UserRepository;
+import com.yen.springBootPOC3.dao.UserCrudRepository;
+import com.yen.springBootPOC3.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /** book p.85 */
 
@@ -15,9 +16,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
     @Autowired
-    private UserRepository userRepository;
+    private UserCrudRepository userCrudRepository;
 
     @GetMapping("/add")
-    @RequestBody
-    public String addNewUser
+    @ResponseBody
+    public String addNewUser(@RequestParam String firstname, @RequestParam String lastname){
+        User user = new User(firstname, lastname);
+        userCrudRepository.save(user);
+        return "saved";
+    }
+
+    @GetMapping("/finduser/{lastname}")
+    @ResponseBody
+    public String findUser(@RequestParam("lastname") String lastname){
+        List<User> userList = userCrudRepository.finDByLastName(lastname);
+        String users = "";
+        for (User user:userList){
+            users += user.toString() + "  ";
+        }
+        return users;
+    }
+
 }
--- springBootPOC3/src/main/java/com/yen/springBootPOC3/dao/UserCrudRepository.java
+++ springBootPOC3/src/main/java/com/yen/springBootPOC3/dao/UserCrudRepository.java
@@ -3,7 +3,10 @@ package com.yen.springBootPOC3.dao;
 import com.yen.springBootPOC3.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /** book p. 77 */
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserCrudRepository extends JpaRepository<User, Long> {
+    List<User> finDByLastName(String lastname);
 }
