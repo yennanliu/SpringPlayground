52349ec0882590a783f9546ac02aa2e6afb93f79
yen.liu <yen.l@binance.com>
2022-05-27 18:56:20 +0800

    fix dep, init entities

---
+++ SpringBlog/src/main/java/com/yen/SpringBlog/entities/Post.java
@@ -0,0 +1,13 @@
+package com.yen.SpringBlog.entities;
+
+// https://www.roshanadhikary.com.np/2021/05/build-a-markdown-based-blog-with-spring-boot-part-1.html
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+public class Post {
+}
