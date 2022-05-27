c9c5bb7fed45bde286ad472b4c49d01691e52e01
yennanliu <f339339@gmail.com>
2022-05-28 04:51:11 +0800

    add

---
+++ springBlog/src/main/java/com/yen/SpringBlog/entities/Author.java
@@ -0,0 +1,31 @@
+package com.yen.SpringBlog.entities;
+
+// https://www.roshanadhikary.com.np/2021/05/build-a-markdown-based-blog-with-spring-boot-part-1.html
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "authors")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String email;
+
+    @Column
+    private String url;
+
+    @Column
+    @OneToMany(mappedBy = "author")
+    private List<Post> posts;
+}
