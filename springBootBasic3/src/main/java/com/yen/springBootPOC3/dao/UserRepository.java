afaf7381c2687f5a40a4a2d8617ff30863d314b2
yennanliu <f339339@gmail.com>
2022-05-28 08:08:18 +0800

    clean

--- springBootBasic3/src/main/java/com/yen/springBootPOC3/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.yen.springBootPOC3.dao;
-
-import com.yen.springBootPOC3.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
