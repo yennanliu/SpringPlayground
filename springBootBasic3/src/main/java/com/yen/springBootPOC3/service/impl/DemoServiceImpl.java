77b764098bc4a3ff6b709a5967bcc152f64939c9
yennanliu <f339339@gmail.com>
2022-05-28 11:30:24 +0800

    add DemoServiceImpl.java, update progress

--- springBootBasic3/src/main/java/com/yen/springBootPOC3/service/impl/DemoServiceImpl.java
+++ springBootBasic3/src/main/java/com/yen/springBootPOC3/service/impl/DemoServiceImpl.java
@@ -4,9 +4,12 @@ import com.yen.springBootPOC3.dao.DemoInfoRepository;
 import com.yen.springBootPOC3.entity.DemoInfo;
 import com.yen.springBootPOC3.service.DemoService;
 import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.crossstore.ChangeSetPersister;
 
 import javax.annotation.Resource;
+
 import java.util.Optional;
 
 /** book p.146 */
@@ -22,18 +25,36 @@ public class DemoServiceImpl implements DemoService {
 
     private static final String DEMO_CACHE_NAME = "demo";
 
+    @CacheEvict(value = DEMO_CACHE_NAME, key = "'demoInfo_' + #id")
     @Override
     public void delete(Long id) {
+        System.out.println(">>> simulate delete " + id);
     }
 
+    @CachePut(value = DEMO_CACHE_NAME, key = "'demoInfo_' + #updated.getId()")
     @Override
     public DemoInfo update(DemoInfo updated) throws ChangeSetPersister.NotFoundException {
-        return null;
+
+        Optional<DemoInfo> optionalInfo = demoInfoRepository.findById(updated.getId());
+
+        if (optionalInfo == null){
+            // TODO : fix below import (NotFoundException)
+            //throw new NotFoundException(">>> Not found");
+            throw new RuntimeException(">>> Not found");
+        }
+
+        DemoInfo demoInfo;
+        optionalInfo.get().setName(updated.getName());
+        optionalInfo.get().setPwd(updated.getPwd());
+        demoInfo = optionalInfo.get();
+        return demoInfo;
     }
 
+    @Cacheable(value = DEMO_CACHE_NAME, key = "'demoInfo_' + '#id'")
     @Override
     public Optional<DemoInfo> findById(long id) {
-        return Optional.empty();
+        System.err.println(">>> not via cache !!! " + id);
+        return demoInfoRepository.findById(id);
     }
 
     @CacheEvict(value = DEMO_CACHE_NAME, key = CACHE_KEY)
