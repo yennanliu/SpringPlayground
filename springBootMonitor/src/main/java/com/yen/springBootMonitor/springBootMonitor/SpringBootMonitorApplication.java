3968c6e8fdbd99a056495c78d01012e1073573cb
yennanliu <f339339@gmail.com>
2022-05-30 22:32:53 +0800

    add springBootMonitor/

---
+++ springBootMonitor/src/main/java/com/yen/springBootMonitor/springBootMonitor/SpringBootMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.yen.springBootMonitor.springBootMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMonitorApplication.class, args);
+	}
+
+}
---
+++ springBootMonitor/src/test/java/com/yen/springBootMonitor/springBootMonitor/SpringBootMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.springBootMonitor.springBootMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
