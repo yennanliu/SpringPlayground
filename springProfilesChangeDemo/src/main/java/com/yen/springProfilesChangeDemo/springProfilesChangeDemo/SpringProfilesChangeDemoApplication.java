ae68713dbeb36a477d2cbd7e1f36f3fb66811598
yennanliu <f339339@gmail.com>
2022-05-31 07:12:00 +0800

    add springProfilesChangeDemo/

---
+++ springProfilesChangeDemo/src/main/java/com/yen/springProfilesChangeDemo/springProfilesChangeDemo/SpringProfilesChangeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yen.springProfilesChangeDemo.springProfilesChangeDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProfilesChangeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProfilesChangeDemoApplication.class, args);
+	}
+
+}
---
+++ springProfilesChangeDemo/src/test/java/com/yen/springProfilesChangeDemo/springProfilesChangeDemo/SpringProfilesChangeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.springProfilesChangeDemo.springProfilesChangeDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProfilesChangeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
