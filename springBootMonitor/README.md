5d9e54c0950407f0b77a0eff002408e796923df4
yennanliu <f339339@gmail.com>
2022-05-30 22:49:06 +0800

    add monitor conf, yml, dep

---
+++ springBootMonitor/README.md
@@ -0,0 +1,22 @@
+# Spring admin monitor
+> Spring app monitor
+
+## Run
+```bash
+# build
+mvn package
+
+# run
+java -jar <built_jar>
+```
+
+## Monitor
+```bash
+jconsole
+```
+
+## API
+
+| API | Type | Purpose | Example cmd | Comment|
+| ----- | -------- | ---- | ----- | ---- |
+| `GET` | GET | | http://localhost:9999/ ||
--- springBootMonitor/src/main/java/com/yen/springBootMonitor/springBootMonitor/SpringBootMonitorApplication.java
+++ springBootMonitor/src/main/java/com/yen/springBootMonitor/springBootMonitor/SpringBootMonitorApplication.java
@@ -1,8 +1,12 @@
 package com.yen.springBootMonitor.springBootMonitor;
 
+// https://www.youtube.com/watch?v=yFI4QwPZMew&list=PLmOn9nNkQxJFKh2PMfWbGT7RVuMowsx-u&index=81
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAdminServer
 @SpringBootApplication
 public class SpringBootMonitorApplication {
 
