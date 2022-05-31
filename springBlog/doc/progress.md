ca82f7ff72b59e156e7c9cd44610b67e10606226
yennanliu <f339339@gmail.com>
2022-05-31 22:24:41 +0800

    add /MdToHtmlRenderer, update progress

--- springBlog/doc/progress.md
+++ springBlog/doc/progress.md
@@ -1,6 +1,11 @@
 # Progress
 
+- 20220531
+    - https://www.roshanadhikary.com.np/2021/05/build-a-markdown-based-blog-with-spring-boot-part-3.html
+    - https://www.roshanadhikary.com.np/2021/05/build-a-markdown-based-blog-with-spring-boot-part-4.html
+
 - 20220530
     - https://www.roshanadhikary.com.np/2021/05/build-a-markdown-based-blog-with-spring-boot-part-3.html
+
 - 20220527
 	- https://www.roshanadhikary.com.np/2021/05/build-a-markdown-based-blog-with-spring-boot-part-2.html
---
+++ springBlog/src/main/java/com/yen/SpringBlog/utils/MdToHtmlRenderer.java
@@ -0,0 +1,28 @@
+package com.yen.SpringBlog.utils;
+
+// https://www.roshanadhikary.com.np/2021/05/build-a-markdown-based-blog-with-spring-boot-part-3.html
+
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+import java.util.List;
+
+public class MdToHtmlRenderer {
+
+    public String redenHTML(List<String> markdownLines){
+
+        Parser parser = Parser.builder().build();
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+
+        StringBuilder renderedSB = new StringBuilder();
+
+        for (String markDownLine : markdownLines){
+            Node document = parser.parse(markDownLine);
+            renderedSB.append(renderer.render(document));
+        }
+
+        return new String(renderedSB);
+    }
+
+}
