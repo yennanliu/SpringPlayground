34ec941a576d1a198b018224c7ea37cef88b1117
yennanliu <f339339@gmail.com>
2022-06-04 19:03:58 +0800

    add spring-boot-app module, dep, conf

---
+++ spring-boot-app/src/main/java.com.yen.springApp/controller/HelloController.java
@@ -0,0 +1,22 @@
+package controller;
+
+// https://www.youtube.com/watch?v=gFz5MLFSQKQ&list=PLmOn9nNkQxJFKh2PMfWbGT7RVuMowsx-u&index=84
+
+import com.yen.hello.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping("/test")
+    public String sayHello(){
+        String msg =  helloService.sayHello("Lynn");
+        return msg;
+    }
+
+}
