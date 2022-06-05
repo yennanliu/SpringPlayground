d35e8e8b51babab35902dc3a293e7cef106ef5b9
yennanliu <f339339@gmail.com>
2022-06-05 08:05:39 +0800

    init springBootAdvance1/

---
+++ springBootAdvance1/src/main/java/com/yen/springBootAdvance1/SpringBootAdvance1Application.java
@@ -0,0 +1,13 @@
+package com.yen.springBootAdvance1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdvance1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdvance1Application.class, args);
+	}
+
+}
---
+++ springBootAdvance1/src/test/java/com/yen/springBootAdvance1/SpringBootAdvance1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.springBootAdvance1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdvance1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
