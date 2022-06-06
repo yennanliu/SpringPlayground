0da8aa7744bd3aef6121f5e0dbdd3a46b8fe41ac
yennanliu <f339339@gmail.com>
2022-06-07 06:57:13 +0800

    add cache to EmployeeService

--- springBootAdvance1/src/main/java/com/yen/springBootAdvance1/service/EmployeeService.java
+++ springBootAdvance1/src/main/java/com/yen/springBootAdvance1/service/EmployeeService.java
@@ -6,6 +6,7 @@ package com.yen.springBootAdvance1.service;
 import com.yen.springBootAdvance1.bean.Employee;
 import com.yen.springBootAdvance1.mapper.EmployeeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,6 +15,29 @@ public class EmployeeService {
     @Autowired
     EmployeeMapper employeeMapper;
 
+    /**
+     *  @Cacheable
+     *  -> cache method result, will use cache when there is same request again
+     *
+     *  - CacheManager for cache management, every cache has its name in CacheManager
+     *
+     *  -> cache param
+     *      - cacheNames/value : cache name
+     *      - key : key for cache (k-v), use method param name by default
+     *          - use SpEl, example: #id (param id), #a0, #p0, #root.args[0]
+     *      - keyGenerator : generate key, we can define our own keyGenerator as well
+     *          - (can only use either key or keyGenerator at once)
+     *      - CacheManager : declare which CacheManager to use
+     *      - CacheResolver
+     *          - (can only use either CacheManager or CacheResolver at once)
+     *      - condition : if condition is true, use cache
+     *      - unless : if unless is true, NOT use cache
+     *          - e.g. : unless = "#result == null"
+     *      - async: whether use async in cache
+     *
+     */
+    @Cacheable(cacheNames = {"emp"})
+    //@Cacheable(cacheNames = {"emp"}, condition = "#id>0")
     public Employee getEmp(Integer id){
         System.out.println(">>> query employee with id = " + id);
         Employee emp = employeeMapper.getEmpById(id);
