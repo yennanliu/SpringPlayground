f6793f9caff1d56905573eeac3fb3f38b496fe67
yen.liu <yen.l@binance.com>
2022-06-09 16:34:50 +0800

    add SwaggerDemo/

---
+++ SwaggerDemo/src/main/java/com/yen/SwaggerDemo/SwaggerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yen.SwaggerDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerDemoApplication.class, args);
+	}
+
+}
