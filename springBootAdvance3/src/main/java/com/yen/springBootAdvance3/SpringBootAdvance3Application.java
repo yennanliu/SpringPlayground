8f97ddaea57ebe5f7af1829fbe97f428b80b777c
yennanliu <f339339@gmail.com>
2022-06-15 07:40:43 +0800

    add springBootAdvance3/

---
+++ springBootAdvance3/src/main/java/com/yen/springBootAdvance3/SpringBootAdvance3Application.java
@@ -0,0 +1,13 @@
+package com.yen.springBootAdvance3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdvance3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdvance3Application.class, args);
+	}
+
+}
---
+++ springBootAdvance3/src/test/java/com/yen/springBootAdvance3/SpringBootAdvance3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.springBootAdvance3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdvance3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
