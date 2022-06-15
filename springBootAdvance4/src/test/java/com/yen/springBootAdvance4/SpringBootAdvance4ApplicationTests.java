39dd241738e7182988ca8b95e2e8d0cfc0e0d692
yennanliu <f339339@gmail.com>
2022-06-16 07:42:50 +0800

    add springBootAdvance4/

---
+++ springBootAdvance4/src/test/java/com/yen/springBootAdvance4/SpringBootAdvance4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.springBootAdvance4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAdvance4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
