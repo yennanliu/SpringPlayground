be9d81f82c92ecd0784aa5401e451ce5e00d886a
yen.liu <yen.l@binance.com>
2022-06-15 17:01:40 +0800

    fix historyController return type

--- pocDownloadService/downloadService-api/src/main/java/com/yen/api/HistoryApi.java
+++ pocDownloadService/downloadService-api/src/main/java/com/yen/api/HistoryApi.java
@@ -4,10 +4,12 @@ import com.yen.data.bean.Task;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+
 @RequestMapping(value = "/history")
 public interface HistoryApi {
 
     @GetMapping("/list")
-    public Task[] getAllHistory();
+    public <T> ArrayList<T> getAllHistory();
 
 }
--- pocDownloadService/downloadService-web/src/main/java/com/yen/web/controller/HistoryController.java
+++ pocDownloadService/downloadService-web/src/main/java/com/yen/web/controller/HistoryController.java
@@ -5,13 +5,25 @@ import com.yen.data.bean.Task;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @Slf4j
 @RestController
 public class HistoryController implements HistoryApi {
     @Override
-    public Task[] getAllHistory() {
+    public ArrayList<Task> getAllHistory() {
         System.out.println(">>> get all history ...");
-        return new Task[0];
+
+        Task t1 = new Task(001, 100, 200, "trade", "running");
+        Task t2 = new Task(002, 101, 101, "trade", "completed");
+
+        ArrayList tasks = new ArrayList();
+        tasks.add(t1);
+        tasks.add(t2);
+
+        System.out.println(">>> tasks = " + tasks.toArray().toString());
+
+        return tasks;
     }
 
 }
