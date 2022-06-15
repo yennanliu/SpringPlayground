bb196e8f6c7e47f76318afef02490f0e004accd9
yennanliu <f339339@gmail.com>
2022-06-15 08:32:36 +0800

    add more send email test

--- springBootAdvance3/src/test/java/com/yen/springBootAdvance3/SpringBootAdvance3ApplicationTests.java
+++ springBootAdvance3/src/test/java/com/yen/springBootAdvance3/SpringBootAdvance3ApplicationTests.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
+import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @SpringBootTest
 class SpringBootAdvance3ApplicationTests {
@@ -34,19 +37,24 @@ class SpringBootAdvance3ApplicationTests {
 	}
 
 	@Test
-	public void testSendEmail2(){
+	public void testSendEmail2() throws MessagingException {
 
 		// step 1) create complex mail instance
 		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+		MimeMessageHelper helper= new MimeMessageHelper(mimeMessage, true);
 
-		SimpleMailMessage message = new SimpleMailMessage();
+		// step 2) setting
+		helper.setSubject("today is holiday!!!");
+		helper.setText("<b>Subject : </b>", true);
+		helper.setText("ho ho ho enjoy ur day");
+		helper.setTo("test@google.com");
+		helper.setFrom("admin@google.com");
 
-		message.setSubject("today is holiday!!!");
-		message.setText("ho ho ho enjoy ur day");
-		message.setTo("test@google.com");
-		message.setFrom("admin@google.com");
+		// step 3) upload file
+		helper.addAttachment("test.txt", new File("springBootAdvance3/README.md"));
+		helper.addAttachment("test2.txt", new File("springBootAdvance3/pom.xml"));
 
-		javaMailSender.send(message);
+		javaMailSender.send(mimeMessage);
 	}
 
 }
