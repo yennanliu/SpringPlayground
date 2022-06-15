1df496dbf7b2d2ac422b48659da08d1bb2c869fe
yen.liu <yen.l@binance.com>
2022-06-15 16:11:36 +0800

    comment no need dep, fix downlaodApp

--- pocDownloadService/downloadService-web/src/main/java/com/yen/web/DownloadApp.java
+++ pocDownloadService/downloadService-web/src/main/java/com/yen/web/DownloadApp.java
@@ -1,7 +1,13 @@
 package com.yen.web;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class DownloadApp {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        SpringApplication.run(DownloadApp.class, args);
     }
+
 }
