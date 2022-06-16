0861d7ad1637f0b0caf74a0b213ea6adb02fc5dc
yennanliu <f339339@gmail.com>
2022-06-17 07:44:11 +0800

    add dep, consumer app

---
+++ springBootAdvance4/consumer-user/src/main/java/com/yen/user/ConsumerUserApplication.java
@@ -0,0 +1,20 @@
+package com.yen.user;
+
+// https://www.youtube.com/watch?v=tF_Y4G1FovQ&list=PLmOn9nNkQxJESDPnrV6v_aiFgsehwLgku&index=34
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *  ConsumerUserApplication
+ *  
+ *   step 1) import dep
+ *   step 2) config dubbo registry url
+ *   step 3) call service
+ */
+@SpringBootApplication
+public class ConsumerUserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerUserApplication.class, args);
+    }
+}
--- springBootAdvance4/provider-ticker/src/main/java/com/yen/ticket/ProviderTicketApplication.java
+++ springBootAdvance4/provider-ticker/src/main/java/com/yen/ticket/ProviderTicketApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- *   Register provider service to registry center:
+ *  ProviderTicketApplication
+ *
+ *    Register provider service to registry center:
  *    - step 1) import dubbo and ZK client dependency
  *    - step 2) config dubbo scan package and registry center url
  *    - step 3) publish service via @Service (TicketServiceImpl in this project)
