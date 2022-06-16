dcd56f1cc33b2161d0fdc2b2cc464f0d4d415fe3
yennanliu <f339339@gmail.com>
2022-06-17 07:07:23 +0800

    add annotation, rename app file, upodate conf

--- springBootAdvance4/provider-ticker/src/main/java/com/yen/ticket/service/impl/TicketServiceImpl.java
+++ springBootAdvance4/provider-ticker/src/main/java/com/yen/ticket/service/impl/TicketServiceImpl.java
@@ -1,9 +1,14 @@
 package com.yen.ticket.service.impl;
 
 // https://www.youtube.com/watch?v=tF_Y4G1FovQ&list=PLmOn9nNkQxJESDPnrV6v_aiFgsehwLgku&index=33
+// https://www.youtube.com/watch?v=tF_Y4G1FovQ&list=PLmOn9nNkQxJESDPnrV6v_aiFgsehwLgku&index=34
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.yen.ticket.service.TicketService;
+import org.springframework.stereotype.Component;
 
+@Component
+@Service  // publish service
 public class TicketServiceImpl implements TicketService {
     @Override
     public String getTicket() {
