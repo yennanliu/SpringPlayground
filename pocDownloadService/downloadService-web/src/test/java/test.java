9a33f1114900ba0b01d68209568d88572fb6d2ac
yennanliu <f339339@gmail.com>
2022-06-16 23:19:41 +0800

    remove compile version

--- pocDownloadService/downloadService-web/src/test/java/test.java
+++ pocDownloadService/downloadService-web/src/test/java/test.java
@@ -1,25 +1,25 @@
-import com.yen.service.RedshiftService;
-import com.yen.web.service.impl.RedshiftServiceImpl;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
-public class test {
-
-    @Autowired
-    RedshiftService redshiftService;
-
-    @Autowired
-    RedshiftServiceImpl redshiftServiceimpl;
-
-    @Test
-    public void test1(){
-
-        RedshiftServiceImpl redshiftService1 = new RedshiftServiceImpl();
-        System.out.println(123);
-        System.out.println(redshiftService1.runQuery("123"));
-        //System.out.println(redshiftServiceimpl.runQuery("123"));
-
-        System.out.println(redshiftService1.runUnload("myTable", "myFile"));
-    }
-
-}
+//import com.yen.service.RedshiftService;
+//import com.yen.web.service.impl.RedshiftServiceImpl;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//public class test {
+//
+//    @Autowired
+//    RedshiftService redshiftService;
+//
+//    @Autowired
+//    RedshiftServiceImpl redshiftServiceimpl;
+//
+//    @Test
+//    public void test1(){
+//
+//        RedshiftServiceImpl redshiftService1 = new RedshiftServiceImpl();
+//        System.out.println(123);
+//        System.out.println(redshiftService1.runQuery("123"));
+//        //System.out.println(redshiftServiceimpl.runQuery("123"));
+//
+//        System.out.println(redshiftService1.runUnload("myTable", "myFile"));
+//    }
+//
+//}
