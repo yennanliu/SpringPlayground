cdcc4c2bf8e67663474a9268e6ad63ad17fdb8d0
yennanliu <f339339@gmail.com>
2022-06-17 08:20:55 +0800

    add springBootAdvance5/

---
+++ springBootAdvance5/eureka-server/src/main/java/com/yen/eurekaserver/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.yen.eurekaserver;
+
+// https://www.youtube.com/watch?v=P5o-6Od5cfc&list=PLmOn9nNkQxJESDPnrV6v_aiFgsehwLgku&index=34
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *  Registry center
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
