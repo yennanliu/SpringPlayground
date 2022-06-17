d808335613500c99d9dfbc5acdb6c67bae54c055
yennanliu <f339339@gmail.com>
2022-06-17 08:02:51 +0800

    update

--- springBootAdvance4/consumer-user/src/test/java/com/yen/user/ConsumerUserApplicationTest.java
+++ springBootAdvance4/consumer-user/src/test/java/com/yen/user/ConsumerUserApplicationTest.java
@@ -4,15 +4,19 @@ package com.yen.user;
 
 import com.yen.user.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class) // https://stackoverflow.com/questions/42550874/intellij-junit-runwith-not-resolved
 @SpringBootTest
 public class ConsumerUserApplicationTest {
 
     @Autowired
     UserService userService;
 
+    // TODO : fix below
     @Test
     public void test1(){
         userService.hello();
