5bb3a70aa4f32ddbd973a757f621430bb95dbae1
yennanliu <f339339@gmail.com>
2022-06-18 18:29:42 +0800

    add health indicator, update app comment

--- springBootAdvance6/src/main/java/com/yen/springBootAdvance6/SpringBootAdvance6Application.java
+++ springBootAdvance6/src/main/java/com/yen/springBootAdvance6/SpringBootAdvance6Application.java
@@ -1,8 +1,17 @@
 package com.yen.springBootAdvance6;
 
+// https://www.youtube.com/watch?v=dZS2D_kHhCM&list=PLmOn9nNkQxJESDPnrV6v_aiFgsehwLgku&index=41
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ *  Custom service health indicator
+ *    step 1) create indicator (implement HealthIndicator interface)
+ *    step 2) indicator name should be as XXXIndicator
+ *    step 3) add above to container
+ */
 @SpringBootApplication
 public class SpringBootAdvance6Application {
 
---
+++ springBootAdvance6/src/main/java/com/yen/springBootAdvance6/health/MyHealthIndicator.java
@@ -0,0 +1,24 @@
+package com.yen.springBootAdvance6.health;
+
+// https://www.youtube.com/watch?v=dZS2D_kHhCM&list=PLmOn9nNkQxJESDPnrV6v_aiFgsehwLgku&index=41
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+
+        /** define health check logic*/
+
+        // if health
+        //return Health.up().build();
+        
+        // if NOT health
+        return Health.down().withDetail("msg", "service is wrong").build();
+    }
+
+}
