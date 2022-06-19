3306c792382972fdddfaf1207681ce63657c09d5
yennanliu <f339339@gmail.com>
2022-06-19 18:31:30 +0800

    add updates

--- pocDownloadService/downloadService-web/src/main/java/com/yen/web/DownloadApp.java
+++ pocDownloadService/downloadService-web/src/main/java/com/yen/web/DownloadApp.java
@@ -1,9 +1,11 @@
 package com.yen.web;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(value = "com.yen.web.mapper")
 public class DownloadApp {
     public static void main(String[] args) {
 
