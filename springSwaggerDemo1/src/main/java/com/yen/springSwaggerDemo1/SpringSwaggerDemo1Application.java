b9d6c799f508dc6d106675c9cbb3e70e8cd62bff
yennanliu <f339339@gmail.com>
2022-06-20 22:42:42 +0800

    add springSwaggerDemo1/

---
+++ springSwaggerDemo1/src/main/java/com/yen/springSwaggerDemo1/SpringSwaggerDemo1Application.java
@@ -0,0 +1,13 @@
+package com.yen.springSwaggerDemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSwaggerDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSwaggerDemo1Application.class, args);
+	}
+
+}
---
+++ springSwaggerDemo1/src/test/java/com/yen/springSwaggerDemo1/SpringSwaggerDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.springSwaggerDemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSwaggerDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
