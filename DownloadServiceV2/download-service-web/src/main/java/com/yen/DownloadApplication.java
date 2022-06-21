9706f734cedce7c0b609bed3b4f33a68d05eabd4
yennanliu <f339339@gmail.com>
2022-06-21 23:46:30 +0800

    rename path, main app

---
+++ DownloadServiceV2/download-service-web/src/main/java/com/yen/DownloadApplication.java
@@ -0,0 +1,12 @@
+package com.yen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DownloadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DownloadApplication.class, args);
+    }
+
+}
