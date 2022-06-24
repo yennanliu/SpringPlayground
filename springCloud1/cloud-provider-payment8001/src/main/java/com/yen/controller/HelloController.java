ee955bbb86435c5532c03a3a35280153656b3251
yennanliu <f339339@gmail.com>
2022-06-24 08:00:05 +0800

    add test controller, make sure spring work

--- springCloud1/cloud-provider-payment8001/src/main/java/com/yen/controller/HelloController.java
+++ springCloud1/cloud-provider-payment8001/src/main/java/com/yen/controller/HelloController.java
@@ -1,10 +1,15 @@
 package com.yen.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-
-@Controller
+@RestController
 public class HelloController {
+
+    @GetMapping("/test")
+    public String hello(){
+        System.out.println(">>> hello");
+        return "hello";
+    }
+
 }
