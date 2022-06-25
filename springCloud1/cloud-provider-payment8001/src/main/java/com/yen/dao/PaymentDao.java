747c7779b97b70fcbf786ef8fb2401ec8e9c523c
yennanliu <f339339@gmail.com>
2022-06-25 18:42:20 +0800

    add select, insert to paymentDao, disable load PaymentMapper.xml, add comment

--- springCloud1/cloud-provider-payment8001/src/main/java/com/yen/dao/PaymentDao.java
+++ springCloud1/cloud-provider-payment8001/src/main/java/com/yen/dao/PaymentDao.java
@@ -3,12 +3,17 @@ package com.yen.dao;
 // https://www.youtube.com/watch?v=4wWM7MmfxXw&list=PLmOn9nNkQxJGVG1ktTV4SedFWuyef_Pi0&index=10
 
 import com.yen.entities.Payment;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper // @Repository // better to use @Mapper instead of @Repository (@Repository may cause error when insert op)
 public interface PaymentDao {
 
+    @Insert("INSERT INTO payment(serial) VALUES (#{serial});")
     public int create(Payment payment);
-    public Payment getPaymentById(@Param("id") Long id);
+
+    @Select("SELECT * FROM payment WHERE id = #{id};")
+    public Payment getPaymentById(Long id);
 }
--- springCloud1/cloud-provider-payment8001/src/main/java/com/yen/service/PaymentService.java
+++ springCloud1/cloud-provider-payment8001/src/main/java/com/yen/service/PaymentService.java
@@ -3,10 +3,9 @@ package com.yen.service;
 // https://www.youtube.com/watch?v=4wWM7MmfxXw&list=PLmOn9nNkQxJGVG1ktTV4SedFWuyef_Pi0&index=10
 
 import com.yen.entities.Payment;
-import org.apache.ibatis.annotations.Param;
 
 public interface PaymentService {
 
     public int create(Payment payment);
-    public Payment getPaymentById(@Param("id") Long id);
+    public Payment getPaymentById(Long id);
 }
