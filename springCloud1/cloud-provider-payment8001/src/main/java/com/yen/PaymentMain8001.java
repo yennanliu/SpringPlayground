ae88e726b3bdad83ff4f6546377c6cfc8e7c8189
yennanliu <f339339@gmail.com>
2022-06-27 07:57:03 +0800

    add maven devtool to pom.xml

--- springCloud1/cloud-provider-payment8001/src/main/java/com/yen/PaymentMain8001.java
+++ springCloud1/cloud-provider-payment8001/src/main/java/com/yen/PaymentMain8001.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PaymentMain8001 {
     public static void main(String[] args) {
-
         SpringApplication.run(PaymentMain8001.class, args);
     }
 }
