a32e9bd3ee08a134f3654044127ab7a1cf9c70fb
yennanliu <f339339@gmail.com>
2022-06-28 07:45:37 +0800

    update OrderController

--- springCloud1/cloud-consumer-order80/src/main/java/com/yen/controller/OrderController.java
+++ springCloud1/cloud-consumer-order80/src/main/java/com/yen/controller/OrderController.java
@@ -6,6 +6,7 @@ import com.yen.bean.CommonResult;
 import com.yen.bean.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -29,9 +30,22 @@ public class OrderController {
 
     @GetMapping("/payment/create")
     public CommonResult<Payment> create(Payment payment){
-        /** NOTE !!! call cloud-provider-payment8001 services via httpClient */
+        /** NOTE !!! call cloud-provider-payment8001 services via httpClient
+         *
+         *  -> postForObject for calling POST request
+         */
+        log.info(">>> consumer-order-80 call /payment/create ...", payment);
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
 
+    @GetMapping("/payment/get/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
+        /** NOTE !!! call cloud-provider-payment8001 services via httpClient
+         *
+         *  -> getForObject for calling GET request
+         */
+        log.info(">>> consumer-order-80 call /payment/get/{id} ...", id);
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
 
 }
