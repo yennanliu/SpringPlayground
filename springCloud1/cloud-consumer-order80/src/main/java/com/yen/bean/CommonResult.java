e43f82a68e49f18f34a17e4a508f6af9f5b9ad10
yennanliu <f339339@gmail.com>
2022-06-28 07:19:29 +0800

    add bean, controller

---
+++ springCloud1/cloud-consumer-order80/src/main/java/com/yen/bean/CommonResult.java
@@ -0,0 +1,29 @@
+package com.yen.bean;
+
+// https://www.youtube.com/watch?v=4wWM7MmfxXw&list=PLmOn9nNkQxJGVG1ktTV4SedFWuyef_Pi0&index=10
+// https://www.youtube.com/watch?v=8d6BvCZxPwQ&list=PLmOn9nNkQxJGVG1ktTV4SedFWuyef_Pi0&index=13
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommonResult<T> {
+
+    private Integer code;  // http status code : 404 not found, 200 OK ...
+    private String message;
+    private T data;
+
+    public CommonResult(Integer code, String message){
+
+        // method 1)
+        //this(code, message, null);
+
+        // method 2)
+        this.code = code;
+        this.message = message;
+    }
+
+}
---
+++ springCloud1/cloud-consumer-order80/src/main/java/com/yen/controller/OrderController.java
@@ -0,0 +1,11 @@
+package com.yen.controller;
+
+// https://www.youtube.com/watch?v=8d6BvCZxPwQ&list=PLmOn9nNkQxJGVG1ktTV4SedFWuyef_Pi0&index=13
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class OrderController {
+}
