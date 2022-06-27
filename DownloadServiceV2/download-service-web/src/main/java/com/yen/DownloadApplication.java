6be3e79e7312b38c0573037feb601ea40e1245ae
yen.liu <yen.l@binance.com>
2022-06-27 15:28:03 +0800

    backup

--- DownloadServiceV2/download-service-web/src/main/java/com/yen/DownloadApplication.java
+++ DownloadServiceV2/download-service-web/src/main/java/com/yen/DownloadApplication.java
@@ -1,9 +1,13 @@
 package com.yen;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.yen.mapper")
+@ComponentScan(basePackages = {"com.yen"})
 public class DownloadApplication {
     public static void main(String[] args) {
 
--- DownloadServiceV2/download-service-web/src/main/java/com/yen/mapper/EmployeeRepository.java
+++ DownloadServiceV2/download-service-web/src/main/java/com/yen/mapper/EmployeeRepository.java
@@ -1,9 +1,20 @@
-package com.yen.mapper;
-
-import com.yen.bean.Employee;
-import org.apache.ibatis.annotations.Mapper;
-import org.springframework.data.repository.CrudRepository;
-
-@Mapper
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
-}
+//package com.yen.mapper;
+//
+//// https://hackmd.io/@KaiChen/HJEN-mMVw
+//
+//import com.yen.bean.Employee;
+//import org.apache.ibatis.annotations.Mapper;
+//import org.apache.ibatis.annotations.Select;
+//import org.springframework.data.domain.Pageable;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.List;
+//
+//@Repository
+//public interface EmployeeRepository extends CrudRepository<Employee, Long>{
+//
+//    @Query("FROM Employee as e")
+//    public List<Employee> findByIDPaging(Pageable pageable);
+//}
