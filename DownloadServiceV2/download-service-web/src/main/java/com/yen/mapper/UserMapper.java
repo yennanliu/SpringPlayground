b8d6f0462082fc3543021826e2e19f16d32f7beb
yennanliu <f339339@gmail.com>
2022-06-28 08:43:00 +0800

    add jdbc, mysql-java-connector dep, comment code make app work

--- DownloadServiceV2/download-service-web/src/main/java/com/yen/mapper/UserMapper.java
+++ DownloadServiceV2/download-service-web/src/main/java/com/yen/mapper/UserMapper.java
@@ -1,16 +1,16 @@
-package com.yen.mapper;
-
-import com.yen.bean.User;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-
-@Mapper
-public interface UserMapper {
-
-    @Select("SELECT * FROM user WHERE id = #{id}")
-    public User getUserByName(String name);
-
-    @Insert("INSERT INTO user (`name`, `age`) VALUES(#{name},#{age}")
-    public void insertUser(User user);
-}
+//package com.yen.mapper;
+//
+//import com.yen.bean.User;
+//import org.apache.ibatis.annotations.Insert;
+//import org.apache.ibatis.annotations.Mapper;
+//import org.apache.ibatis.annotations.Select;
+//
+//@Mapper
+//public interface UserMapper {
+//
+//    @Select("SELECT * FROM user WHERE id = #{id}")
+//    public User getUserByName(String name);
+//
+//    @Insert("INSERT INTO user (`name`, `age`) VALUES(#{name},#{age}")
+//    public void insertUser(User user);
+//}
--- DownloadServiceV2/download-service-web/src/main/java/com/yen/service/UserService.java
+++ DownloadServiceV2/download-service-web/src/main/java/com/yen/service/UserService.java
@@ -1,22 +1,22 @@
-package com.yen.service;
-
-// https://kucw.github.io/blog/2020/2/spring-unit-test-mockito/
-
-import com.yen.bean.User;
-import com.yen.mapper.UserMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-
-public class UserService {
-
-    @Autowired
-    UserMapper userMapper;
-
-    public User getUserById(String name) {
-        return userMapper.getUserByName(name);
-    }
-
-    public void insertUser(User user) {
-         userMapper.insertUser(user);
-    }
-
-}
+//package com.yen.service;
+//
+//// https://kucw.github.io/blog/2020/2/spring-unit-test-mockito/
+//
+//import com.yen.bean.User;
+//import com.yen.mapper.UserMapper;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//public class UserService {
+//
+//    @Autowired
+//    UserMapper userMapper;
+//
+//    public User getUserById(String name) {
+//        return userMapper.getUserByName(name);
+//    }
+//
+//    public void insertUser(User user) {
+//         userMapper.insertUser(user);
+//    }
+//
+//}
