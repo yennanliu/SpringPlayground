08ec1c2b1ac2dd22004f8eede22b9a84615310c6
yennanliu <f339339@gmail.com>
2022-07-06 07:02:52 +0800

    update ddl

--- DownloadServiceV2/sql/ddl/user.sql
+++ DownloadServiceV2/sql/ddl/user.sql
@@ -1,7 +1,7 @@
 -- DDL for history table (mysql)
 -- DB : data
 
--- DROP table user;
+DROP table user;
 
 CREATE TABLE IF NOT EXISTS user (
     id INT AUTO_INCREMENT,
@@ -15,4 +15,12 @@ INSERT INTO user(name, age)
 values
 ("amy",10),
 ("tim",45),
-("lynn",16);
+("lynn",16),
+("kevin",32),
+("sam",1),
+("john",99),
+("jim",55),
+("shin",23),
+("yamazaki",12),
+("ruby",58),
+("yak",4);
