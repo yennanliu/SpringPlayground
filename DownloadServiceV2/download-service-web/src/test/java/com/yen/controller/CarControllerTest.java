504d5f0baf33e7b94fbe9beb3ad8fa25ae74a580
yennanliu <f339339@gmail.com>
2022-07-16 21:04:18 +0800

    add test

--- DownloadServiceV2/download-service-web/src/test/java/com/yen/controller/CarControllerTest.java
+++ DownloadServiceV2/download-service-web/src/test/java/com/yen/controller/CarControllerTest.java
@@ -1,14 +1,13 @@
 package com.yen.controller;
 
-// https://www.tpisoftware.com/tpu/articleDetails/1256
-
-import com.yen.bean.User;
-import javafx.application.Application;
+import com.yen.bean.Car;
+import com.yen.service.CarService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,34 +18,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class UserControllerTest {
+public class CarControllerTest {
 
     private MockMvc mockMvc;
 
     @Autowired
-    private UserController userController;
+    CarController carController;
 
-    private User user;
+    @MockBean
+    private CarService carService;
+
+    private Car car;
 
     @Before
     public void setup() throws Exception {
-        this.mockMvc = standaloneSetup(this.userController).build();// Standalone context
+        this.mockMvc = standaloneSetup(this.carController).build(); // Standalone context
 
-        User u1 = new User("may", 15);
-    }
-
-    @Test
-    public void testGetUser() throws Exception{
-
-        // Mocking
-        //when(this.employeeService.getEmployeeByName("randy")).thenReturn(employee);
-
-        ResultActions resultActions = mockMvc.perform(get("/user").contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(status().is2xxSuccessful());
+        Car car1 = new Car("ZJZ", 1000);
     }
 
 }
