42d607e612c0b2f121aec74491873cd3f3e55521
yennanliu <f339339@gmail.com>
2022-07-24 19:56:03 +0800

    fix code format, add comment

--- springBootBasic1/src/main/java/com/yen/SpringBootPart1/SpringBootPoc1Application.java
+++ springBootBasic1/src/main/java/com/yen/SpringBootPart1/SpringBootPoc1Application.java
@@ -3,11 +3,17 @@ package com.yen.SpringBootPart1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/** default main application (from spring.io) */
+/**
+ *   Default main application (from spring.io)
+ *
+ *    -> if only wanna run application (regardless of special op in MainApplication)
+ *    -> run this app instead
+ */
 @SpringBootApplication
 public class SpringBootPoc1Application {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringBootPoc1Application.class, args);
 	}
 
