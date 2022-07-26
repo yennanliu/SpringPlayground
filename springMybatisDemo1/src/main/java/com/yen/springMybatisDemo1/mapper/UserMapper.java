c79e44700cd223a4a54b7a11a60749b85b22e871
yen.liu <yen.l@binance.com>
2022-07-26 16:26:19 +0800

    update xml, mapper

--- springMybatisDemo1/src/main/java/com/yen/springMybatisDemo1/mapper/UserMapper.java
+++ springMybatisDemo1/src/main/java/com/yen/springMybatisDemo1/mapper/UserMapper.java
@@ -1,4 +1,13 @@
 package com.yen.springMybatisDemo1.mapper;
 
-public class UserMapper {
+// https://dotblogs.com.tw/zjh/2018/09/28/mybatis_1
+
+import com.yen.springMybatisDemo1.bean.User;
+
+public interface UserMapper {
+
+    public User select1(int id);
+    public int insert1(User user);
+    public int delete1(int id);
+    public int update1(User user);
 }
