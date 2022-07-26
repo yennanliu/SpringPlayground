7eb996ae080cea1df66c3ebb52a29842d390ee10
yen.liu <yen.l@binance.com>
2022-07-26 15:33:15 +0800

    add springMybatisDemo1/

---
+++ springMybatisDemo1/src/main/java/com/yen/springMybatisDemo1/SpringMybatisDemo1Application.java
@@ -0,0 +1,13 @@
+package com.yen.springMybatisDemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMybatisDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMybatisDemo1Application.class, args);
+	}
+
+}
---
+++ springMybatisDemo1/src/test/java/com/yen/springMybatisDemo1/SpringMybatisDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.springMybatisDemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMybatisDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
