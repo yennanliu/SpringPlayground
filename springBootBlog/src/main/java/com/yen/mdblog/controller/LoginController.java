ae20da5a66c42eda51fecc2a2eba6335ca737f8e
yennanliu <f339339@gmail.com>
2022-07-31 16:20:06 +0800

    add model and set val to LoginController, update blog login.html, fix springBootBasic2AdminSystem html format

--- springBootBlog/src/main/java/com/yen/mdblog/controller/LoginController.java
+++ springBootBlog/src/main/java/com/yen/mdblog/controller/LoginController.java
@@ -3,27 +3,41 @@ package com.yen.mdblog.controller;
 import com.yen.mdblog.entity.User;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @Log4j2
 public class LoginController {
 
-    @GetMapping("/login")
-    public String login(User user){
+    @GetMapping(value = {"/login"})
+    public String loginInit(Model model){
 
-        // check login account, pwd
-        if (StringUtils.hasLength(user.getUserName()) && "123".equals(user.getPassWord())){
-            return "redirect:/login_success";
-        }else{
-            log.info(">>> login failed, plz try again ...");
-            return "login";
-        }
+        model.addAttribute("userName", "user");
+        model.addAttribute("passWord", 123);
+
+        return "login";
     }
+
+//    @PostMapping("/login")
+//    public String login(User user){
+//
+//        // check login account, pwd
+//        if (StringUtils.hasLength(user.getUserName()) && "123".equals(user.getPassWord())){
+//            //return "redirect:/login";
+//            return "login_success";
+//        }else{
+//            log.info(">>> login failed, plz try again ...");
+//            //return "login_success";
+//            return "redirect:/login";
+//        }
+//    }
 
     @GetMapping("/login_success")
     public String login_success(){
+
         return "login_success";
     }
 
