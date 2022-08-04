ee23dcb0fffbf71a0b0db81926ee787e88d2f6d1
yennanliu <f339339@gmail.com>
2022-08-04 08:20:45 +0800

    add dest_file as get request param

--- DownloadServiceV2/download-service-web/src/main/java/com/yen/controller/S3Controller.java
+++ DownloadServiceV2/download-service-web/src/main/java/com/yen/controller/S3Controller.java
@@ -6,6 +6,7 @@ import lombok.extern.log4j.Log4j2;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
@@ -20,20 +21,21 @@ public class S3Controller {
     S3Service s3Service;
 
     @GetMapping("/s3/upload")
-    public void upload(){
+    public void upload(@RequestParam("dest_file") String destFile){
 
         Path currentRelativePath = Paths.get("");
         String current_path = currentRelativePath.toAbsolutePath().toString();
         log.info("current_path : {}", current_path);
 
         String BUCKET_NAME =  "yen-bucket1";
-        String KEY =  "test2/";
+        //String KEY =  "README2.md"; // upload file name (s3)
+        String KEY = destFile;
         //String file = "test.txt";
-        String file = "README.md";
+        String file = "README.md"; // to-upload file name (local)
 
-        log.info(">>> putS3Object run ...");
+        log.info(">>> putS3Object run ... destFile={}", destFile);
 
-        s3Service.putS3Object(BUCKET_NAME, KEY+file, new File(file));
+        s3Service.putS3Object(BUCKET_NAME, KEY, new File(file));
     }
 
 }
