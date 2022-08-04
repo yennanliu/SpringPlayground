c396ce46a09df54ea1cab09f1956c36cd50e2a39
yennanliu <f339339@gmail.com>
2022-08-04 08:41:58 +0800

    add s3 download endpoint to controller

--- DownloadServiceV2/download-service-web/src/main/java/com/yen/controller/S3Controller.java
+++ DownloadServiceV2/download-service-web/src/main/java/com/yen/controller/S3Controller.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -28,14 +30,37 @@ public class S3Controller {
         log.info("current_path : {}", current_path);
 
         String BUCKET_NAME =  "yen-bucket1";
-        //String KEY =  "README2.md"; // upload file name (s3)
-        String KEY = destFile;
-        //String file = "test.txt";
+        String KEY = destFile;  // upload file name (s3)
         String file = "README.md"; // to-upload file name (local)
 
         log.info(">>> putS3Object run ... destFile={}", destFile);
 
         s3Service.putS3Object(BUCKET_NAME, KEY, new File(file));
+    }
+
+    @GetMapping("/s3/download")
+    public String download(@RequestParam("s3_file") String s3File){
+
+        Path currentRelativePath = Paths.get("");
+        String current_path = currentRelativePath.toAbsolutePath().toString();
+        log.info("current_path : {}", current_path);
+
+        String BUCKET_NAME =  "yen-bucket1";
+        String KEY = s3File;  // upload file name (s3)
+        String file = "README.md"; // to-upload file name (local)
+        log.info(">>> BUCKET_NAME={}, Key={}, file={}", BUCKET_NAME, s3File, file);
+
+        URL s3FileURL = s3Service.getS3FileUrl(BUCKET_NAME, KEY, new File(file));
+        log.info(">>> s3FileURL = {}", s3FileURL);
+        log.info(">>> s3FileURL.getPath() = {}", s3FileURL.getPath());
+        log.info(">>> s3FileURL.getFile() = {}", s3FileURL.getFile());
+//        try {
+//            log.info(">>> s3FileURL = {}", s3FileURL.getContent());
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+
+        return s3FileURL.getPath();
     }
 
 }
