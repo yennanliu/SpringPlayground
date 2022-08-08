7268b9de77cf0f639981715a360262eda18a5371
yennanliu <f339339@gmail.com>
2022-08-09 07:10:10 +0800

    update MyUserMapper xml. mapper

--- springMybatisDemo1/src/main/java/com/yen/springMybatisDemo1/mapper/MyUserMapper.java
+++ springMybatisDemo1/src/main/java/com/yen/springMybatisDemo1/mapper/MyUserMapper.java
@@ -2,8 +2,20 @@ package com.yen.springMybatisDemo1.mapper;
 
 // https://www.youtube.com/watch?v=xVdgzSkU6po&list=PLmOn9nNkQxJEWFBs6hVmDC5m8SbbIiDwY&index=10
 
+import com.yen.springMybatisDemo1.bean.MyUser;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MyUserMapper {
+
+    /**
+     *     MyBatis facing interface programming :
+     *
+     *     1) namespace should use mapper class (com.yen.springMybatisDemo1.mapper.MyUserMapper in MyUserMapper.xml)
+     *     2) SQL method name (in MyUserMapper.java) is as same as SQL id in xml (in MyUserMapper.xml)
+     */
+
+    int insertUser(MyUser user);
+
 }
