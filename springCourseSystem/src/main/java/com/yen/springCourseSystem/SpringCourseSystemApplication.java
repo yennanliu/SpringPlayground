98cc5a492b601ac987415ee2ef9b8f939a7bf764
yennanliu <f339339@gmail.com>
2022-08-09 23:28:01 +0800

    add springCourseSystem/ project

---
+++ springCourseSystem/src/main/java/com/yen/springCourseSystem/SpringCourseSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yen.springCourseSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourseSystemApplication.class, args);
+	}
+
+}
---
+++ springCourseSystem/src/test/java/com/yen/springCourseSystem/SpringCourseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.springCourseSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
