7f4efe3b2ebf4c844eeccfd42cc4b44ec0a583c5
yennanliu <f339339@gmail.com>
2022-08-11 08:03:19 +0800

    fix/update ddl

--- springCourseSystem/sql/ddl/course_type.sql
+++ springCourseSystem/sql/ddl/course_type.sql
@@ -3,6 +3,14 @@
 
 CREATE TABLE IF NOT EXISTS course_type(
 
-	type_id int PRIMARY KEY AUTO_INCREMENT,
-	type_name varchar(30) not null
+    type_id int PRIMARY KEY AUTO_INCREMENT,
+    type_name varchar(30) not null
 );
+
+-- DML
+INSERT INTO course_type(type_name)
+values
+("professional_compulsory"),
+("professional_elective"),
+("campus_elective"),
+("speech");
--- springCourseSystem/sql/ddl/users.sql
+++ springCourseSystem/sql/ddl/users.sql
@@ -3,7 +3,14 @@
 
 CREATE TABLE IF NOT EXISTS users(
 
-	user_no varchar(20) AUTO_INCREMENT,
-	user_pwd varchar(1000) not null,
-	user_name varchar(100) not null
+    user_no int PRIMARY KEY AUTO_INCREMENT,
+    user_pwd varchar(1000) not null,
+    user_name varchar(100) not null
 );
+
+-- DML
+INSERT INTO users(user_pwd, user_name)
+values
+('123', 'jack'),
+('123', 'lynn'),
+('123', 'ann');
