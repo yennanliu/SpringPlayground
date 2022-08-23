f6910c242d19b085e541bf382fced029d296c556
yennanliu <f339339@gmail.com>
2022-08-23 08:01:11 +0800

    add bulk delete example

--- springMybatisDemo1/src/main/java/com/yen/springMybatisDemo1/mapper/SQLMapper.java
+++ springMybatisDemo1/src/main/java/com/yen/springMybatisDemo1/mapper/SQLMapper.java
@@ -1,6 +1,7 @@
 package com.yen.springMybatisDemo1.mapper;
 
 // https://www.youtube.com/watch?v=sgmK0NNNQdE&list=PLmOn9nNkQxJEWFBs6hVmDC5m8SbbIiDwY&index=35
+// https://www.youtube.com/watch?v=8XqiR7n3O5U&list=PLmOn9nNkQxJEWFBs6hVmDC5m8SbbIiDwY&index=36
 
 import com.yen.springMybatisDemo1.bean.MyUser;
 import org.apache.ibatis.annotations.Mapper;
@@ -14,4 +15,7 @@ public interface SQLMapper {
 
     /** select user by fuzzy search */
     List<MyUser> getUserByLike(@Param("username") String username);
+
+    /** bulk delete */
+    int deleteMulti(@Param("ids") String ids);  // e.g. : "id1,id2"
 }
--- springMybatisDemo1/src/test/java/com/yen/springMybatisDemo1/mapper/TestSQLMapper.java
+++ springMybatisDemo1/src/test/java/com/yen/springMybatisDemo1/mapper/TestSQLMapper.java
@@ -1,6 +1,7 @@
 package com.yen.springMybatisDemo1.mapper;
 
 // https://www.youtube.com/watch?v=sgmK0NNNQdE&list=PLmOn9nNkQxJEWFBs6hVmDC5m8SbbIiDwY&index=35
+// https://www.youtube.com/watch?v=8XqiR7n3O5U&list=PLmOn9nNkQxJEWFBs6hVmDC5m8SbbIiDwY&index=36
 
 import com.yen.springMybatisDemo1.bean.MyUser;
 import org.junit.jupiter.api.Test;
@@ -22,6 +23,13 @@ public class TestSQLMapper {
         List<MyUser> res1 = sqlMapper.getUserByLike("z");
         System.out.println(">>> res1 = " + res1);
         System.out.println(">>> res1.get(0) = " + res1.get(0));
+    }
+
+    @Test
+    public void test2(){
+
+        int res1 = sqlMapper.deleteMulti("3,4,5");
+        System.out.println(">>> res1 = " + res1);
     }
 
 }
