45d04fde721aeac8ca70433cfa5be03fdfc20408
yen.liu <yen.l@binance.com>
2022-08-25 18:33:59 +0800

    add annotation to main app, remove no need import, update progress

--- springCourseSystem/src/main/java/com/yen/springCourseSystem/SpringCourseSystemApplication.java
+++ springCourseSystem/src/main/java/com/yen/springCourseSystem/SpringCourseSystemApplication.java
@@ -1,8 +1,16 @@
 package com.yen.springCourseSystem;
 
+// book p. 262
+
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@ComponentScan(basePackages = "com.yen.springCourseSystem")
+@MapperScan(basePackages = "com.yen.springCourseSystem.mapper")
+@EnableTransactionManagement // TODO : recheck this
 @SpringBootApplication
 public class SpringCourseSystemApplication {
 
--- springCourseSystem/src/main/java/com/yen/springCourseSystem/controller/CourseController.java
+++ springCourseSystem/src/main/java/com/yen/springCourseSystem/controller/CourseController.java
@@ -18,7 +18,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
