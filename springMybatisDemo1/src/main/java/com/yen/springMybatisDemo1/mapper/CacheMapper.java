16a4154be3b31f3d7bb588e045a5ba18d11b0195
yennanliu <f339339@gmail.com>
2022-09-12 22:32:38 +0800

    add mybatis cache example 2

--- springMybatisDemo1/src/main/java/com/yen/springMybatisDemo1/mapper/CacheMapper.java
+++ springMybatisDemo1/src/main/java/com/yen/springMybatisDemo1/mapper/CacheMapper.java
@@ -1,6 +1,7 @@
 package com.yen.springMybatisDemo1.mapper;
 
 // https://www.youtube.com/watch?v=ozomvpRfzSU&list=PLmOn9nNkQxJEWFBs6hVmDC5m8SbbIiDwY&index=57
+// https://www.youtube.com/watch?v=kvf8-YwsuAc&list=PLmOn9nNkQxJEWFBs6hVmDC5m8SbbIiDwY&index=57
 
 import com.yen.springMybatisDemo1.bean.Emp;
 import org.apache.ibatis.annotations.Mapper;
@@ -10,4 +11,5 @@ import org.apache.ibatis.annotations.Param;
 public interface CacheMapper {
 
     Emp getEmpByEid(@Param("eid") Integer eid);
+    void insertEmp(Emp emp);
 }
--- springMybatisDemo1/src/test/java/com/yen/springMybatisDemo1/mapper/TestCacheMapper.java
+++ springMybatisDemo1/src/test/java/com/yen/springMybatisDemo1/mapper/TestCacheMapper.java
@@ -1,6 +1,7 @@
 package com.yen.springMybatisDemo1.mapper;
 
 // https://www.youtube.com/watch?v=ozomvpRfzSU&list=PLmOn9nNkQxJEWFBs6hVmDC5m8SbbIiDwY&index=57
+// https://www.youtube.com/watch?v=kvf8-YwsuAc&list=PLmOn9nNkQxJEWFBs6hVmDC5m8SbbIiDwY&index=57
 
 import com.yen.springMybatisDemo1.bean.Emp;
 import org.junit.jupiter.api.Test;
@@ -13,10 +14,9 @@ public class TestCacheMapper {
     @Autowired
     CacheMapper cacheMapper;
 
-    /**  Test level 1 cache
+    /**  Level 1 cache : demo 1
      *
      *      -> level 1 cache : SqlSession level
-     *
      */
     @Test
     public void test1(){
@@ -29,6 +29,22 @@ public class TestCacheMapper {
 
         Emp emp3 = cacheMapper.getEmpByEid(1);
         System.out.println("emp3 = " + emp3);
+    }
+
+    /**  Level 1 cache : demo 2
+     *
+     *      ->  Level 1 cache will NOT work if there are INSERT, UPDATE, DELETE op
+     */
+    @Test
+    public void test2(){
+
+        Emp emp1 = cacheMapper.getEmpByEid(1);
+        System.out.println("emp1 = " + emp1);
+
+        cacheMapper.insertEmp(new Emp());
+
+        Emp emp2 = cacheMapper.getEmpByEid(1);
+        System.out.println("emp2 = " + emp2);
     }
 
 }
