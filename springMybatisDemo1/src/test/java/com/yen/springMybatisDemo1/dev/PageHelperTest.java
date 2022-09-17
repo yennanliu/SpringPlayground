cf149d0d5db710062531dd0ef6d588a06600ea72
yennanliu <f339339@gmail.com>
2022-09-17 17:08:57 +0800

    add paging dep, conf, demo script

---
+++ springMybatisDemo1/src/test/java/com/yen/springMybatisDemo1/dev/PageHelperTest.java
@@ -0,0 +1,26 @@
+package com.yen.springMybatisDemo1.dev;
+
+// https://www.youtube.com/watch?v=1njY-J-jcq0&list=PLmOn9nNkQxJEWFBs6hVmDC5m8SbbIiDwY&index=66
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ *  Paging demo 1
+ *
+ *   1) limit, index, pageSize, pageNum
+ *   2) index : current page index
+ *   3) pageSize : records shown by every page
+ *   4) pageNum : current page
+ *   5) -> index = (pageNum - 1) * pageSize
+ *
+ */
+@SpringBootTest
+public class PageHelperTest {
+
+    @Test
+    public void test1(){
+
+    }
+
+}
