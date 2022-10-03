22037cfdaa1e9b1788860f24ae3419f9b4cd3543
yennanliu <f339339@gmail.com>
2022-10-03 23:43:18 +0800

    modify pom parent to spring-cloud, add cloud dep, fix /UserInfoDao annotation

--- springUserSystem/src/main/java/com/yen/springUserSystem/mapper/UserInfoDao.java
+++ springUserSystem/src/main/java/com/yen/springUserSystem/mapper/UserInfoDao.java
@@ -3,7 +3,9 @@ package com.yen.springUserSystem.mapper;
 // book p.2-28
 
 import com.yen.springUserSystem.bean.UserInfo;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserInfoDao {
 
     UserInfo selectByMobileNo(String mobileNo);
