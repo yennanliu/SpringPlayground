6c0c93f9c4e8bd4e6ecc8cd3388cc6da2e115ef8
yennanliu <f339339@gmail.com>
2022-10-03 23:53:17 +0800

    add /bootstrap.yml, add EnableDiscoveryClient to main app, update progress

--- springUserSystem/src/main/java/com/yen/springUserSystem/UserSystemApplication.java
+++ springUserSystem/src/main/java/com/yen/springUserSystem/UserSystemApplication.java
@@ -1,8 +1,12 @@
 package com.yen.springUserSystem;
 
+// book p.2-36
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserSystemApplication {
 
