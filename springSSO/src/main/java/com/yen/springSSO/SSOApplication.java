8840dac30ec736b6dfc09ec4bbcd64f8f0b3cdfd
yennanliu <f339339@gmail.com>
2022-10-06 07:02:39 +0800

    add dep, conf, update progress

--- springSSO/src/main/java/com/yen/springSSO/SSOApplication.java
+++ springSSO/src/main/java/com/yen/springSSO/SSOApplication.java
@@ -1,14 +1,19 @@
 package com.yen.springSSO;
 
+// book p. 3-14
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SSOApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(SSOApplication.class, args);
+		SpringApplication.run(
+				SSOApplication.class, args);
 	}
 
 }
--- springSSO/src/test/java/com/yen/springSSO/SSOApplicationTests.java
+++ springSSO/src/test/java/com/yen/springSSO/SSOApplicationTests.java
@@ -1,13 +1,13 @@
-package com.yen.springSSO;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SSOApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.yen.springSSO;
+//
+//import org.junit.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SSOApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
