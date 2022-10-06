df80e9ad34175f906b837b94a0f09ae91415f4ce
yennanliu <f339339@gmail.com>
2022-10-06 09:23:39 +0800

    add AuthConfig, update conf, progress

---
+++ springSSO/src/main/java/com/yen/springSSO/config/AuthServerConfiguration.java
@@ -0,0 +1,126 @@
+package com.yen.springSSO.config;
+
+// book p. 3-18
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.JdbcApprovalStore;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+import javax.sql.DataSource;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfiguration extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    // constructor
+    public AuthServerConfiguration(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+
+        this.authenticationManager =
+                authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+
+        // TODO : implement it !!! book p.3-20
+        //super.configure(security);
+    }
+
+    /** client info op via JDBC */
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+        //super.configure(clients);
+        clients.withClientDetails(new JdbcClientDetailsService(dataSource));
+    }
+
+    /** endpoint auth setting */
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+
+        //super.configure(endpoints);
+
+        // set up endpoint param
+        DefaultTokenServices tokenServices = new DefaultTokenServices();
+        tokenServices.setTokenStore(getJdbcTokenStore());
+
+        // support token refresh
+        tokenServices.setSupportRefreshToken(true);
+        tokenServices.setReuseRefreshToken(false);
+
+        // set up accessToken valid time, use 30 days here
+        tokenServices.setRefreshTokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(30));
+
+        // set up refreshToken valid time, use 30 days here
+        tokenServices.setRefreshTokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(15));
+
+        tokenServices.setClientDetailsService(getClientDetailsService());
+
+        // DB auth info
+        endpoints.authenticationManager( this.authenticationManager )
+                .accessTokenConverter(jwtAccessTokenConverter())
+                .tokenStore(getJdbcTokenStore())
+                .tokenServices(tokenServices)
+                .authorizationCodeServices(getJdbcAuthorizationCodeServices())
+                .approvalStore(getJdbcApprovalStore());
+    }
+
+    /** DB token bean */
+    @Bean
+    public JdbcTokenStore getJdbcTokenStore(){
+
+        return new JdbcTokenStore(dataSource);
+    }
+
+    /** DB token client info */
+    @Bean
+    public ClientDetailsService getClientDetailsService(){
+
+        return new JdbcClientDetailsService(dataSource);
+    }
+
+    /** DB auth code */
+    @Bean
+    public AuthorizationCodeServices getJdbcAuthorizationCodeServices(){
+
+        return new JdbcAuthorizationCodeServices(dataSource);
+    }
+
+    /** DB auth code confirmation record */
+    @Bean
+    public ApprovalStore getJdbcApprovalStore(){
+
+        return new JdbcApprovalStore(dataSource);
+    }
+
+    // TODO : implement it !!!
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter(){
+
+        return null;
+    }
+
+}
