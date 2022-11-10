4c71d2b40fe899601b765d66a2792a95cb7991ca
yennanliu <f339339@gmail.com>
2022-11-11 07:04:22 +0800

    xi cç

--- springGeoFence/efence/src/test/java/com/yen/efence/EfenceApplicationTests.java
+++ springGeoFence/efence/src/test/java/com/yen/efence/EfenceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.yen.efence;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class EfenceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.yen.efence;
+//
+//import org.junit.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class EfenceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
