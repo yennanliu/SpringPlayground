8453f31b55a126ed15fa6275308af2acb4918dbb
yennanliu <f339339@gmail.com>
2022-11-14 07:12:13 +0800

    update

--- springGeoFence/configserver/src/main/java/com/yen/configserver/ConfigserverApplication.java
+++ springGeoFence/configserver/src/main/java/com/yen/configserver/ConfigserverApplication.java
@@ -1,12 +1,17 @@
 package com.yen.configserver;
 
+// https://github.com/yennanliu/SpringPlayground/blob/main/courses/springBoot_springCloud_%E9%A0%82%E7%B4%9A%E9%96%8B%E7%99%BC_src_code/chapter04-configserver/src/main/java/com/wudimanong/configserver/ConfigServer.java
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ConfigserverApplication.class, args);
 	}
 
