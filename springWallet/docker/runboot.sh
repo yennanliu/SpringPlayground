f90db5830ebb3f959b196813c4e70a4d9d279a44
yennanliu <f339339@gmail.com>
2022-12-06 08:30:00 +0800

    fix docker plugin dep, add dockerfiles, update progress, code comment

---
+++ springWallet/docker/runboot.sh
@@ -0,0 +1,3 @@
+# book 5.81
+# https://github.com/yennanliu/SpringPlayground/blob/main/courses/springBoot_springCloud_%E9%A0%82%E7%B4%9A%E9%96%8B%E7%99%BC_src_code/chapter05-wallet/src/main/docker/runboot.shsleep 10
+java -Djava.security.egd=file:/dev/./urandom -jar  /app/wallet.jar
