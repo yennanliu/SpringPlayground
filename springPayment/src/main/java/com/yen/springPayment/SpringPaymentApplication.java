5ba7d12025d7a96750b1585ad7db1a2bdde4d692
yennanliu <f339339@gmail.com>
2022-12-08 07:15:14 +0800

    update validator, app annotation

--- springPayment/src/main/java/com/yen/springPayment/SpringPaymentApplication.java
+++ springPayment/src/main/java/com/yen/springPayment/SpringPaymentApplication.java
@@ -1,9 +1,13 @@
 package com.yen.springPayment;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+@MapperScan("com.yen.springPayment.dao.mapper")
 public class SpringPaymentApplication {
 
 	public static void main(String[] args) {
--- springPayment/src/main/java/com/yen/springPayment/validator/EnumValue.java
+++ springPayment/src/main/java/com/yen/springPayment/validator/EnumValue.java
@@ -8,7 +8,6 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
