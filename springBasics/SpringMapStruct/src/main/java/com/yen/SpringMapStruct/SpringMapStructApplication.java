84a694b2a158c71b0b41128c41b4e13e7e2e91b0
yennanliu <f339339@gmail.com>
2022-12-21 07:20:20 +0800

    add /SpringMapStruct/, /SpringNginx/ project

---
+++ springBasics/SpringMapStruct/src/main/java/com/yen/SpringMapStruct/SpringMapStructApplication.java
@@ -0,0 +1,13 @@
+package com.yen.SpringMapStruct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMapStructApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMapStructApplication.class, args);
+	}
+
+}
---
+++ springBasics/SpringNginx/src/main/java/com/yen/SpringNginx/SpringNginxApplication.java
@@ -0,0 +1,13 @@
+package com.yen.SpringNginx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNginxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNginxApplication.class, args);
+	}
+
+}
