3ce7d2f9b3c43b9a17c505ccf03d532b1344cd88
yennanliu <f339339@gmail.com>
2022-12-22 07:21:55 +0800

    fix dep, conf, add controller

---
+++ springBasics/SpringMapStruct/src/main/java/com/yen/SpringMapStruct/controller/myController.java
@@ -0,0 +1,17 @@
+package com.yen.SpringMapStruct.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/index")
+@RestController
+public class myController {
+
+    @GetMapping("/test1")
+    public String test1(){
+        return "OK";
+    }
+}
