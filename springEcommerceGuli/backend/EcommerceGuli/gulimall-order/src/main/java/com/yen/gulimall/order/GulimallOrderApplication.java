71f570f14db9e4e8a5da70fc2a227337e9d91e0a
yennanliu <f339339@gmail.com>
2023-01-28 06:45:59 +0000

    fix, add order module, update parent pom

---
+++ springEcommerceGuli/backend/EcommerceGuli/gulimall-order/src/main/java/com/yen/gulimall/order/GulimallOrderApplication.java
@@ -0,0 +1,14 @@
+package com.yen.gulimall.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallOrderApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(GulimallOrderApplication.class, args);
+	}
+
+}
