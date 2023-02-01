90a15e89749489c5e0b8451ec5d4727c9d74f00b
yennanliu <f339339@gmail.com>
2023-02-01 20:53:50 +0000

    add Mapperscan, remove no need dep, add code comment

--- springEcommerceGuli/backend/EcommerceGuli/gulimall-product/src/main/java/com/yen/gulimall/product/GulimallProductApplication.java
+++ springEcommerceGuli/backend/EcommerceGuli/gulimall-product/src/main/java/com/yen/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,24 @@
 package com.yen.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *    Integrate with mybatis-plus
+ *    	- https://www.youtube.com/watch?v=Ky5BZim-Y94&list=PLmOn9nNkQxJEwPjhNwGliP_bw3RjkgFCf&index=18
+ *
+ *      1) add dep (gulimall-common)
+ *      2) config
+ *      	- data source
+ *      		- db connector
+ *      		- db info in application.yml
+ *       	- mybatis-plus info
+ *       		- MapperScan in main app
+ *       		- let mybatis know mapper xml file location
+ */
+
+@MapperScan("com.yen.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
