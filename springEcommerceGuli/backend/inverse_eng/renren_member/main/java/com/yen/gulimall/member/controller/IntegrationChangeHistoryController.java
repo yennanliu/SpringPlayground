2a95d0730012e365e60fa2616794de0b6b22d54e
yennanliu <f339339@gmail.com>
2023-02-04 10:36:17 +0000

    add member inverse eng backup file

---
+++ springEcommerceGuli/backend/inverse_eng/renren_member/main/java/com/yen/gulimall/member/controller/IntegrationChangeHistoryController.java
@@ -0,0 +1,90 @@
+package com.yen.gulimall.member.controller;
+
+import java.util.Arrays;
+import java.util.Map;
+
+//import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yen.gulimall.member.entity.IntegrationChangeHistoryEntity;
+import com.yen.gulimall.member.service.IntegrationChangeHistoryService;
+import com.yen.common.utils.PageUtils;
+import com.yen.common.utils.R;
+
+
+
+/**
+ * 
+ *
+ * @author yen
+ * @email yen_dev@gmail.com
+ * @date 2023-02-04 10:20:55
+ */
+@RestController
+@RequestMapping("member/integrationchangehistory")
+public class IntegrationChangeHistoryController {
+    @Autowired
+    private IntegrationChangeHistoryService integrationChangeHistoryService;
+
+    /**
+     * 列表
+     */
+    @RequestMapping("/list")
+    //@RequiresPermissions("member:integrationchangehistory:list")
+    public R list(@RequestParam Map<String, Object> params){
+        PageUtils page = integrationChangeHistoryService.queryPage(params);
+
+        return R.ok().put("page", page);
+    }
+
+
+    /**
+     * 信息
+     */
+    @RequestMapping("/info/{id}")
+    //@RequiresPermissions("member:integrationchangehistory:info")
+    public R info(@PathVariable("id") Long id){
+		IntegrationChangeHistoryEntity integrationChangeHistory = integrationChangeHistoryService.getById(id);
+
+        return R.ok().put("integrationChangeHistory", integrationChangeHistory);
+    }
+
+    /**
+     * 保存
+     */
+    @RequestMapping("/save")
+    //@RequiresPermissions("member:integrationchangehistory:save")
+    public R save(@RequestBody IntegrationChangeHistoryEntity integrationChangeHistory){
+		integrationChangeHistoryService.save(integrationChangeHistory);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改
+     */
+    @RequestMapping("/update")
+    //@RequiresPermissions("member:integrationchangehistory:update")
+    public R update(@RequestBody IntegrationChangeHistoryEntity integrationChangeHistory){
+		integrationChangeHistoryService.updateById(integrationChangeHistory);
+
+        return R.ok();
+    }
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    //@RequiresPermissions("member:integrationchangehistory:delete")
+    public R delete(@RequestBody Long[] ids){
+		integrationChangeHistoryService.removeByIds(Arrays.asList(ids));
+
+        return R.ok();
+    }
+
+}
---
+++ springEcommerceGuli/backend/inverse_eng/renren_member/main/java/com/yen/gulimall/member/service/impl/MemberReceiveAddressServiceImpl.java
@@ -0,0 +1,29 @@
+package com.yen.gulimall.member.service.impl;
+
+import org.springframework.stereotype.Service;
+import java.util.Map;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.yen.common.utils.PageUtils;
+import com.yen.common.utils.Query;
+
+import com.yen.gulimall.member.dao.MemberReceiveAddressDao;
+import com.yen.gulimall.member.entity.MemberReceiveAddressEntity;
+import com.yen.gulimall.member.service.MemberReceiveAddressService;
+
+
+@Service("memberReceiveAddressService")
+public class MemberReceiveAddressServiceImpl extends ServiceImpl<MemberReceiveAddressDao, MemberReceiveAddressEntity> implements MemberReceiveAddressService {
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params) {
+        IPage<MemberReceiveAddressEntity> page = this.page(
+                new Query<MemberReceiveAddressEntity>().getPage(params),
+                new QueryWrapper<MemberReceiveAddressEntity>()
+        );
+
+        return new PageUtils(page);
+    }
+
+}
