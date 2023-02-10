312bafe2a1e7fd491b376f9f0956dd803d5d90b2
yennanliu <f339339@gmail.com>
2023-02-10 09:05:20 +0000

    add gulimall-geteway module

---
+++ springEcommerceGuli/backend/EcommerceGuli/gulimall-gateway/src/main/java/com/yen/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.yen.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallGatewayApplication.class, args);
+	}
+
+}
