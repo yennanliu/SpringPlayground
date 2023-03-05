e1a1b57f74fcc98443b347789c6c7602970df341
yennanliu <f339339@gmail.com>
2023-03-05 10:21:02 +0000

    add Mybatis paging config, update BrandServiceImpl for fuzzy matching

--- springEcommerceGuli/backend/EcommerceGuli/gulimall-product/src/main/java/com/yen/gulimall/product/service/impl/BrandServiceImpl.java
+++ springEcommerceGuli/backend/EcommerceGuli/gulimall-product/src/main/java/com/yen/gulimall/product/service/impl/BrandServiceImpl.java
@@ -1,5 +1,6 @@
 package com.yen.gulimall.product.service.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -18,6 +19,22 @@ public class BrandServiceImpl extends ServiceImpl<BrandDao, BrandEntity> impleme
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        /**
+         *  update :
+         *      - key SQL fuzzy matching
+         *     - https://youtu.be/dG2Bo8noDtY?t=222
+         */
+        // 0) init query wrapper
+        QueryWrapper<BrandEntity> queryWrapper = new QueryWrapper<>();
+        // 1) get key
+        String key = (String) params.get("key");
+        // 2) if key not null, then have to do "fuzzy matching"
+        if(!StringUtils.isNotEmpty(key)){
+            queryWrapper
+                    .eq("brand_id", key).or()
+                    .like("name", key);
+        }
+
         IPage<BrandEntity> page = this.page(
                 new Query<BrandEntity>().getPage(params),
                 new QueryWrapper<BrandEntity>()
