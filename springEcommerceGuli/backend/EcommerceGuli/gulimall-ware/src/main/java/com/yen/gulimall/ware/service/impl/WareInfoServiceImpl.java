f3949d6e93e03fc6e7c677559cc5bebbf634e557
yennanliu <f339339@gmail.com>
2023-04-16 22:21:54 +0100

    update query condition, add log level, add comment, fix layout

--- springEcommerceGuli/backend/EcommerceGuli/gulimall-ware/src/main/java/com/yen/gulimall/ware/service/impl/WareInfoServiceImpl.java
+++ springEcommerceGuli/backend/EcommerceGuli/gulimall-ware/src/main/java/com/yen/gulimall/ware/service/impl/WareInfoServiceImpl.java
@@ -1,5 +1,6 @@
 package com.yen.gulimall.ware.service.impl;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -9,7 +10,6 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 //import com.yen.common.utils.Query;
 import com.yen.gulimall.common.utils.PageUtils;
 import com.yen.gulimall.common.utils.Query;
-
 import com.yen.gulimall.ware.dao.WareInfoDao;
 import com.yen.gulimall.ware.entity.WareInfoEntity;
 import com.yen.gulimall.ware.service.WareInfoService;
@@ -18,11 +18,27 @@ import com.yen.gulimall.ware.service.WareInfoService;
 @Service("wareInfoService")
 public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity> implements WareInfoService {
 
+    /**
+     * Update:
+     *  - https://youtu.be/Wcnwj5eLMiY?t=319
+     */
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+
+        QueryWrapper<WareInfoEntity> wareInfoEntityQueryWrapper = new QueryWrapper<>();
+        String key = (String) params.get("key");
+
+        if(!StringUtils.isEmpty(key)){
+            // prepare SQL where condition
+            wareInfoEntityQueryWrapper.eq("id", key)
+                    .or().like("name", key)
+                    .or().like("address", key)
+                    .or().like("areacode", key);
+        }
+
         IPage<WareInfoEntity> page = this.page(
                 new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
+                wareInfoEntityQueryWrapper
         );
 
         return new PageUtils(page);
