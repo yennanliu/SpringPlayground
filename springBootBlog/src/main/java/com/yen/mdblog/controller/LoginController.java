e87e1b18480be1292cff4a5ad51fd994ef2dc3d3
yennanliu <f339339@gmail.com>
2023-04-21 00:31:16 +0100

    rename edit url, name, undo login in index html, undo login in LoginController

--- springBootBlog/src/main/java/com/yen/mdblog/controller/LoginController.java
+++ springBootBlog/src/main/java/com/yen/mdblog/controller/LoginController.java
@@ -16,7 +16,6 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import java.util.List;
 
 @Controller
@@ -26,49 +25,52 @@ public class LoginController {
     @Autowired
     PostRepository postRepository;
 
-    @GetMapping(value = {"/login"})
-    public String loginInit(Model model){
-        // return LoginRequest instance as placeholder, so login html can use it and pass var to login post method (as below)
-        model.addAttribute("LoginRequest", new LoginRequest());
-
-        return "login";
-    }
-
-    @RequestMapping(value="/login", method= RequestMethod.POST)
-    public String login(LoginRequest request, Model model){
-
-        log.info(">>> login start ...");
-        log.info(">>> loginRequest = " + request);
-
-        User user = new User();
-        user.setUserName(request.getUserName());
-        user.setPassWord(request.getPassWord());
-
-        // check login account, pwd // TODO : validate its from info from DB
-        if (StringUtils.hasLength(user.getUserName()) && "123".equals(user.getPassWord())){
-            //return "redirect:/login";
-
-            // add blogs for editing blogs at admin-age
-            // TODO : fix below
-            final int page = 0;
-            final int size = 100;
-            Pageable pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "DateTime"));
-            Page<Post> postsPage = postRepository.findAll(pageRequest);
-            List<Post> posts = postsPage.toList();
-
-            model.addAttribute("posts", posts);
-            model.addAttribute("LoginRequest", request);
-            return "login_success";
-        }else{
-            log.info(">>> login failed, plz try again ...");
-            return "redirect:/login";
-        }
-    }
+//    @GetMapping(value = {"/login"})
+//    public String loginInit(Model model){
+//        // return LoginRequest instance as placeholder, so login html can use it and pass var to login post method (as below)
+//        model.addAttribute("LoginRequest", new LoginRequest());
+//
+//        return "login";
+//    }
+//
+//    @RequestMapping(value="/login", method= RequestMethod.POST)
+//    public String login(LoginRequest request, Model model){
+//
+//        log.info(">>> login start ...");
+//        log.info(">>> loginRequest = " + request);
+//
+//        User user = new User();
+//        user.setUserName(request.getUserName());
+//        user.setPassWord(request.getPassWord());
+//
+//        // check login account, pwd // TODO : validate its from info from DB
+//        if (StringUtils.hasLength(user.getUserName()) && "123".equals(user.getPassWord())){
+//            //return "redirect:/login";
+//
+//            // add blogs for editing blogs at admin-age
+//            // TODO : fix below
+//            final int page = 0;
+//            final int size = 100;
+//            Pageable pageRequest = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "DateTime"));
+//            Page<Post> postsPage = postRepository.findAll(pageRequest);
+//            List<Post> posts = postsPage.toList();
+//
+//            model.addAttribute("posts", posts);
+//            model.addAttribute("LoginRequest", request);
+//            return "login_success";
+//        }else{
+//            log.info(">>> login failed, plz try again ...");
+//            return "redirect:/login";
+//        }
+//    }
 
     @GetMapping("/login_success")
     public String login_success(LoginRequest request){
 
+        // TODO: get this from request
+        request.setUserName("admin");
         log.info(">>> username = " + request.getUserName());
+
         return "login_success";
     }
 
