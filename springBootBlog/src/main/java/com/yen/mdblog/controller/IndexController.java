0dd14b33c6cd2ae646c970376029861c9a514849
yennanliu <f339339@gmail.com>
2023-05-03 09:57:16 +0100

    fix pom groupId, disable test, add indexController redirect to all posts, fix code format

---
+++ springBootBlog/src/main/java/com/yen/mdblog/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.yen.mdblog.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@Log4j2
+public class IndexController {
+
+    @GetMapping("/")
+    public ModelAndView index(ModelMap model){
+
+        model.addAttribute("attribute", "redirectWithXMLConfig");
+        //return "";
+        return new ModelAndView("redirect:/posts/all", model);
+    }
+
+}
