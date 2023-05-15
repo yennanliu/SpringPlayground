9ee8639da42a653a99a6e2aadd95d6846c68d846
yennanliu <f339339@gmail.com>
2023-05-15 22:47:15 +0100

    ignore vue src format check (.eslintignore), update category.vue

--- springEcommerceGuli/frontend/renren-fast-vue/src/views/modules/product/category.vue
+++ springEcommerceGuli/frontend/renren-fast-vue/src/views/modules/product/category.vue
@@ -4,7 +4,11 @@
 -->
 
 <template>
-<el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+  <el-tree
+    :data="data"
+    :props="defaultProps"
+    @node-click="handleNodeClick"
+  ></el-tree>
 </template>
 
 <script>
@@ -13,10 +17,77 @@ export default {
   props: {},
   computed: {},
   watch: {},
-  methods: {}
-}
+  data() {
+    return {
+      data: [
+        {
+          label: "一级 1",
+          children: [
+            {
+              label: "二级 1-1",
+              children: [
+                {
+                  label: "三级 1-1-1",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: "一级 2",
+          children: [
+            {
+              label: "二级 2-1",
+              children: [
+                {
+                  label: "三级 2-1-1",
+                },
+              ],
+            },
+            {
+              label: "二级 2-2",
+              children: [
+                {
+                  label: "三级 2-2-1",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: "一级 3",
+          children: [
+            {
+              label: "二级 3-1",
+              children: [
+                {
+                  label: "三级 3-1-1",
+                },
+              ],
+            },
+            {
+              label: "二级 3-2",
+              children: [
+                {
+                  label: "三级 3-2-1",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      defaultProps: {
+        children: "children",
+        label: "label",
+      },
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
