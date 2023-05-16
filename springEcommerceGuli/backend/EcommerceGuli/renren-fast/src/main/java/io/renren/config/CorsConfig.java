7f4e7e0aeb05d7a4e3c2b6a3af093c6f588caa61
yennanliu <f339339@gmail.com>
2023-05-16 08:48:09 +0100

    remove cors in renren-fast, add code comment, make admin login work

--- springEcommerceGuli/backend/EcommerceGuli/renren-fast/src/main/java/io/renren/config/CorsConfig.java
+++ springEcommerceGuli/backend/EcommerceGuli/renren-fast/src/main/java/io/renren/config/CorsConfig.java
@@ -15,12 +15,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-            //.allowedOriginPatterns("*") // TODO: fix this (enable this)
-            .allowCredentials(true)
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-            .maxAge(3600);
-    }
+    /**
+     *  ONLY setup CORS in API GATEWAY
+     *      - https://youtu.be/VNP6inKmw5I?t=949
+     *
+     *      - comment such setting in renren-fast service
+     *      - to avoid below error in browser (multiple source value is not allowed)
+     *      - Access to XMLHttpRequest at 'http://localhost:88/api/sys/login' from origin 'http://localhost:8001' has been blocked by CORS policy: The 'Access-Control-Allow-Origin' header contains multiple values 'http://localhost:8001, http://localhost:8001', but only one is allowed.
+     */
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//            //.allowedOriginPatterns("*") // TODO: fix this (enable this)
+//            .allowCredentials(true)
+//            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//            .maxAge(3600);
+//    }
+
 }
