991a37b53b65f66b3c06ae8c1950647ef3f6e8b9
yennanliu <f339339@gmail.com>
2023-05-20 09:43:59 +0100

    add append, delete span, function

--- springEcommerceGuli/frontend/renren-fast-vue/src/views/modules/product/category.vue
+++ springEcommerceGuli/frontend/renren-fast-vue/src/views/modules/product/category.vue
@@ -4,11 +4,25 @@
 -->
 
 <template>
-  <el-tree
-    :data="menus"
-    :props="defaultProps"
-    @node-click="handleNodeClick"
-  ></el-tree>
+  <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick">
+    <!--
+      Append, delete button
+
+      https://youtu.be/-2S3c3Sh-H4?t=78
+      https://element.eleme.io/#/zh-CN/component/tree
+    -->
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+      <span>{{ node.label }}</span>
+      <span>
+        <el-button type="text" size="mini" @click="() => append(data)">
+          Append
+        </el-button>
+        <el-button type="text" size="mini" @click="() => remove(node, data)">
+          Delete
+        </el-button>
+      </span>
+    </span>
+  </el-tree>
 </template>
 
 <script>
@@ -30,12 +44,13 @@ export default {
     handleNodeClick(data) {
       console.log(data);
     },
+
     // https://youtu.be/QngyGaQXxz4?t=560
-    getMenu(){
-        //consloe.log(data);
-        this.$http({
-          url: this.$http.adornUrl('/product/category/list/tree'),
-          method: 'get',
+    getMenu() {
+      //consloe.log(data);
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get",
         //   params: this.$http.adornParams({
         //     'page': this.pageIndex,
         //     'limit': this.pageSize,
@@ -43,15 +58,33 @@ export default {
         //   })
 
         // ({data}) : can get attr in data, so we can call data.data below : https://youtu.be/KKFJPtW3730?t=588
-        }).then(({data}) => {
-            console.log("get data success : ", data.data)
-            this.menus = data.data
-        })
-    }
+      }).then(({ data }) => {
+        console.log("get data success : ", data.data);
+        this.menus = data.data;
+      });
+    },
   },
-  created(){
+
+  created() {
     this.getMenu();
-  }
+  },
+
+  // https://youtu.be/-2S3c3Sh-H4?t=138
+  // https://element.eleme.io/#/zh-CN/component/tree
+  append(data) {
+    const newChild = { id: id++, label: "testtest", children: [] };
+    if (!data.children) {
+      this.$set(data, "children", []);
+    }
+    data.children.push(newChild);
+  },
+
+  remove(node, data) {
+    const parent = node.parent;
+    const children = parent.data.children || parent.data;
+    const index = children.findIndex((d) => d.id === data.id);
+    children.splice(index, 1);
+  },
 };
 </script>
 
