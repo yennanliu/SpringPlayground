7c78ed69ad2a6862cc67930eca0f07b34643bc36
yennanliu <f339339@gmail.com>
2023-05-21 11:49:43 +0100

    make dialogue dynamic

--- springEcommerceGuli/frontend/renren-fast-vue/src/views/modules/product/category.vue
+++ springEcommerceGuli/frontend/renren-fast-vue/src/views/modules/product/category.vue
@@ -30,6 +30,15 @@
           >
             Append
           </el-button>
+
+          <!-- 
+            Edit
+            https://youtu.be/heF-gu9EXDs?t=43
+          -->
+          <el-button type="text" size="mini" @click="() => edit(data)">
+            Edit
+          </el-button>
+
           <el-button
             v-if="node.childNodes.length == 0"
             type="text"
@@ -45,8 +54,12 @@
     <!--
       dialogue
       https://youtu.be/KKv81DvbbMQ?t=105 
+
+      :title="提示"
+      bind title with variable
+      https://youtu.be/heF-gu9EXDs?t=441
     -->
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+    <el-dialog v-bind:title="title" :visible.sync="dialogVisible" width="30%">
       <el-form :model="category">
         <el-form-item label="分類名稱">
           <el-input v-model="category.name" autocomplete="off"></el-input>
@@ -56,13 +69,15 @@
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
         <!-- https://youtu.be/KKv81DvbbMQ?t=356 -->
-        <el-button type="primary" @click="addCategory">确 定</el-button>
+        <el-button type="primary" @click="submitData">确 定</el-button>
       </span>
     </el-dialog>
   </div>
 </template>
 
 <script>
+import { timeStamp } from "console";
+
 export default {
   components: {},
   props: {},
@@ -70,7 +85,16 @@ export default {
   watch: {},
   data() {
     return {
-      category: { name: "", parentCid: 0, catLevel: 0, showStatus: 1, sort: 0 },
+      title: "",
+      dialogType: "", // edit, append ...
+      category: {
+        name: "",
+        parentCid: 0,
+        catLevel: 0,
+        showStatus: 1,
+        sort: 0,
+        catId: null,
+      },
       dialogVisible: false,
       menus: [],
       expandedKey: [],
@@ -105,7 +129,8 @@ export default {
     // https://youtu.be/KKv81DvbbMQ?t=140
     append(data) {
       console.log("append : ", data);
-      this.dialogVisible = true;
+      this.dialogType = "append";
+      (this.title = "Add category"), (this.dialogVisible = true);
       this.category.parentCid = data.catId;
       this.category.catLevel = data.catLevel * 1 + 1; // transform string to int : data.catLevel * 1
     },
@@ -132,6 +157,30 @@ export default {
         // expand deleted node's parent as new expanded node
         this.expandedKey = [this.category.parentCid]; // https://youtu.be/KKv81DvbbMQ?t=880
       });
+    },
+
+    // Edit category method
+    // https://youtu.be/heF-gu9EXDs?t=66
+    edit(data) {
+      console.log("to edit : ", data);
+      this.dialogType = "edit";
+      (this.title = "Edit category"), (this.dialogVisible = true);
+      this.category.name = data.name;
+      this.category.catId = data.catId;
+    },
+
+    // modify category data
+    editCategory() {},
+
+    // method handles which dialogue to submit
+    // https://youtu.be/heF-gu9EXDs?t=338
+    submitData() {
+      if (this.dialogType == "add") {
+        this.addCategory();
+      }
+      if (this.dialogType == "edit") {
+        this.editCategory();
+      }
     },
 
     remove(node, data) {
