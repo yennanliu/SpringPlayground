1c2c4b6a07cd08993f2aecc7ac752d4473d60430
yennanliu <f339339@gmail.com>
2023-05-27 11:28:01 +0100

    always return true from isAuth

--- springEcommerceGuli/frontend/renren-fast-vue/src/utils/index.js
+++ springEcommerceGuli/frontend/renren-fast-vue/src/utils/index.js
@@ -16,7 +16,10 @@ export function getUUID () {
  * @param {*} key
  */
 export function isAuth (key) {
-  return JSON.parse(sessionStorage.getItem('permissions') || '[]').indexOf(key) !== -1 || false
+  // always show button
+  // https://youtu.be/PGAg72V1xuM?t=290
+  //return JSON.parse(sessionStorage.getItem('permissions') || '[]').indexOf(key) !== -1 || false
+  return true;
 }
 
 /**
