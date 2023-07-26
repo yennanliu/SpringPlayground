a89d1d7ce634d7126e34dd0bb8aaf3aec115d6b7
yennanliu <f339339@gmail.com>
2023-07-26 20:23:04 +0100

    add in memory user manager

--- springBootBlog/src/main/java/com/yen/mdblog/config/OAuthSecurityConfig.java
+++ springBootBlog/src/main/java/com/yen/mdblog/config/OAuthSecurityConfig.java
@@ -7,11 +7,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class OAuthSecurityConfig {
+
+    // in memory user
+    // https://www.youtube.com/watch?v=66DtzkhBlSA&t=515s
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new InMemoryUserDetailsManager(
+                User.withUsername("admin")
+                        .password("{noop}123")
+                        .authorities("READ", "ROLE_USER")
+                        .build());
+    }
 
     // social login : https://youtu.be/us0VjFiHogo?t=241
     @Bean
