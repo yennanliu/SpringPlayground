645aa00a023d3a2a0252f8541712405d33fec103
yennanliu <f339339@gmail.com>
2023-07-29 15:19:42 +0100

    add

--- springBootReddit/README.md
+++
@@ -1,6 +0,0 @@
-# springBootReddit
-
-## Ref
-- https://github.com/SaiUpadhyayula/angular-reddit-clone
-- https://github.com/SaiUpadhyayula/spring-reddit-clone
-- https://www.youtube.com/watch?v=7PsjGI-88sc&list=PLSVW22jAG8pAGrwFjsUERCu9WSo2-uEMg&index=1
---
+++ springReddit/backend/SpringReddit/src/main/java/com/yen/SpringReddit/SpringRedditApplication.java
@@ -0,0 +1,13 @@
+package com.yen.SpringReddit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedditApplication.class, args);
+	}
+
+}
