29880c89077142df9bd9351eecaf297e7fe1f2e3
yennanliu <f339339@gmail.com>
2023-08-13 17:26:20 +0100

    add dep, refine JwtProvider.java, add JwtAuthenticationFilter.java

---
+++ springReddit/backend/SpringReddit/src/main/java/com/yen/SpringReddit/security/JwtAuthenticationFilter.java
@@ -0,0 +1,49 @@
+package com.yen.SpringReddit.security;
+
+// https://youtu.be/PN3gxkub4Ew
+// https://github.com/SaiUpadhyayula/spring-reddit-clone/blob/revert-16-patch-1/src/main/java/com/programming/techie/springredditclone/security/JwtAuthenticationFilter.java
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+
+    @Autowired
+    private JwtProvider jwtProvider;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String jwt = getJwtFromRequest(request);
+
+        //if(StringUtils.hasText(jwt) && jwtProvider.v)
+    }
+
+
+
+    private String getJwtFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return bearerToken;
+    }
+}
