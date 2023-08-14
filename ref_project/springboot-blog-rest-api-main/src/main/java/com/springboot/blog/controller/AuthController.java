d63bff4efd67a51f2eb8662ab27193040ae19421
yennanliu <f339339@gmail.com>
2023-08-14 08:37:44 +0100

    add ref project

---
+++ ref_project/springboot-blog-rest-api-main/src/main/java/com/springboot/blog/controller/AuthController.java
@@ -0,0 +1,41 @@
+package com.springboot.blog.controller;
+
+import com.springboot.blog.payload.JWTAuthResponse;
+import com.springboot.blog.payload.LoginDto;
+import com.springboot.blog.payload.RegisterDto;
+import com.springboot.blog.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    // Build Login REST API
+    @PostMapping(value = {"/login", "/signin"})
+    public ResponseEntity<JWTAuthResponse> login(@RequestBody LoginDto loginDto){
+        String token = authService.login(loginDto);
+
+        JWTAuthResponse jwtAuthResponse = new JWTAuthResponse();
+        jwtAuthResponse.setAccessToken(token);
+
+        return ResponseEntity.ok(jwtAuthResponse);
+    }
+
+    // Build Register REST API
+    @PostMapping(value = {"/register", "/signup"})
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto){
+        String response = authService.register(registerDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+}
