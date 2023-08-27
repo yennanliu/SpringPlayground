a8d83665973bfc6e4a68bd99d2d2116ea87882e4
yennanliu <f339339@gmail.com>
2023-08-27 10:39:23 +0100

    init springAssignmentSystem/backend/

---
+++ springAssignmentSystem/backend/SpringAssignmentSystem/src/main/java/com/yen/SpringAssignmentSystem/SpringAssignmentSystemApplication.java
@@ -0,0 +1,14 @@
+package com.yen.SpringAssignmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAssignmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(
+				SpringAssignmentSystemApplication.class, args);
+	}
+
+}
