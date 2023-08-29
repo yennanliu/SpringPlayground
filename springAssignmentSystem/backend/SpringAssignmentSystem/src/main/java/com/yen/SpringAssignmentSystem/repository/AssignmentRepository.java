b835924bd0fb5331588e3887fb4329d6a88f608a
yennanliu <f339339@gmail.com>
2023-08-29 19:39:27 +0100

    fix sql, syntax in repository

--- springAssignmentSystem/backend/SpringAssignmentSystem/src/main/java/com/yen/SpringAssignmentSystem/repository/AssignmentRepository.java
+++ springAssignmentSystem/backend/SpringAssignmentSystem/src/main/java/com/yen/SpringAssignmentSystem/repository/AssignmentRepository.java
@@ -1,26 +1,24 @@
-//package com.yen.SpringAssignmentSystem.repository;
-//
-//import com.yen.SpringAssignmentSystem.domain.Assignment;
-//import com.yen.SpringAssignmentSystem.domain.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.data.jpa.repository.Query;
-//
-//import java.util.List;
-//import java.util.Set;
-//
-//public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
-//
-//    // TODO : check is this necessary ?
-//    Set<Assignment> findByUser(User user);
-//
-//    @Query("select a from Assignment a "
-//            + "where ((a.status = 'submitted' or a.status = 'resubmitted') and (a.codeReviewer is null or a.codeReviewer = :codeReviewer))"
-//            + "or a.codeReviewer = :codeReviewer")
-//    Set<Assignment> findByCodeReviewer(User codeReviewer);
-//
-//    @Query("select a from Assignment a " +
-//            "join fetch a.user u " +
-//            "where u.cohortStartDate is not null and u.bootcampDurationInWeeks is not null")
-//    List<Assignment> findAllActiveBootcampStudents();
-//
-//}
+package com.yen.SpringAssignmentSystem.repository;
+
+import com.yen.SpringAssignmentSystem.domain.Assignment;
+import com.yen.SpringAssignmentSystem.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Set;
+
+public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
+
+    // TODO : check is this necessary ?
+    Set<Assignment> findByUser(User user);
+
+    @Query(value = "select a.* from assignment a where (a.status = 'submitted' or a.status = 'resubmitted')",
+            nativeQuery = true)
+    Set<Assignment> findByCodeReviewer(User codeReviewer);
+
+    @Query(value = "select a.* from assignment a join users u on a.user_id = u.id where u.cohort_start_date is not null",
+            nativeQuery = true)
+    List<Assignment> findAllActiveBootcampStudents();
+
+}
--- springAssignmentSystem/backend/SpringAssignmentSystem/src/main/java/com/yen/SpringAssignmentSystem/repository/CommentRepository.java
+++ springAssignmentSystem/backend/SpringAssignmentSystem/src/main/java/com/yen/SpringAssignmentSystem/repository/CommentRepository.java
@@ -3,6 +3,7 @@ package com.yen.SpringAssignmentSystem.repository;
 import com.yen.SpringAssignmentSystem.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Set;
 
@@ -10,6 +11,6 @@ public interface CommentRepository extends JpaRepository<Comment, Long>{
 
     @Query("select c from Comment c "
             + " where c.assignment.id = :assignmentId")
-    Set<Comment> findByAssignmentId(Long assignmentId);
+    Set<Comment> findByAssignmentId(@Param("assignmentId") Long assignmentId);
 
 }
