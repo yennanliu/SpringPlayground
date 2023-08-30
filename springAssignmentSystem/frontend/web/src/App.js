1451ff585a457567c6de9a6f1c07b8521e9885c0
yennanliu <f339339@gmail.com>
2023-08-30 10:32:47 +0100

    add useEffect, useState

--- springAssignmentSystem/frontend/web/src/App.js
+++ springAssignmentSystem/frontend/web/src/App.js
@@ -1,42 +1,70 @@
 //import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-/** MAIN APP (entry point) */
+/**
+ *
+ * MAIN APP (entry point)
+ *
+ *  https://github.com/tp02ga/AssignmentSubmissionApp/blob/master/front-end/src/App.js
+ */
 
 function App() {
 
-  console.log("APP start!")
-
-  const reqBody = {
-    "username": "admin",
-    "password": "123"
-}
+  console.log("APP start!");
 
   /** 
-   * Fetch : call backend/other api, then go to next line of code, will not freeze and only operate once receive result
-   * https://youtu.be/N1QStjH1rVI?si=ozUe3M3tx6z3i-T1&t=1256 
+   *  https://youtu.be/pupnAIRpbKo?si=O-NSykY6ChvYenb7&t=629
+   *  
+   *  useState can save state (val) in the scope of function
+   * 
    */
-  fetch("api/auth/login", {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method: "post",
-    body: JSON.stringify(reqBody)
-  })
-    // .then((response) => response.json)
-    // .then((data) => console.log(data))
-    .then((response) => Promise.all([response.json(), response.headers]))
-    .then(([data, headers]) => {
+  const[getJwt, setJwt] = useState("") // getter, setter
 
-      headers.forEach((element) => {
-        console.log(element)
-      })
+  /** 
+   *  https://youtu.be/pupnAIRpbKo?si=ne6sMKKJR_cR3gds&t=391
+   *  
+   *  useEffect( () => function}, array)
+   * 
+   */
+  useEffect(() => {
+    const reqBody = {
+      username: "admin",
+      password: "123",
+    };
 
+    /**
+     * Fetch : call backend/other api, then go to next line of code, will not freeze and only operate once receive result
+     * https://youtu.be/N1QStjH1rVI?si=ozUe3M3tx6z3i-T1&t=1256
+     */
+    fetch("api/auth/login", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "post",
+      body: JSON.stringify(reqBody),
     })
+      // .then((response) => response.json)
+      // .then((data) => console.log(data))
+      .then((response) => Promise.all([response.json(), response.headers]))
+      .then(([body, headers]) => {
+        // headers.forEach((element) => {
+        //   console.log(element);
+        // });
+
+        // jwt : json web token
+        //const jwt = headers.get("authorization")
+        setJwt(headers.get("authorization"))
+        //console.log(jwt)
+        console.log(getJwt)
+        console.log(body)
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1>HELLO WORLD</h1>
+      <div>JWT value is {getJwt}</div>
     </div>
   );
 }
