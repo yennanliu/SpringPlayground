305c9ca46233963db05fb7361ec24672bb97dec5
yennanliu <f339339@gmail.com>
2023-08-31 11:54:01 +0100

    update

--- springAssignmentSystem/frontend/frontend_app/src/Login/index.js
+++ springAssignmentSystem/frontend/frontend_app/src/Login/index.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import { useLocalState } from "../util/useLocalStorage";
 
 // https://youtu.be/YYDpGYOjfqM?si=KndWtVvDf0dtxBBS&t=161
 // https://youtu.be/6qJ8JXWC9IA?si=Dv8hRVq4jVtHc34j&t=118
 
 const Login = () => {
+
+  // binding value with useState (init with null), so once val is updated, React will automatically update element as well  
+  // https://youtu.be/6qJ8JXWC9IA?si=Az2koaES1S5aCnVN&t=460
+  const[username, setUsername] = useState("")
+  const[password, setPassword] = useState("")
+
+
   const [getJwt, setJwt] = useLocalState("", "jwt"); // getter, setter
 
   function sendLoginRequest() {
@@ -44,11 +51,11 @@ const Login = () => {
     <>
       <div>
         <label htmlFor="username">Username</label>
-        <input type="email" id="username"></input>
+        <input type="email" id="username" value={username}></input> {/** set username equals as input */}
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password"></input>
+        <input type="password" id="password" value={password}></input>  {/** set password equals as input */}
       </div>
       <div>
         <button id="submit" type="button" onClick={() => sendLoginRequest()}>
