a1b0ef236976a8e3b49b4b8bf8e2b25a93bbda0b
yennanliu <f339339@gmail.com>
2023-08-31 10:52:34 +0100

    add login view

--- springAssignmentSystem/frontend/frontend_app/src/Login/index.js
+++ springAssignmentSystem/frontend/frontend_app/src/Login/index.js
@@ -1,8 +1,44 @@
 import React from "react";
+import { useLocalState } from "../util/useLocalStorage";
 
 // https://youtu.be/YYDpGYOjfqM?si=KndWtVvDf0dtxBBS&t=161
+// https://youtu.be/6qJ8JXWC9IA?si=Dv8hRVq4jVtHc34j&t=118
 
 const Login = () => {
+  const [getJwt, setJwt] = useLocalState("", "jwt"); // getter, setter
+
+  function sendLoginRequest() {
+    console.log(">>> sendLoginRequest start");
+
+    const reqBody = {
+      username: "admin",
+      password: "123",
+    };
+
+    fetch("api/auth/login", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "post",
+      body: JSON.stringify(reqBody),
+    })
+      // .then((response) => response.json)
+      // .then((data) => console.log(data))
+      .then((response) => Promise.all([response.json(), response.headers]))
+      .then(([body, headers]) => {
+        // headers.forEach((element) => {
+        //   console.log(element);
+        // });
+
+        // jwt : json web token
+        //const jwt = headers.get("authorization")
+        setJwt(headers.get("authorization"));
+        //console.log(jwt)
+        console.log(getJwt);
+        console.log(body);
+      });
+  }
+
   return (
     // <></> : React fragment
     <>
@@ -13,6 +49,11 @@ const Login = () => {
       <div>
         <label htmlFor="password">Password</label>
         <input type="password" id="password"></input>
+      </div>
+      <div>
+        <button id="submit" type="button" onClick={() => sendLoginRequest()}>
+          Login
+        </button>
       </div>
     </>
   );
