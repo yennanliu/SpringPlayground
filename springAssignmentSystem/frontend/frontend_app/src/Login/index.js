dae3eeac118883f73f362a2c378166249dba7df8
yennanliu <f339339@gmail.com>
2023-09-02 09:55:59 +0100

    add username, password from event.target.val

--- springAssignmentSystem/frontend/frontend_app/src/Login/index.js
+++ springAssignmentSystem/frontend/frontend_app/src/Login/index.js
@@ -1,18 +1,19 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useLocalState } from "../util/useLocalStorage";
 
 // https://youtu.be/YYDpGYOjfqM?si=KndWtVvDf0dtxBBS&t=161
 // https://youtu.be/6qJ8JXWC9IA?si=Dv8hRVq4jVtHc34j&t=118
 
 const Login = () => {
-
-  // binding value with useState (init with null), so once val is updated, React will automatically update element as well  
+  // binding value with useState (init with null), so once val is updated, React will automatically update element as well
   // https://youtu.be/6qJ8JXWC9IA?si=Az2koaES1S5aCnVN&t=460
-  const[username, setUsername] = useState("")
-  const[password, setPassword] = useState("")
-
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   const [getJwt, setJwt] = useLocalState("", "jwt"); // getter, setter
+
+  console.log(">>> username = " + username);
+  console.log(">>> password = " + password);
 
   function sendLoginRequest() {
     console.log(">>> sendLoginRequest start");
@@ -51,11 +52,23 @@ const Login = () => {
     <>
       <div>
         <label htmlFor="username">Username</label>
-        <input type="email" id="username" value={username}></input> {/** set username equals as input */}
+        <input
+          type="email"
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        ></input>{" "}
+        {/** set username equals as input */}
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" value={password}></input>  {/** set password equals as input */}
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        ></input>{" "}
+        {/** set password equals as input */}
       </div>
       <div>
         <button id="submit" type="button" onClick={() => sendLoginRequest()}>
