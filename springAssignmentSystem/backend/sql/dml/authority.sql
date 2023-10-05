8d8bab8b21386c326c29974820bdd766c2f845a1
yennanliu <f339339@gmail.com>
2023-10-05 19:21:58 +0800

    update sql, progress

--- springAssignmentSystem/backend/sql/dml/authority.sql
+++ springAssignmentSystem/backend/sql/dml/authority.sql
@@ -3,8 +3,10 @@
 
 /** users */
 INSERT INTO users(password, username, email, name)
-VALUES( '123', 'admin', '', 'admin');
+VALUES( '123', 'admin', '', 'admin'),
+VALUES( '123', 'code_reviewer', '', 'code_reviewer');
 
 /** authority */
 INSERT INTO authority(authority, user_id)
-VALUES ('ROLE_STUDENT', 1);
+VALUES ('ROLE_STUDENT', 1),
+VALUES ('CODE_REVIEWER', 2);
