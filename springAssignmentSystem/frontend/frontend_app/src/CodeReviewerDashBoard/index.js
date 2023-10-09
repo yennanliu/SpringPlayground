75d08751ccfc840410d368fbc956f2ec3eef796b
yennanliu <f339339@gmail.com>
2023-10-09 17:49:40 +0800

    add Needs Update section in CodeReviewerDashBoard view

--- springAssignmentSystem/frontend/frontend_app/src/CodeReviewerDashBoard/index.js
+++ springAssignmentSystem/frontend/frontend_app/src/CodeReviewerDashBoard/index.js
@@ -213,7 +213,73 @@ const CodeReviewerDashboard = () => {
         </Card>
       </div>
 
-      {/* <div className="assignment-wrapper needs-update"></div> */}
+      {/**
+       *
+       *  Needs Update
+       */}
+
+      <div className="assignment-wrapper needs-update">
+        <div className="assignment-wrapper-title h3 mt-5">Needs Update</div>
+        {assignments &&
+          assignments
+            .filter((assignment) => assignment.status === "Needs Update")
+            .map((assignment) => (
+              // <Col>
+              <Card
+                key={assignment.id}
+                style={{ width: "18rem", height: "20rem" }}
+              >
+                <Card.Body className="d-flex flex-column justify-content-around">
+                  <Card.Title>Assignment #{assignment.number}</Card.Title>
+
+                  <div className="d-flex align-items-start">
+                    <StatusBadge text={assignment.status} />
+                  </div>
+
+                  <Card.Text style={{ marginTop: "1em" }}>
+                    <b>GitHub URL</b>: {assignment.githubUrl}
+                    <br />
+                    <b>Branch</b>: {assignment.branch}
+                  </Card.Text>
+
+                  {assignment && assignment.status === "Completed" ? (
+                    <>
+                      <Button
+                        onClick={() => {
+                          window.open(assignment.codeReviewVideoUrl);
+                        }}
+                        className="mb-4"
+                      >
+                        Watch Review
+                      </Button>
+                      <Button
+                        variant="secondary"
+                        onClick={() => {
+                          navigate(`/assignments/${assignment.id}`);
+                        }}
+                      >
+                        View
+                      </Button>
+                    </>
+                  ) : (
+                    <Button
+                      variant="secondary"
+                      onClick={() => {
+                        //navigate(`/assignments/${assignment.id}`);
+                        claimAssignment(assignment);
+                      }}
+                    >
+                      Claim
+                    </Button>
+                  )}
+                </Card.Body>
+              </Card>
+              // </Col>
+            ))}
+        <Card style={{ width: "18rem", height: "20rem" }}>
+          <Card.Body className="d-flex flex-column justify-content-around"></Card.Body>
+        </Card>
+      </div>
     </Container>
   );
 };
