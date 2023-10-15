c1f088b3e178f1f0db9ddf8bc206a01ba229a678
yennanliu <f339339@gmail.com>
2023-10-15 16:16:14 +0800

    fix product input status, show status, add enum, fix ddl

--- springWarehouse/sql/ddl/product.sql
+++ springWarehouse/sql/ddl/product.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS product(
     type_id int not null,
     price int not null,
     merchant_id varchar(100),
-    product_status varchar(1) not null,
+    product_status  varchar(10),
     amount int DEFAULT 0,
     constraint FK_PRODUCT_TYPE FOREIGN KEY (type_id) references product_type(type_id)
 );
@@ -17,6 +17,6 @@ CREATE TABLE IF NOT EXISTS product(
 -- DML
 INSERT INTO product(name, type_id, price, merchant_id, amount, product_status)
 VALUES
-("i-phone", 2, 300, 1, 3, "0"),
-("cookie2", 4, 10, 2, 2, "0"),
-("soap", 1, 50, 1, 1, "0");
+("i-phone", 2, 300, 1, 3, "on_board"),
+("cookie2", 4, 10, 2, 2, "new"),
+("soap", 1, 50, 1, 1, "off_board");
