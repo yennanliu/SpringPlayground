c6b33c4fdb23ed82e88b3f866e4ef967aa6b8aab
yennanliu <f339339@gmail.com>
2023-11-04 16:25:13 +0800

    add chatRoom src code, update readme

---
+++ springChatRoom/src/test/java/com/yen/springChatRoom/SpringChatRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.springChatRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
