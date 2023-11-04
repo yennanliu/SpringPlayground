24bc16ec4017c21b8e4d6fb034738fe7db1ac43a
yennanliu <f339339@gmail.com>
2023-11-04 16:55:28 +0800

    update main app name

--- springChatRoom/src/main/java/com/yen/springChatRoom/ChatRoomApplication.java
+++ springChatRoom/src/main/java/com/yen/springChatRoom/ChatRoomApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringChatRoomApplication {
+public class ChatRoomApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(SpringChatRoomApplication.class, args);
+		SpringApplication.run(ChatRoomApplication.class, args);
 	}
 
 }
