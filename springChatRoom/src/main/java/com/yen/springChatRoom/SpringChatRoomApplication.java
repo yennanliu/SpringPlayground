b66a0ae714b7e50390abde777a50485594315543
yennanliu <f339339@gmail.com>
2023-11-04 16:25:13 +0800

    add chatRoom src code, update readme

---
+++ springChatRoom/src/main/java/com/yen/springChatRoom/SpringChatRoomApplication.java
@@ -0,0 +1,14 @@
+package com.yen.springChatRoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatRoomApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringChatRoomApplication.class, args);
+	}
+
+}
