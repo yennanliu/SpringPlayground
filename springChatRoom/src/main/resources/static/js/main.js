35f8cf9cd48eeafd3d42bdd5a149ec743341e67d
yennanliu <f339339@gmail.com>
2023-11-07 16:14:31 +0800

    print error in js console

--- springChatRoom/src/main/resources/static/js/main.js
+++ springChatRoom/src/main/resources/static/js/main.js
@@ -51,6 +51,7 @@ function onConnected() {
 
 
 function onError(error) {
+    console.log(">>> error = " + error)
     connectingElement.textContent = 'Could not connect to WebSocket server. Please refresh this page to try again!';
     connectingElement.style.color = 'red';
 }
