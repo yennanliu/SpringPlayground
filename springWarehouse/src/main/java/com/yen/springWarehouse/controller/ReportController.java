e5745c4aed2b330e9e26dcd7b120cba9ba6caecf
yennanliu <f339339@gmail.com>
2023-11-07 23:10:17 +0800

    add reportController, html, update url

--- springWarehouse/src/main/java/com/yen/springWarehouse/controller/ReportController.java
+++ springWarehouse/src/main/java/com/yen/springWarehouse/controller/ReportController.java
@@ -1,4 +1,23 @@
 package com.yen.springWarehouse.controller;
 
+import com.yen.springWarehouse.bean.Merchant;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Slf4j
+@Controller
+@RequestMapping("/report")
 public class ReportController {
+
+    @GetMapping("/report_page")
+    public String getReport(Map<String, Object> map) {
+
+        //map.put("Merchant", new Merchant());
+        return "report/report";
+    }
+
 }
