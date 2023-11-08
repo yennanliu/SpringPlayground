921ac47aa760e78ea15cdf2f9a7b4b1c91ea742a
yennanliu <f339339@gmail.com>
2023-11-08 17:03:16 +0800

    fix service name, update download html, controller return val

--- springWarehouse/src/main/java/com/yen/springWarehouse/controller/DownloadController.java
+++ springWarehouse/src/main/java/com/yen/springWarehouse/controller/DownloadController.java
@@ -1,9 +1,17 @@
 package com.yen.springWarehouse.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yen.springWarehouse.bean.DownloadStatus;
+import com.yen.springWarehouse.service.DownloadStatusService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
@@ -12,11 +20,31 @@ import java.util.Map;
 @RequestMapping("/download")
 public class DownloadController {
 
-    @GetMapping("/list")
-    public String getReport(Map<String, Object> map) {
+    @Autowired
+    DownloadStatusService downloadStatusService;
 
-        //map.put("Merchant", new Merchant());
+    @GetMapping("/list")
+    public String list(Map<String, Object> map, @RequestParam(value="pageNo", required=false, defaultValue="1") String pageNoStr) {
+
+        int pageNo;
+        // check pageNo
+        pageNo = Integer.parseInt(pageNoStr);
+        if(pageNo < 1){
+            pageNo = 1;
+        }
+        log.info("pageNo = {}", pageNo);
+        Page<DownloadStatus> page = new Page<>(pageNo,3);
+        QueryWrapper<DownloadStatus> queryWrapper = new QueryWrapper<>();
+        IPage<DownloadStatus> iPage = downloadStatusService.page(page,
+                new LambdaQueryWrapper<DownloadStatus>()
+                        .orderByAsc(DownloadStatus::getId)
+        );
+
+        log.info("iPage.total = {}, iPage.getPages = {} iPage = {}",  iPage.getTotal(), iPage.getPages(), iPage);
+        map.put("page", iPage);
+
         return "download/list_download";
     }
+
 
 }
