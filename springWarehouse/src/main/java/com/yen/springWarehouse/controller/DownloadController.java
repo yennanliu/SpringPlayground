fcea668adfe2c63b22c24bbf618ffb2f3ece34ea
yennanliu <f339339@gmail.com>
2023-11-08 16:40:47 +0800

    fix url, controller endpoint, path

--- springWarehouse/src/main/java/com/yen/springWarehouse/controller/DownloadController.java
+++ springWarehouse/src/main/java/com/yen/springWarehouse/controller/DownloadController.java
@@ -1,6 +1,5 @@
 package com.yen.springWarehouse.controller;
 
-import com.yen.springWarehouse.bean.Merchant;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +9,14 @@ import java.util.Map;
 
 @Slf4j
 @Controller
-@RequestMapping("/report")
-public class ReportController {
+@RequestMapping("/download")
+public class DownloadController {
 
-    @GetMapping("/report_page")
+    @GetMapping("/list")
     public String getReport(Map<String, Object> map) {
 
         //map.put("Merchant", new Merchant());
-        return "report/report";
+        return "download/list_download";
     }
 
 }
