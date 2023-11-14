02d86235a1902b740673ec2da1dfad17c43a7bfc
yennanliu <f339339@gmail.com>
2023-11-14 17:35:57 +0800

    add error html, error config, controller, setting

---
+++ springBootBlog/src/main/java/com/yen/mdblog/controller/CustomErrorController.java
@@ -0,0 +1,22 @@
+package com.yen.mdblog.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class CustomErrorController implements ErrorController{
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @RequestMapping("/error")
+    public String handleError(){
+
+        // Provide logic to determine the error status and redirect accordingly
+        // For example, you can use the HttpServletResponse.getStatus() method
+        return "error"; // Thymeleaf template name (e.g., "error.html")
+    }
+
+}
