040757258ba8de134861624ffe6d8b57dcfaaaa1
yennanliu <f339339@gmail.com>
2023-11-23 17:07:07 +0800

    hardcode download path, add test, dummy file

--- springWarehouse/src/test/java/com/yen/springWarehouse/controller/DownloadControllerTest.java
+++ springWarehouse/src/test/java/com/yen/springWarehouse/controller/DownloadControllerTest.java
@@ -2,7 +2,10 @@ package com.yen.springWarehouse.controller;
 
 import com.yen.springWarehouse.util.DateTimeUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,6 +22,23 @@ class DownloadControllerTest {
 
         DateTimeUtils dateTimeUtils = new DateTimeUtils();
         System.out.println(dateTimeUtils.getCurrentDateYYYYMMDDHHMMSS());
+    }
+
+    @Test
+    public void testGetResourcePath() throws IOException {
+
+        ClassPathResource r1 = new ClassPathResource("application.properties");
+        System.out.println(r1.getPath());
+        System.out.println(r1.exists());
+        System.out.println(r1.getURL());
+
+        //ClassPathResource r2 = new ClassPathResource("/report/20231123-16-16-09_report.json");
+        ClassPathResource r2 = new ClassPathResource("20231123-16-52-30_report.json");
+        System.out.println(r2.getPath());
+        System.out.println(r2.exists());
+        System.out.println(r2.getURL());
+
+
     }
 
 }
