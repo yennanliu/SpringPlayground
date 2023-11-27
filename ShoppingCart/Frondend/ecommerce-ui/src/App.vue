99242c0cae403a6746830ded4b31d3a5c23bab7c
yennanliu <f339339@gmail.com>
2023-11-27 16:31:24 +0800

    add wishlist to router, add ProductBox.vue

--- ShoppingCart/Frondend/ecommerce-ui/src/App.vue
+++ ShoppingCart/Frondend/ecommerce-ui/src/App.vue
@@ -4,7 +4,8 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/admin/category">Category</router-link>  |
-      <router-link to="/admin/category/add">Add</router-link>
+      <router-link to="/admin/category/add">Add</router-link> |
+      <router-link to="/admin/wishlist">Wishlist</router-link>
     </nav>
     <router-view/>
   </div>
