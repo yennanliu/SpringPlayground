3a82cc54bddcc8897da8c6d1f5f6dd44dcdc2245
yennanliu <f339339@gmail.com>
2023-11-27 08:40:19 +0800

    add controller, config, update readme

---
+++ ShoppingCart/Backend/src/main/java/com/yen/ShoppingCart/controller/CategoryController.java
@@ -0,0 +1,51 @@
+package com.yen.ShoppingCart.controller;
+
+import com.yen.ShoppingCart.common.ApiResponse;
+import com.yen.ShoppingCart.model.Category;
+import com.yen.ShoppingCart.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<Category>> getCategories() {
+        List<Category> body = categoryService.listCategories();
+        return new ResponseEntity<>(body, HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ApiResponse> createCategory(@RequestBody Category category) {
+
+        if (Objects.nonNull(categoryService.readCategory(category.getCategoryName()))) {
+            return new ResponseEntity<ApiResponse>(new ApiResponse(false, "category already exists"), HttpStatus.CONFLICT);
+        }
+        categoryService.createCategory(category);
+        return new ResponseEntity<>(new ApiResponse(true, "created the category"), HttpStatus.CREATED);
+    }
+
+
+    @PostMapping("/update/{categoryID}")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable("categoryID") Integer categoryID, @RequestBody Category category) {
+        // Check to see if the category exists.
+        if (Objects.nonNull(categoryService.readCategory(categoryID))) {
+            // If the category exists then update it.
+            categoryService.updateCategory(categoryID, category);
+            return new ResponseEntity<ApiResponse>(new ApiResponse(true, "updated the category"), HttpStatus.OK);
+        }
+
+        // If the category doesn't exist then return a response of unsuccessful.
+        return new ResponseEntity<>(new ApiResponse(false, "category does not exist"), HttpStatus.NOT_FOUND);
+    }
+
+}
