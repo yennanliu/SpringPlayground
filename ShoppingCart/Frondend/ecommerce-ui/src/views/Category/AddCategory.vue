77b2e365615ef8477d6358c601e46142e5c328c0
yennanliu <f339339@gmail.com>
2023-11-27 10:22:32 +0800

    update router index, add Category/ view

---
+++ ShoppingCart/Frondend/ecommerce-ui/src/views/Category/AddCategory.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 text-center">
+          <h4 class="pt-3">Add new Category</h4>
+        </div>
+      </div>
+ 
+      <div class="row">
+        <div class="col-3"></div>
+        <div class="col-md-6 px-5 px-md-0">
+         <form>
+           <div class="form-group">
+             <label>Category Name</label>
+             <input type="text" class="form-control" v-model="categoryName" required>
+           </div>
+           <div class="form-group">
+             <label>Description</label>
+             <input type="text" class="form-control" v-model="description" required>
+           </div>
+           <div class="form-group">
+             <label>ImageURL</label>
+             <input type="url" class="form-control" v-model="imageURL" required>
+           </div>
+           <button type="button" class="btn btn-primary">Submit</button>
+         </form>
+        </div>
+        <div class="col-3"></div>
+      </div>
+   </div>
+ </template>
+ 
+ <script>
+ export default {
+   data(){
+     return {
+       categoryName : null,
+       description : null,
+       imageURL : null,
+     }
+   },
+   props : [],
+   methods : {
+     
+   }
+ }
+ </script>
+ 
+ <style scoped>
+ h4 {
+   font-family: 'Roboto', sans-serif;
+   color: #484848;
+   font-weight: 700;
+ }
+ </style>
