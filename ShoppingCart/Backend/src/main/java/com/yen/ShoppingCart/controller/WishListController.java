3f98dcb505b7c0ceabaacc1b60c299e75697ad86
yennanliu <f339339@gmail.com>
2023-11-30 14:40:27 +0800

    add method in WishListControlle, add dep

--- ShoppingCart/Backend/src/main/java/com/yen/ShoppingCart/controller/WishListController.java
+++ ShoppingCart/Backend/src/main/java/com/yen/ShoppingCart/controller/WishListController.java
@@ -4,8 +4,11 @@ import com.yen.ShoppingCart.common.ApiResponse;
 import com.yen.ShoppingCart.model.Product;
 import com.yen.ShoppingCart.model.User;
 import com.yen.ShoppingCart.model.WishList;
+import com.yen.ShoppingCart.model.dto.ProductDto;
 import com.yen.ShoppingCart.service.AuthenticationService;
+import com.yen.ShoppingCart.service.ProductService;
 import com.yen.ShoppingCart.service.WishListService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/wishlist")
 public class WishListController {
@@ -28,10 +32,28 @@ public class WishListController {
     @Autowired
     private AuthenticationService authenticationService;
 
+    @Autowired
+    private ProductService productService;
+
+
+    @GetMapping("/{token}")
+    public ResponseEntity<List<ProductDto>> getWishList(@PathVariable("token") String token) {
+
+        log.info("(getWishList) token = " + token);
+        int user_id = authenticationService.getUser(token).getId();
+        List<WishList> body = wishListService.readWishList(user_id);
+        List<ProductDto> products = new ArrayList<ProductDto>();
+        for (WishList wishList : body) {
+            products.add(ProductService.getDtoFromProduct(wishList.getProduct()));
+        }
+
+        return new ResponseEntity<List<ProductDto>>(products, HttpStatus.OK);
+    }
 
     @PostMapping("/add")
     public ResponseEntity<ApiResponse> addWishList(@RequestBody Product product, @RequestParam("token") String token) {
 
+        log.info("(addWishList) token = " + token);
         authenticationService.authenticate(token);
         User user = authenticationService.getUser(token);
         WishList wishList = new WishList(user, product);
