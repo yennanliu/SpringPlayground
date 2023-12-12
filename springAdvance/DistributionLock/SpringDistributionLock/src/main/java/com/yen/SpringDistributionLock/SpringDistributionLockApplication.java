beca5f1054982e4462e6e373119145f4b3b78c05
yennanliu <f339339@gmail.com>
2023-12-12 18:26:50 +0800

    init DistributionLock/ project

---
+++ springAdvance/DistributionLock/SpringDistributionLock/src/main/java/com/yen/SpringDistributionLock/SpringDistributionLockApplication.java
@@ -0,0 +1,14 @@
+package com.yen.SpringDistributionLock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDistributionLockApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringDistributionLockApplication.class, args);
+	}
+
+}
