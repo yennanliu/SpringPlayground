ad20ba11ef3af586cb3492a96c1ba79498c216de
yennanliu <f339339@gmail.com>
2023-12-14 17:26:09 +0800

    add springEmployeeSystem/

---
+++ springEmployeeSystem/backend/EmployeeSystem/src/main/java/EmployeeSystem/demo/EmployeeSystemApplication.java
@@ -0,0 +1,14 @@
+package EmployeeSystem.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSystemApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(EmployeeSystemApplication.class, args);
+	}
+
+}
---
+++ springEmployeeSystem/backend/EmployeeSystem/src/test/java/EmployeeSystem/demo/EmployeeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package EmployeeSystem.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
