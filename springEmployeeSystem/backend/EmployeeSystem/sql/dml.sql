d42540da49ef740eb8f9c82d61f0f028c6acbe88
yennanliu <f339339@gmail.com>
2023-12-14 17:59:09 +0800

    fix swagger, add sql

---
+++ springEmployeeSystem/backend/EmployeeSystem/sql/dml.sql
@@ -0,0 +1,10 @@
+-- database : employee_system
+
+-- DML for users table
+
+-- truncate users;
+
+INSERT INTO `users` VALUES
+(1, "tom@google.com", "tom", "chen", "123", "USER"),
+(2, "kate@google.com", "lisa", "park", "123", "USER"),
+(3, "tom@google.com", "shohei", "ohtani", "123", "USER");
