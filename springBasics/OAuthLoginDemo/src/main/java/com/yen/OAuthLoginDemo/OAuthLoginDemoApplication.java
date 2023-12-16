a9c76bf1915c33c7c18285201098a0e00e071057
yennanliu <f339339@gmail.com>
2023-12-16 17:53:09 +0800

    add springBasics/OAuthLoginDemo/

---
+++ springBasics/OAuthLoginDemo/src/main/java/com/yen/OAuthLoginDemo/OAuthLoginDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yen.OAuthLoginDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthLoginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthLoginDemoApplication.class, args);
+	}
+
+}
