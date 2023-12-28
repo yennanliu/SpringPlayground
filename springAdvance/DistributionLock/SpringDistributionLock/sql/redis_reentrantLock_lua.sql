d95d23a2e9a0241f833ff42915cea7eec3c3f3dc
yennanliu <f339339@gmail.com>
2023-12-28 11:40:49 +0800

    update sql

--- springAdvance/DistributionLock/SpringDistributionLock/sql/redis_reentrantLock_lua.sql
+++ springAdvance/DistributionLock/SpringDistributionLock/sql/redis_reentrantLock_lua.sql
@@ -4,6 +4,10 @@
 
 -- https://youtu.be/V-OREDbG2UA?si=dNHrBr83Gq5dR8RY
 
+-------------------------------------
+-- Lock
+-------------------------------------
+
 -- Lock Steps
 --  1. check whether lock existed (exists), if not, get lock directly (hset key field value 1)
 --  2. if lock existed  (state!=0), check if such lock is owned by its thread (HEXISTS lock <key>) (the thread which create the lock)
@@ -22,3 +26,35 @@ hincrby lock 66-666-666 1
 --(integer) 0
 --127.0.0.1:6379> hincrby lock 66-666-666 1
 --(integer) 2
+
+-------------------------------------
+-- Lock Redis Lua cmd
+-------------------------------------
+
+-- if lock not existed
+if redis.call('exists', 'lock') = 0
+then
+--  lock directly, return true
+    redis.call('hset', 'lock', uuid, 1)
+    redis.call('expire', 'lock', 30)
+    return 1
+-- if lock existed, check if lock owned by current thread
+elseif redis.call('hexists', 'lock', uuid) == 1
+then
+--  if above is true, add increase val by 1, get lock, return true
+    redis.call('hincrby', 'lock', uuid, 1)
+    redis.call('expire', 'lock', 30)
+    return 1
+-- if above is false, lock failed, return false
+else
+    return 0
+end
+
+
+-------------------------------------
+-- Unlock
+-------------------------------------
+
+-------------------------------------
+-- Unlock Redis Lua cmd
+-------------------------------------
