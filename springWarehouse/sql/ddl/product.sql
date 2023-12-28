82fcd51591e63b8f33568871a2a006a29bf65af8
yennanliu <f339339@gmail.com>
2023-12-28 16:11:26 +0800

    fix dml

--- springWarehouse/sql/ddl/product.sql
+++ springWarehouse/sql/ddl/product.sql
@@ -3,20 +3,25 @@
 
 -- create database warehouse_system;
 
+-- DROP TABLE product;
+
 CREATE TABLE IF NOT EXISTS product(
     id int PRIMARY KEY AUTO_INCREMENT,
     name varchar(100) not null,
-    type_id int not null,
+    type_id int DEFAULT 1,
     price int not null,
     merchant_id varchar(100),
     product_status  varchar(10),
-    amount int DEFAULT 0,
-    constraint FK_PRODUCT_TYPE FOREIGN KEY (type_id) references product_type(type_id)
+    amount int DEFAULT 0
+    -- constraint FK_PRODUCT_TYPE FOREIGN KEY (type_id) references product_type(type_id)
 );
 
 -- DML
-INSERT INTO product(name, type_id, price, merchant_id, amount, product_status)
+-- truncate product;
+
+INSERT INTO product(name, price, merchant_id, amount, product_status)
 VALUES
-("i-phone", 2, 300, 1, 3, "on_board"),
-("cookie2", 4, 10, 2, 2, "new"),
-("soap", 1, 50, 1, 1, "off_board");
+("i-phone", 300, 1, 5000, "on_board"),
+("cookie2", 10, 2, 5000, "new"),
+("soap", 50, 3, 5000, "off_board"),
+("soap", 70, 4, 5000, "off_board");
