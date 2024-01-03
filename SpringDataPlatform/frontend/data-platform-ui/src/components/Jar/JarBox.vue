1ccdc14229f13615ddf0b731c33ae96162036961
yennanliu <f339339@gmail.com>
2024-01-03 14:39:25 +0800

    add jar view, component, disable multi-word-component-names

---
+++ SpringDataPlatform/frontend/data-platform-ui/src/components/Jar/JarBox.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="card h-100 w-100">
+    <div class="card-body">
+      <div class="card-header">
+        <router-link :to="{ name: 'showJarDetails', params: { id: jar.id } }">
+          <h5 class="card-title">{{ jar.name }}</h5>
+        </router-link>
+        <router-link
+          id="edit-jar"
+          :to="{ name: 'EditJar', params: { id: jar.id } }"
+          v-show="$route.name == 'AdminJar'"
+          class="edit-link"
+        >
+          Edit
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+var axios = require("axios");
+import swal from "sweetalert";
+export default {
+  name: "JarBox",
+  props: ["jar"],
+  data() {
+    return {
+      jars: [],
+    };
+  },
+  methods: {
+    ShowJarDetails() {
+      console.log(
+        "(ShowJarDetails) this.$route.params.id = " + this.$route.params.id
+      );
+      this.$router.push({
+        name: "ShowJarDetails",
+        arams: { id: this.$route.params.id },
+      });
+    },
+
+    async EditDepartment() {
+      axios
+        .post("http://localhost:9999/jar/", this.jar)
+        .then((res) => {
+          console.log(res);
+          //sending the event to parent to handle
+          this.$emit("fetchData");
+          this.$router.push({ name: "AdminJar" });
+          swal({
+            text: "Jar Updated Successfully!",
+            icon: "success",
+            closeOnClickOutside: false,
+          });
+        })
+        .catch((err) => console.log("err", err));
+    },
+  },
+};
+</script>
+
+<style scoped>
+.card-header {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.embed-responsive .card-img-top {
+  object-fit: cover;
+}
+
+a {
+  text-decoration: none;
+}
+
+.card-title {
+  color: #484848;
+  font-size: 1.1rem;
+  font-weight: 400;
+}
+
+.card-title:hover {
+  font-weight: bold;
+}
+
+.edit-link {
+  background-color: #007bff;
+  color: #fff;
+  padding: 5px 5px;
+  border-radius: 3px;
+  cursor: pointer;
+}
+
+.edit-link:hover {
+  background-color: #0056b3;
+}
+</style>
---
+++ SpringDataPlatform/frontend/data-platform-ui/src/views/Jar/Jar.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h4 class="pt-3">Flink Jars</h4>
+        <router-link
+          id="add-department"
+          :to="{ name: 'AddJar' }"
+          v-show="$route.name == 'AdminJar'"
+        >
+          <button class="btn">Add a new Jar</button>
+        </router-link>
+      </div>
+    </div>
+    <div class="row">
+      <div
+        v-for="jar of jars"
+        :key="jar.id"
+        class="col-md-6 col-xl-4 col-12 pt-3 justify-content-around d-flex"
+      >
+        <JarBox :jar="jar"> </JarBox>
+      </div>
+    </div>
+  </div>
+</template>
+  
+  <script>
+// https://youtu.be/VZ1NV7EHGJw?si=JPmnA7oQoVdPJwAL&t=1450
+// https://github.com/webtutsplus/ecommerce-vuejs/blob/master/src/views/Product/Product.vue
+
+import JarBox from "../../components/Jar/JarBox";
+var axios = require("axios");
+
+export default {
+  name: "Jar",
+  components: { JarBox },
+  props: ["baseURL"],
+
+  data() {
+    return {
+      //baseURL: "http://localhost:9999/", // NOTE !! we read baseURL from App.vue
+      jars: [],
+    };
+  },
+  methods: {
+    async getDepartments() {
+      await axios
+        .get("http://localhost:9999/jar/")
+        .then((res) => (this.jars = res.data))
+        .catch((err) => console.log(err));
+    },
+  },
+  mounted() {
+    this.getDepartments();
+  },
+};
+</script>
+  
+  <style scoped>
+h4 {
+  font-family: "Roboto", sans-serif;
+  color: #484848;
+  font-weight: 700;
+}
+
+#add-user {
+  float: right;
+  font-weight: 500;
+}
+</style>
