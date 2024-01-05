a7bb223be4fe9b9a6eb600feabf9aabefb560e4b
yennanliu <f339339@gmail.com>
2024-01-05 11:21:09 +0800

    update zkclient test

--- springAdvance/DistributionLock/SpringDistributionLock/src/main/java/com/yen/SpringDistributionLock/zookeeper/ZkTest.java
+++ springAdvance/DistributionLock/SpringDistributionLock/src/main/java/com/yen/SpringDistributionLock/zookeeper/ZkTest.java
@@ -1,20 +1,21 @@
 package com.yen.SpringDistributionLock.zookeeper;
 
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.Watcher;
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
 
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
 
 /**
  * ZK client test (org.apache.zookeeper)
+ *
+ *
  * <p>
- * - https://youtu.be/LTiRPXieav4?si=XCZiKKPk47OR1cYu&t=191
+ *  - https://youtu.be/LTiRPXieav4?si=XCZiKKPk47OR1cYu&t=191
+ *  - https://youtu.be/zvYXU_oxZ08?si=F_b72ozRuYsWLlO7&t=23
  */
 public class ZkTest {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, KeeperException {
 
         ZooKeeper zooKeeper = null;
 
@@ -36,8 +37,27 @@ public class ZkTest {
                     cdl.countDown(); // amount -= 1
                 }
             });
+
+
             cdl.await(); // will block (await) before amount == 0
             System.out.println("--> some operation ...");
+
+            // create permanent node
+            zooKeeper.create("/yen/test_1", "hello zk !!!".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+
+            // create Ephemeral node
+            zooKeeper.create("/yen/test_2", "hello Ephemeral zk !!!".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+
+            // create permanent serialization node
+            zooKeeper.create("/yen/test_3", "hello permanent serialization zk !!!".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+//            zooKeeper.create("/yen/test_3", "hello permanent serialization zk !!!".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+//            zooKeeper.create("/yen/test_3", "hello permanent serialization zk !!!".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT_SEQUENTIAL);
+
+            // create Ephemeral serialization node
+            zooKeeper.create("/yen/test_4", "hello EPHEMERAL serialization zk !!!".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+            zooKeeper.create("/yen/test_4", "hello EPHEMERAL serialization zk !!!".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+            zooKeeper.create("/yen/test_4", "hello EPHEMERAL serialization zk !!!".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+            
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
