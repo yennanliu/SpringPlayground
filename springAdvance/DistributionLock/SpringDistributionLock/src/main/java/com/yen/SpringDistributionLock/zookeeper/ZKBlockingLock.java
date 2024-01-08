24d5b7b20cc81f6f5a397ef8813a4e126da182f8
yennanliu <f339339@gmail.com>
2024-01-08 11:12:30 +0800

    implement ZKBlockingLock, update readme

--- springAdvance/DistributionLock/SpringDistributionLock/src/main/java/com/yen/SpringDistributionLock/zookeeper/ZKBlockingLock.java
+++ springAdvance/DistributionLock/SpringDistributionLock/src/main/java/com/yen/SpringDistributionLock/zookeeper/ZKBlockingLock.java
@@ -1,27 +1,35 @@
 package com.yen.SpringDistributionLock.zookeeper;
 
-import org.apache.zookeeper.CreateMode;
-import org.apache.zookeeper.KeeperException;
-import org.apache.zookeeper.ZooDefs;
-import org.apache.zookeeper.ZooKeeper;
+import jodd.util.CollectionUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.zookeeper.*;
+import org.springframework.util.CollectionUtils;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
+import java.util.stream.Collectors;
 
 /**
- *  ZK BlockingLock
- *
- *  - 阻塞鎖
- *     - 利用 `ZK 臨時序列化節點`
- *
- *  - https://youtu.be/yAp8mbhxWlM?si=yfpVYi5e4N9DVNS9&t=414
+ * ZK BlockingLock
+ * <p>
+ * - 阻塞鎖
+ * - 利用 `ZK 臨時序列化節點`
+ * <p>
+ * - https://youtu.be/yAp8mbhxWlM?si=yfpVYi5e4N9DVNS9&t=414
+ * - https://youtu.be/lrlOo4ouzHY?si=Q2IcmKzi4kJrN1NA&t=113
  */
 public class ZKBlockingLock implements Lock {
 
     // attr
-    String lockName;
-    ZooKeeper zooKeeper;
+    private String lockName;
+    private ZooKeeper zooKeeper;
+
+    private String currentNodePath;
 
     private static final String ROOT_PATH = "/locks";
 
@@ -30,7 +38,7 @@ public class ZKBlockingLock implements Lock {
 
         // check if root path exists; if not, create
         try {
-            if (zooKeeper.exists(ROOT_PATH, false) == null){
+            if (zooKeeper.exists(ROOT_PATH, false) == null) {
                 zooKeeper.create(ROOT_PATH, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
             }
         } catch (KeeperException e) {
@@ -57,9 +65,8 @@ public class ZKBlockingLock implements Lock {
 
 
     /**
-     *  create ZK node, if can't create, keep retrying
-     *  https://youtu.be/rw6jIVHirvQ?si=fbGVH9knZ38uPFa4
-     *
+     * create ZK node, if can't create, keep retrying
+     * https://youtu.be/rw6jIVHirvQ?si=fbGVH9knZ38uPFa4
      */
     @Override
     public boolean tryLock() {
@@ -71,35 +78,55 @@ public class ZKBlockingLock implements Lock {
              *
              *   - Here we create blocking lock via ZK ephemeral serialization node (臨時序列化節點)
              *   - 阻塞鎖 利用 `ZK 臨時序列化節點`
+             *   - "-" is for below getPreNode() method
              *
              *   - https://youtu.be/yAp8mbhxWlM?si=7B48O3JvfV43e-JC&t=414
              */
-            String curNodePath = this.zooKeeper.create(
-                    ROOT_PATH + "/" + this.lockName,
+            this.currentNodePath = this.zooKeeper.create(
+                    ROOT_PATH + "/" + this.lockName + "-",
                     null,
                     ZooDefs.Ids.OPEN_ACL_UNSAFE,
                     CreateMode.EPHEMERAL_SEQUENTIAL
             );
 
-            System.out.println("curNodePath = " + curNodePath);
+            System.out.println("currentNodePath = " + this.currentNodePath);
             /**
              *
              *  get previous node, if null, then get lock successfully
              *  otherwise, monitor previous node
+             *
+             *  - https://youtu.be/lrlOo4ouzHY?si=Q2IcmKzi4kJrN1NA&t=113
              */
             String preNodePath = this.getPreNode();
+            // check if preNodePath is null
+            if (preNodePath != null) {
+                /**
+                 *  implement block via blocking lock
+                 *
+                 */
+                CountDownLatch cdl = new CountDownLatch(1);
+                 
+                /**
+                 *  NOTE !!!
+                 *     check if ZK previous node existed
+                 *     since getPreNode() is NOT ATOM (原子性操作)
+                 *
+                 *     - https://youtu.be/lrlOo4ouzHY?si=fmXX2boUnlH2QMSk&t=1145
+                 */
+                if (this.zooKeeper.exists(ROOT_PATH + "/" + preNodePath, new Watcher() {
+                    @Override
+                    public void process(WatchedEvent watchedEvent) {
+                        cdl.countDown();
+                    }
+                }) == null) {
+                    return true;
+                }
+                cdl.await();
+            }
             return true;
 
         } catch (Exception e) {
             e.printStackTrace();
-            try {
-                // retry
-                System.out.println("Can't get lock, sleep 80 millisecond, and try ...");
-                Thread.sleep(80);
-                this.tryLock();
-            } catch (InterruptedException ex) {
-                ex.printStackTrace();
-            }
         }
         return false;
     }
@@ -110,16 +137,16 @@ public class ZKBlockingLock implements Lock {
     }
 
     /**
-     *  delete ZK node
-     *
-     *  https://youtu.be/rw6jIVHirvQ?si=NS1cRbHlqaONNP8_&t=236
+     * delete ZK node
+     * <p>
+     * https://youtu.be/rw6jIVHirvQ?si=NS1cRbHlqaONNP8_&t=236
      */
     @Override
     public void unlock() {
 
         try {
             // version = -1 : delete lock anyway
-            this.zooKeeper.delete(ROOT_PATH + "/" + lockName, -1);
+            this.zooKeeper.delete(this.currentNodePath, -1);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         } catch (KeeperException e) {
@@ -134,9 +161,71 @@ public class ZKBlockingLock implements Lock {
     }
 
     // local method
+
+    /**
+     * Method get previous node in ZK
+     * <p>
+     * https://youtu.be/lrlOo4ouzHY?si=es2MC0ehpCaVLqYC&t=225
+     */
     private String getPreNode() {
 
-        return null;
+        // get all nodes under root path
+        try {
+
+            // get all nodes under root path
+            List<String> children = this.zooKeeper.getChildren(ROOT_PATH, false);
+
+            // edge case : children is null
+            if (CollectionUtils.isEmpty(children)) {
+                throw new IllegalMonitorStateException("(getPreNode) error : children is null");
+            }
+
+            // get (previous) nodes has same resources as current node
+            List<String> nodes = children.stream().filter(node -> {
+                /**
+                 *  example :
+                 *      if current node = /locks/lock-
+                 *      and we have children as below:
+                 *           /locks/lock-
+                 *           /locks/lock-
+                 *           /locks/lockx-
+                 *           /locks/locky
+                 *           /locks/lockxy-
+                 *           ...
+                 *      so we should collect /locks/lock-, /locks/lock-
+                 */
+                return StringUtils.startsWith(node, lockName + "-");
+            }).collect(Collectors.toList());
+
+            // edge case : nodes is null
+            if (CollectionUtils.isEmpty(nodes)) {
+                throw new IllegalMonitorStateException("(getPreNode) error : nodes is null");
+            }
+
+            // ordering
+            Collections.sort(nodes);
+
+            // get current node name
+            // example :  if path = /aa/bb, then we get /bb
+            String curNode = StringUtils.substringAfterLast(currentNodePath, "/");
+            /**
+             *   get current node subscript (下標)
+             *   if subscript == 0, get current lock
+             *   else, monitor previous node
+             */
+            int index = Collections.binarySearch(nodes, curNode);
+            if (index < 0) {
+                throw new IllegalMonitorStateException("(getPreNode) error : index smaller than 0");
+            } else if (index > 0) {
+                return nodes.get(index - 1); // else, monitor previous node
+            }
+            // if subscript == 0, get current lock -> return null
+            return null;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new IllegalMonitorStateException("(getPreNode) error : Invalid op");
+        }
     }
 
 }
