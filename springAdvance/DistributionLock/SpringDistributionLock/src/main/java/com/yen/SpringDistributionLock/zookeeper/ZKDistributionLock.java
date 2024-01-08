4d8fd63029227135e58f5b9a7c25f60bde9f86ea
yennanliu <f339339@gmail.com>
2024-01-08 09:35:38 +0800

    implement lock, unlock in ZK lock, update conf

--- springAdvance/DistributionLock/SpringDistributionLock/src/main/java/com/yen/SpringDistributionLock/zookeeper/ZKDistributionLock.java
+++ springAdvance/DistributionLock/SpringDistributionLock/src/main/java/com/yen/SpringDistributionLock/zookeeper/ZKDistributionLock.java
@@ -15,6 +15,8 @@ import java.util.concurrent.locks.Lock;
  *  - https://youtu.be/dcmhIij3eNM?si=botaWUkhGIXVimUX&t=34
  *  - https://youtu.be/dcmhIij3eNM?si=auTjc50ax8GpmIas&t=461
  *
+ *  - https://youtu.be/rw6jIVHirvQ?si=i8UA43t7VLkJZGst
+ *
  */
 public class ZKDistributionLock implements Lock {
 
@@ -42,11 +44,11 @@ public class ZKDistributionLock implements Lock {
         this.lockName = lockName;
     }
 
+    // method
     @Override
     public void lock() {
 
-        // create ZK node, if can't create, keep retrying
-
+        this.tryLock();
     }
 
     @Override
@@ -54,8 +56,32 @@ public class ZKDistributionLock implements Lock {
 
     }
 
+
+    /**
+     *  create ZK node, if can't create, keep retrying
+     *  https://youtu.be/rw6jIVHirvQ?si=fbGVH9knZ38uPFa4
+     *
+     */
     @Override
     public boolean tryLock() {
+
+        try {
+            /**
+             *  here we create EPHEMERAL node (臨時節點) (to avoid permanent node -> avoid dead lock)
+             */
+            this.zooKeeper.create(ROOT_PATH + "/" + this.lockName, null, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                // retry
+                System.out.println("Can't get lock, sleep 80 millisecond, and try ...");
+                Thread.sleep(80);
+                this.tryLock();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
         return false;
     }
 
@@ -64,10 +90,22 @@ public class ZKDistributionLock implements Lock {
         return false;
     }
 
+    /**
+     *  delete ZK node
+     *
+     *  https://youtu.be/rw6jIVHirvQ?si=NS1cRbHlqaONNP8_&t=236
+     */
     @Override
     public void unlock() {
 
-        // delete ZK node
+        try {
+            // version = -1 : delete lock anyway
+            this.zooKeeper.delete(ROOT_PATH + "/" + lockName, -1);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (KeeperException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -75,7 +113,5 @@ public class ZKDistributionLock implements Lock {
     public Condition newCondition() {
         return null;
     }
-
-    // method
 
 }
