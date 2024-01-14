c9b040282cdf01400cc8cf1117c4f26684eebd1a
yennanliu <f339339@gmail.com>
2024-01-14 17:33:15 +0800

    BE add file IO service, config, exception

---
+++ SpringDataPlatform/backend/DataPlatform/FlinkRestService/src/main/java/com/yen/FlinkRestService/exception/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package com.yen.FlinkRestService.exception;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
