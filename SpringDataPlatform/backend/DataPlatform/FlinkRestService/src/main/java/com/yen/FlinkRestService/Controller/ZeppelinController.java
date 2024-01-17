945a5dd116288baa3680093d79ee50779a2b2c87
yennanliu <f339339@gmail.com>
2024-01-17 18:38:25 +0800

    add endpoint to ZeppelinController

--- SpringDataPlatform/backend/DataPlatform/FlinkRestService/src/main/java/com/yen/FlinkRestService/Controller/ZeppelinController.java
+++ SpringDataPlatform/backend/DataPlatform/FlinkRestService/src/main/java/com/yen/FlinkRestService/Controller/ZeppelinController.java
@@ -1,13 +1,16 @@
 package com.yen.FlinkRestService.Controller;
 
 import com.yen.FlinkRestService.Service.ZeppelinService;
+import com.yen.FlinkRestService.model.Job;
+import com.yen.FlinkRestService.model.Notebook;
 import com.yen.FlinkRestService.model.dto.zeppelin.AddParagraphDto;
 import com.yen.FlinkRestService.model.dto.zeppelin.CreateNoteDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/zeppelin")
@@ -16,7 +19,21 @@ public class ZeppelinController {
     @Autowired
     private ZeppelinService zeppelinService;
 
-    @PostMapping("/create")
+    @GetMapping("/")
+    public ResponseEntity<List<Notebook>> getAllNotebooks(){
+
+        List<Notebook> notebooks = zeppelinService.getNotebooks();
+        return new ResponseEntity<>(notebooks, HttpStatus.OK);
+    }
+
+    @GetMapping("/{notebookId}")
+    public ResponseEntity<Notebook> getJobByJobId(@PathVariable("notebookId") Integer notebookId){
+
+        Notebook notebook = zeppelinService.getNotebookById(notebookId);
+        return new ResponseEntity<>(notebook, HttpStatus.OK);
+    }
+
+    @PostMapping("/add")
     public String createNotebook(@RequestBody CreateNoteDto createNoteDto){
 
         String res = zeppelinService.createNote(createNoteDto);
