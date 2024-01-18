08a0641a7a200d6047431d2d95ac2a2e6e53bf9c
yennanliu <f339339@gmail.com>
2024-01-18 18:54:37 +0800

    FE add method send addParagraph request to BE with selected nb id

--- SpringDataPlatform/frontend/data-platform-ui/src/views/Zeppelin/NotebookConsole.vue
+++ SpringDataPlatform/frontend/data-platform-ui/src/views/Zeppelin/NotebookConsole.vue
@@ -3,13 +3,25 @@
     <h1>Notebook console</h1>
     <label>Select Notebook:</label>
     <select v-model="selectedNotebook">
-      <option v-for="notebook in notebooks" :key="notebook.id" :value="notebook.zeppelinNoteId">
-        {{ "Notebook ID = " + notebook.zeppelinNoteId + ",     Interpreter = " + notebook.interpreterGroup}}
+      <option
+        v-for="notebook in notebooks"
+        :key="notebook.id"
+        :value="notebook.zeppelinNoteId"
+      >
+        {{
+          "Notebook ID = " +
+          notebook.zeppelinNoteId +
+          ",     Interpreter = " +
+          notebook.interpreterGroup
+        }}
       </option>
     </select>
     <button @click="addCell">Add Cell</button>
     <div v-for="(cell, index) in cells" :key="index">
-      <textarea v-model="cell.code" placeholder="Type your code here"></textarea>
+      <textarea
+        v-model="cell.code"
+        placeholder="Type your code here"
+      ></textarea>
       <button @click="executeCode(index)">Run Code</button>
       <div v-if="cell.result !== undefined">
         <strong>Result:</strong>
@@ -36,14 +48,13 @@ export default {
     };
   },
   methods: {
-
     async getNotebooks() {
       // fetch users
       await axios
         .get("http://localhost:9999/zeppelin/")
         .then((res) => {
           this.notebooks = res.data;
-          console.log("this.notebooks = " + JSON.stringify(this.notebooks))
+          console.log("this.notebooks = " + JSON.stringify(this.notebooks));
         })
         .catch((err) => console.log(err));
     },
@@ -54,14 +65,37 @@ export default {
         result: undefined,
       });
     },
-    executeCode(index) {
-      // Mock code execution, replace with actual execution logic
-      const result = this.mockExecuteCode(this.cells[index].code);
-      console.log("Run on cell ID = " + this.selectedNotebook)
-      this.$set(this.cells, index, {
-        ...this.cells[index],
-        result,
-      });
+    // executeCode(index) {
+    //   // Mock code execution, replace with actual execution logic
+    //   const result = this.mockExecuteCode(this.cells[index].code);
+    //   console.log("Run on cell ID = " + this.selectedNotebook)
+    //   this.$set(this.cells, index, {
+    //     ...this.cells[index],
+    //     result,
+    //   });
+    // },
+    async executeCode(index) {
+      console.log(
+        "Run on cell ID = " + this.selectedNotebook + " index = " + index
+      );
+      const codeCmd = {
+        noteId: this.selectedNotebook,
+        text: "kkk ",
+      };
+      console.log("codeCmd = " + JSON.stringify(codeCmd));
+      await axios({
+        method: "post",
+        url: "http://localhost:9999/zeppelin/addParagraph",
+        data: JSON.stringify(codeCmd),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((res) => {
+          //sending the event to parent to handle
+          console.log(res);
+        })
+        .catch((err) => console.log(err));
     },
     mockExecuteCode(code) {
       // Mock execution, replace with actual code execution logic
