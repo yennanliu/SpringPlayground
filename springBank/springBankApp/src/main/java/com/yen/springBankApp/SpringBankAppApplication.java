a09611cd31ff6ab9340d903624e4b5258ead0a57
yennanliu <f339339@gmail.com>
2024-01-20 17:46:29 +0800

    add springBankApp/

---
+++ springBank/springBankApp/src/main/java/com/yen/springBankApp/SpringBankAppApplication.java
@@ -0,0 +1,14 @@
+package com.yen.springBankApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBankAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringBankAppApplication.class, args);
+	}
+
+}
---
+++ springBank/springBankApp/src/test/java/com/yen/springBankApp/SpringBankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.springBankApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
