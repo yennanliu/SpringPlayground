79ffe5f121ef5ac7e0164319e6194f401a9ba5af
yennanliu <f339339@gmail.com>
2024-03-23 15:25:54 +0800

    add SpotifyPlayList BE app

---
+++ springSpotifyPlayList/backend/SpotifyPlayList/src/main/java/com/yen/SpotifyPlayList/SpotifyPlayListApplication.java
@@ -0,0 +1,14 @@
+package com.yen.SpotifyPlayList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyPlayListApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpotifyPlayListApplication.class, args);
+	}
+
+}
---
+++ springSpotifyPlayList/backend/SpotifyPlayList/src/test/java/com/yen/SpotifyPlayList/SpotifyPlayListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.SpotifyPlayList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyPlayListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
