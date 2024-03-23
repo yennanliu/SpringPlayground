2d91bbdb58be89547f2f8f78a891e1586421573c
yennanliu <f339339@gmail.com>
2024-03-23 16:09:10 +0800

    update test code

--- springSpotifyPlayList/backend/SpotifyPlayList/src/test/java/com/yen/SpotifyPlayList/AuthTest.java
+++ springSpotifyPlayList/backend/SpotifyPlayList/src/test/java/com/yen/SpotifyPlayList/AuthTest.java
@@ -3,9 +3,17 @@ package com.yen.SpotifyPlayList;
 import org.apache.hc.core5.http.ParseException;
 import org.junit.jupiter.api.Test;
 import se.michaelthelin.spotify.SpotifyApi;
+import se.michaelthelin.spotify.SpotifyHttpManager;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
 import se.michaelthelin.spotify.model_objects.credentials.ClientCredentials;
+import se.michaelthelin.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 import se.michaelthelin.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+
+
+import java.net.URI;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 
 import java.io.IOException;
 
@@ -18,16 +26,21 @@ public class AuthTest {
         final String clientId = "";
         final String clientSecret = "";
 
-        final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+        final SpotifyApi spotifyApi = new SpotifyApi
+                .Builder()
                 .setClientId(clientId)
                 .setClientSecret(clientSecret)
                 .build();
-        final ClientCredentialsRequest clientCredentialsRequest = spotifyApi.clientCredentials()
+
+        final ClientCredentialsRequest clientCredentialsRequest = spotifyApi
+                .clientCredentials()
                 .build();
+
         try {
             final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
             String token = clientCredentials.getAccessToken();
             System.out.println(">>> token = " + token);
+
             // Set access token for further "spotifyApi" object usage
             spotifyApi.setAccessToken(clientCredentials.getAccessToken());
             System.out.println("Auth OK !!");
@@ -36,7 +49,41 @@ public class AuthTest {
         } catch (IOException | SpotifyWebApiException | ParseException e) {
             System.out.println(">>> Error: " + e.getMessage());
         }
+    }
+
+    // https://github.com/spotify-web-api-java/spotify-web-api-java/blob/master/examples/authorization/authorization_code/AuthorizationCodeUriExample.java
+    @Test
+    public void testAuthRedirect(){
+
+        final String clientId = "";
+        final String clientSecret = "";
+        final String redirectURI = "localhost:8080";
+        final URI redirectUri = SpotifyHttpManager.makeUri(redirectURI);
+
+        try{
+            final SpotifyApi spotifyApi = new SpotifyApi.Builder()
+                    .setClientId(clientId)
+                    .setClientSecret(clientSecret)
+                    .setRedirectUri(redirectUri)
+                    .build();
+
+            final AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
+//          .state("x4xkmn9pu3j6ukrs8n")
+//          .scope("user-read-birthdate,user-read-email")
+//          .show_dialog(true)
+                    .build();
+            System.out.println("Auth OK !!");
+
+            final URI uri = authorizationCodeUriRequest.execute();
+            System.out.println("uri = " + uri);
+
+        }catch (Exception e){
+            System.out.println("Auth failed : " + e);
+            e.printStackTrace();
+        }
+
 
     }
+
 
 }
