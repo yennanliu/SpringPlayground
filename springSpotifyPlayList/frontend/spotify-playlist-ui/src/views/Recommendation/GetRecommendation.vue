a484a06d4f7bdc48403ab30726096008b2ab228b
yennanliu <f339339@gmail.com>
2024-04-07 15:47:22 +0800

    fix FE shows recommend results

--- springSpotifyPlayList/frontend/spotify-playlist-ui/src/views/Recommendation/GetRecommendation.vue
+++ springSpotifyPlayList/frontend/spotify-playlist-ui/src/views/Recommendation/GetRecommendation.vue
@@ -1,7 +1,42 @@
 <template>
   <div>
-    <h1>Recommendation Tracks</h1>
-    <pre v-if="album">{{ JSON.stringify(album, null, 2) }}</pre>
+    <form>
+      <div class="form-group">
+        <label>Recommendation</label>
+        <input type="text" class="form-control" v-model="albumId" required />
+      </div>
+      <button type="button" class="btn btn-primary" @click="getRecommend">
+        Submit
+      </button>
+    </form>
+
+    <!-- <h1 v-if="tracks">
+      Album: {{ album.name }} | Artist: {{ album.artists[0].name }}
+    </h1> -->
+    <div v-if="tracks">
+      <!-- <p>
+        Album URL:
+        <a :href="album.externalUrls.externalUrls.spotify" target="_blank">{{
+          album.externalUrls.externalUrls.spotify
+        }}</a>
+      </p>
+      <img
+        v-if="album.images && album.images.length > 0"
+        :src="album.images[0].url"
+        :alt="album.name"
+        style="max-width: 300px; max-height: 300px"
+      /> -->
+      <div v-for="track in tracks.tracks" :key="track.id">
+        <p>Track: {{ track.name }}</p>
+        <p>
+          URL:
+          <a :href="track.externalUrls.externalUrls.spotify" target="_blank">{{
+            track.externalUrls.externalUrls.spotify
+          }}</a>
+        </p>
+        <hr />
+      </div>
+    </div>
     <div v-else>Loading...</div>
   </div>
 </template>
@@ -10,21 +45,26 @@
 export default {
   data() {
     return {
-      album: null,
+      albumId: null,
+      tracks: null,
     };
   },
-  mounted() {
-    this.fetchAlbum();
-  },
+  mounted() {},
   methods: {
-    async fetchAlbum() {
+    async getRecommend() {
       try {
-        const response = await fetch("http://localhost:8888/recommend/");
+        const response = await fetch(
+          //"http://localhost:8888/album/5zT1JLIj9E57p3e1rFm9Uq"
+          //`http://localhost:8888/album/${this.albumId}`
+          "http://localhost:8888/recommend/"
+        );
         if (!response.ok) {
           throw new Error("Failed to fetch album");
         }
         const data = await response.json();
-        this.album = data;
+        this.tracks = data;
+        console.log("this.tracks = {}", JSON.stringify(this.tracks));
+        // console.log("external url = {}", JSON.stringify(this.album.externalUrls.spotify))
       } catch (error) {
         console.error(error);
       }
@@ -32,3 +72,4 @@ export default {
   },
 };
 </script>
+  
