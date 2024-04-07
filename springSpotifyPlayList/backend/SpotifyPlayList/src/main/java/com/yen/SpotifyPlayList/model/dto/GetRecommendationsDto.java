aa534699282622bc76878c5e2b616b1335c0a908
yennanliu <f339339@gmail.com>
2024-04-07 16:46:52 +0800

    aff GetRecommendationsDto

---
+++ springSpotifyPlayList/backend/SpotifyPlayList/src/main/java/com/yen/SpotifyPlayList/model/dto/GetRecommendationsDto.java
@@ -0,0 +1,19 @@
+package com.yen.SpotifyPlayList.model.dto;
+
+import com.neovisionaries.i18n.CountryCode;
+import lombok.Data;
+import lombok.ToString;
+
+@ToString
+@Data
+public class GetRecommendationsDto {
+
+    private int amount = 10;
+    private CountryCode market = CountryCode.JP;
+    private int maxPopularity = 100;
+    private int minPopularity = 0;
+    private String seedArtistId; // e.g. : 0LcJLqbBmaGUft1e9Mm8HV
+    private String seedGenres;
+    private String seedTrack; // e.g. 01iyCAUm8EvOFqVWYJ3dVX
+    private int targetPopularity = 50;
+}
