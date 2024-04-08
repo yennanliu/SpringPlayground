69e4f98c988bb55b4923a2d7dec5faad50797d39
yennanliu <f339339@gmail.com>
2024-04-08 19:26:58 +0800

    FE update form send recommendation req

--- springSpotifyPlayList/frontend/spotify-playlist-ui/src/views/Recommendation/GetRecommendation.vue
+++ springSpotifyPlayList/frontend/spotify-playlist-ui/src/views/Recommendation/GetRecommendation.vue
@@ -2,30 +2,63 @@
   <div>
     <form>
       <div class="form-group">
-        <label>Recommendation</label>
-        <input type="text" class="form-control" v-model="albumId" required />
+        <label>Amount</label>
+        <input type="number" class="form-control" v-model="amount" required />
+      </div>
+      <div class="form-group">
+        <label>Market</label>
+        <input type="text" class="form-control" v-model="market" required />
+      </div>
+      <div class="form-group">
+        <label>Max Popularity</label>
+        <input
+          type="number"
+          class="form-control"
+          v-model="maxPopularity"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label>Min Popularity</label>
+        <input
+          type="number"
+          class="form-control"
+          v-model="minPopularity"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label>Seed Artist ID</label>
+        <input
+          type="text"
+          class="form-control"
+          v-model="seedArtistId"
+          required
+        />
+      </div>
+      <div class="form-group">
+        <label>Seed Genres</label>
+        <input type="text" class="form-control" v-model="seedGenres" required />
+      </div>
+      <div class="form-group">
+        <label>Seed Track</label>
+        <input type="text" class="form-control" v-model="seedTrack" required />
+      </div>
+      <div class="form-group">
+        <label>Target Popularity</label>
+        <input
+          type="number"
+          class="form-control"
+          v-model="targetPopularity"
+          required
+        />
       </div>
       <button type="button" class="btn btn-primary" @click="getRecommend">
         Submit
       </button>
     </form>
 
-    <!-- <h1 v-if="tracks">
-      Album: {{ album.name }} | Artist: {{ album.artists[0].name }}
-    </h1> -->
     <div v-if="tracks">
-      <!-- <p>
-        Album URL:
-        <a :href="album.externalUrls.externalUrls.spotify" target="_blank">{{
-          album.externalUrls.externalUrls.spotify
-        }}</a>
-      </p>
-      <img
-        v-if="album.images && album.images.length > 0"
-        :src="album.images[0].url"
-        :alt="album.name"
-        style="max-width: 300px; max-height: 300px"
-      /> -->
       <div v-for="track in tracks.tracks" :key="track.id">
         <p>Track: {{ track.name }} | Artist: {{ track.artists[0].name }}</p>
         <p>
@@ -48,31 +81,35 @@
     <div v-else>Loading...</div>
   </div>
 </template>
-  
-  <script>
+
+<script>
 export default {
   data() {
     return {
-      albumId: null,
+      amount: 10,
+      market: "JP",
+      maxPopularity: 100,
+      minPopularity: 0,
+      seedArtistId: "",
+      seedGenres: "",
+      seedTrack: "",
+      targetPopularity: 50,
       tracks: null,
     };
   },
-  mounted() {},
   methods: {
     async getRecommend() {
       try {
         const response = await fetch(
-          //"http://localhost:8888/album/5zT1JLIj9E57p3e1rFm9Uq"
-          //`http://localhost:8888/album/${this.albumId}`
+          // `http://localhost:8888/recommend?amount=${this.amount}&market=${this.market}&maxPopularity=${this.maxPopularity}&minPopularity=${this.minPopularity}&seedArtistId=${this.seedArtistId}&seedGenres=${this.seedGenres}&seedTrack=${this.seedTrack}&targetPopularity=${this.targetPopularity}`
           "http://localhost:8888/recommend/"
         );
         if (!response.ok) {
-          throw new Error("Failed to fetch album");
+          throw new Error("Failed to fetch recommendations");
         }
         const data = await response.json();
         this.tracks = data;
         console.log("this.tracks = {}", JSON.stringify(this.tracks));
-        // console.log("external url = {}", JSON.stringify(this.album.externalUrls.spotify))
       } catch (error) {
         console.error(error);
       }
@@ -80,4 +117,3 @@ export default {
   },
 };
 </script>
-  
