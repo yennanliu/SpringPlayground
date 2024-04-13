f52323984faa60b9e1b0cad1123942fb1128439a
yennanliu <f339339@gmail.com>
2024-04-13 16:21:38 +0800

    FE add auth button to recommend view

--- springSpotifyPlayList/frontend/spotify-playlist-ui/src/views/Recommendation/GetRecommendation.vue
+++ springSpotifyPlayList/frontend/spotify-playlist-ui/src/views/Recommendation/GetRecommendation.vue
@@ -53,6 +53,11 @@
           required
         />
       </div>
+
+      <button v-if="!authorized" @click="authorize">
+        Authorize with Spotify
+      </button>
+
       <button type="submit" class="btn btn-primary">Submit</button>
 
       <button class="btn btn-success" @click="addToPlaylist()">
@@ -88,6 +93,7 @@
 export default {
   data() {
     return {
+      authorized: false,
       amount: 10,
       market: "JP",
       maxPopularity: 100,
@@ -101,6 +107,23 @@ export default {
     };
   },
   methods: {
+    async authorize() {
+      try {
+        const response = await fetch("http://localhost:8888/authorize");
+        if (!response.ok) {
+          throw new Error("Failed to authorize with Spotify");
+        }
+        const data = await response.json();
+        if (data.url) {
+          window.location.href = data.url; // Redirect to the Spotify authorization page
+        } else {
+          throw new Error("Redirect URI not found in response");
+        }
+      } catch (error) {
+        console.error(error);
+        // Handle error
+      }
+    },
     async getRecommend() {
       try {
         const response = await fetch("http://localhost:8888/recommend/", {
@@ -150,7 +173,7 @@ export default {
           },
           body: JSON.stringify({
             songUris: this.trackURIs.toString(), //"xxx", //this.trackURIs,
-            playlistId: "yyy" // this.playlistId,
+            playlistId: "yyy", // this.playlistId,
           }),
         });
         if (!response.ok) {
