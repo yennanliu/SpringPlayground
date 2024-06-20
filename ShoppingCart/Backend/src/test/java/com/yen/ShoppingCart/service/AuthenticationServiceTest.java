4496ad6059914c7481740484345b556d495f844e
yennanliu <f339339@gmail.com>
2024-06-20 17:23:43 +0800

    add test for AuthService

--- ShoppingCart/Backend/src/test/java/com/yen/ShoppingCart/service/AuthenticationServiceTest.java
+++ ShoppingCart/Backend/src/test/java/com/yen/ShoppingCart/service/AuthenticationServiceTest.java
@@ -1,5 +1,6 @@
 package com.yen.ShoppingCart.service;
 
+import com.yen.ShoppingCart.config.MessageStrings;
 import com.yen.ShoppingCart.enums.Role;
 import com.yen.ShoppingCart.exception.AuthenticationFailException;
 import com.yen.ShoppingCart.model.AuthenticationToken;
@@ -17,6 +18,7 @@ import org.mockito.quality.Strictness;
 
 import static org.assertj.core.util.DateUtil.now;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -28,11 +30,13 @@ class AuthenticationServiceTest {
     @InjectMocks
     AuthenticationService authenticationService;
 
-    User user1;
+    private User user1;
 
-    User user2;
+    private User user2;
 
-    AuthenticationToken token1;
+    private AuthenticationToken token1;
+
+    private AuthenticationToken token;
 
     @BeforeEach
     public void setUp(){
@@ -43,13 +47,84 @@ class AuthenticationServiceTest {
         token1 = new AuthenticationToken(1, "my_token", now(), user1);
 
         user2 = new User();
+
+        token = new AuthenticationToken();
+        token.setToken("valid-token");
+        token.setUser(user1);
+    }
+
+    @Test
+    public void saveConfirmationToken() {
+
+        authenticationService.saveConfirmationToken(token);
+        verify(tokenRepository, times(1)).save(token);
+    }
+
+    @Test
+    public void getToken() {
+
+        // mock
+        when(tokenRepository.findTokenByUser(user1)).thenReturn(token);
+
+        AuthenticationToken result = authenticationService.getToken(user1);
+
+        assertNotNull(result);
+        assertEquals(token, result);
+    }
+
+    @Test
+    public void getUser() {
+
+        // mock
+        when(tokenRepository.findTokenByToken(any())).thenReturn(token);
+        User result = authenticationService.getUser("valid-token");
+
+        assertNotNull(result);
+        assertEquals(user1, result);
+    }
+
+    @Test
+    public void getUser_NotFound() {
+
+        // mock
+        when(tokenRepository.findTokenByToken("invalid-token")).thenReturn(null);
+        User result = authenticationService.getUser("invalid-token");
+        assertNull(result);
+    }
+
+    @Test
+    public void authenticate_ValidToken() {
+
+        // mock
+        when(tokenRepository.findTokenByToken("valid-token")).thenReturn(token);
+        assertDoesNotThrow(() -> authenticationService.authenticate("valid-token"));
+    }
+
+    @Test
+    public void authenticate_TokenNotPresent() {
+
+        Exception exception = assertThrows(AuthenticationFailException.class, () -> {
+            authenticationService.authenticate(null);
+        });
+        assertEquals(MessageStrings.AUTH_TOEKN_NOT_PRESENT, exception.getMessage());
+    }
+
+    @Test
+    public void authenticate_TokenNotValid() {
+
+        // mock
+        when(tokenRepository.findTokenByToken("invalid-token")).thenReturn(null);
+        Exception exception = assertThrows(AuthenticationFailException.class, () -> {
+            authenticationService.authenticate("invalid-token");
+        });
+        assertEquals(MessageStrings.AUTH_TOEKN_NOT_VALID, exception.getMessage());
     }
 
     @Test
     public void shouldGetUserFromTokenIfExist(){
 
         // mock
-        Mockito.when(tokenRepository.findTokenByToken("my_token")).thenReturn(token1);
+        when(tokenRepository.findTokenByToken("my_token")).thenReturn(token1);
 
         assertEquals(authenticationService.getUser("my_token"), user1);
         assertEquals(authenticationService.getUser("my_token").getEmail(), "email");
@@ -60,7 +135,7 @@ class AuthenticationServiceTest {
     public void shouldReturnNullFromTokenIfNotExist(){
 
         // mock
-        Mockito.when(tokenRepository.findTokenByToken("some_token")).thenReturn(null);
+        when(tokenRepository.findTokenByToken("some_token")).thenReturn(null);
 
         assertEquals(authenticationService.getUser("some_token"), null);
     }
@@ -69,7 +144,7 @@ class AuthenticationServiceTest {
     public void shouldThrow_AUTH_TOEKN_NOT_PRESENT_WhenAuthenticateIfNullToken(){
 
         // mock
-        Mockito.when(tokenRepository.findTokenByToken(null)).thenReturn(null);
+        when(tokenRepository.findTokenByToken(null)).thenReturn(null);
 
         Exception exception = assertThrows(AuthenticationFailException.class, () -> {
             authenticationService.authenticate(null);
@@ -82,7 +157,7 @@ class AuthenticationServiceTest {
     public void shouldThrow_AUTH_TOEKN_NOT_PRESENT_WhenAuthenticateIfNotValidToken(){
 
         // mock
-        Mockito.when(tokenRepository.findTokenByToken("some_not_valid_token")).thenReturn(null);
+        when(tokenRepository.findTokenByToken("some_not_valid_token")).thenReturn(null);
 
         Exception exception = assertThrows(AuthenticationFailException.class, () -> {
             authenticationService.authenticate("some_not_valid_token");
@@ -95,7 +170,7 @@ class AuthenticationServiceTest {
     public void shouldReturnNotThrowExceptionIfAuthWithValidToken(){
 
         // mock
-        Mockito.when(tokenRepository.findTokenByToken("my_token")).thenReturn(token1);
+        when(tokenRepository.findTokenByToken("my_token")).thenReturn(token1);
 
         // test Exception is NOT thrown
         // https://www.w3docs.com/snippets/java/how-to-test-that-no-exception-is-thrown.html
