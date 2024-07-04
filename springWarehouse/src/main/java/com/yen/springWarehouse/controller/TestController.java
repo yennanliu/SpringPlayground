2916cc607342a530a16d64dfc352c28d55a08405
yennanliu <f339339@gmail.com>
2024-07-04 19:28:03 +0800

    add controller for testService

---
+++ springWarehouse/src/main/java/com/yen/springWarehouse/controller/TestController.java
@@ -0,0 +1,29 @@
+package com.yen.springWarehouse.controller;
+
+import com.yen.springWarehouse.service.TestService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
+
+@Controller
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+    @Autowired
+    TestService testService;
+
+    @GetMapping("/getLock")
+    @ResponseBody
+    public String getValueAndLock() throws Exception {
+
+        System.out.println(testService.getValue("some param"));
+        return "getValueAndLock done";
+    }
+
+}
