14b22adf478652eb74dc06df22501dadb2ab767b
yennanliu <f339339@gmail.com>
2024-07-06 15:26:22 +0800

    testService add more getValue methods

--- springWarehouse/src/main/java/com/yen/springWarehouse/service/TestService.java
+++ springWarehouse/src/main/java/com/yen/springWarehouse/service/TestService.java
@@ -1,6 +1,9 @@
 package com.yen.springWarehouse.service;
 
 import com.yen.MyRLock.annotation.MyRLock;
+import com.yen.MyRLock.annotation.MyRLockKey;
+import com.yen.springWarehouse.bean.Product;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.stereotype.Service;
 
 import static com.yen.MyRLock.model.LockTimeoutStrategy.NO_OPERATION;
@@ -17,9 +20,25 @@ public class TestService {
     public String getValue(String param) throws Exception {
         System.out.println("param = " + param);
         if ("sleep".equals(param)) {//线程休眠或者断点阻塞，达到一直占用锁的测试效果
-            System.out.println("sleep 5 sec ...");
+            System.out.println("(getValue) sleep 5 sec ...");
             Thread.sleep(1000 * 5);
         }
+        return "success";
+    }
+
+    @MyRLock(keys = {"#userId"})
+    public String getValue(String userId, @MyRLockKey int id) throws Exception {
+        System.out.println("userId = " + userId + ", id = " + id);
+        System.out.println("(getValue with userId, id) sleep 5 sec ...");
+        Thread.sleep(1000 * 5);
+        return "success";
+    }
+
+    @MyRLock(keys = {"#user.name", "user.id"})
+    public String getValue(Product product) throws Exception {
+        System.out.println("product = " + product);
+        System.out.println("(getValue product) sleep 5 sec ...");
+        Thread.sleep(1000 * 5);
         return "success";
     }
 
