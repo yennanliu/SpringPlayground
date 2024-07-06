c38a4ecc38bb2f0aa622a283ee8993c78095fd2c
yennanliu <f339339@gmail.com>
2024-07-06 15:14:23 +0800

    update test service lock annotation

--- springWarehouse/src/main/java/com/yen/springWarehouse/service/TestService.java
+++ springWarehouse/src/main/java/com/yen/springWarehouse/service/TestService.java
@@ -10,8 +10,10 @@ import static com.yen.MyRLock.model.ReleaseTimeoutStrategy.FAIL_FAST;
 @Service
 public class TestService {
 
-    @MyRLock(name = "MyRLock-1", lockType = Reentrant, waitTime = Long.MAX_VALUE, leaseTime = 1000L, keys = {}, lockTimeoutStrategy = NO_OPERATION, customLockTimeoutStrategy = "", releaseTimeoutStrategy = FAIL_FAST, customReleaseTimeoutStrategy = "")
+    //@MyRLock(name = "MyRLock-1", lockType = Reentrant, waitTime = Long.MAX_VALUE, leaseTime = 1000L, keys = {}, lockTimeoutStrategy = NO_OPERATION, customLockTimeoutStrategy = "", releaseTimeoutStrategy = FAIL_FAST, customReleaseTimeoutStrategy = "")
     //@MyRLock(keys = {"#userId"})
+    //@MyRLock()
+    @MyRLock(waitTime = Long.MAX_VALUE)
     public String getValue(String param) throws Exception {
         System.out.println("param = " + param);
         if ("sleep".equals(param)) {//线程休眠或者断点阻塞，达到一直占用锁的测试效果
