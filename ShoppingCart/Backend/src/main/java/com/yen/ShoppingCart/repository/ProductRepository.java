076045997573aeee48902cd975699b9d23b4487d
yennanliu <f339339@gmail.com>
2024-07-23 16:02:42 +0800

    fix jpa repo custom sql, log filter product

--- ShoppingCart/Backend/src/main/java/com/yen/ShoppingCart/repository/ProductRepository.java
+++ ShoppingCart/Backend/src/main/java/com/yen/ShoppingCart/repository/ProductRepository.java
@@ -13,8 +13,8 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     List<Product> findByNameContainingIgnoreCase(@Param("name") String name);
 
-    // TODO : fix below
+    // custom SQL with JpaRepository
     // https://github.com/yennanliu/til?tab=readme-ov-file#20240215
-    //@Query(value = "SELECT * FROM Products p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))")
-    //List<Product> searchProductsByName(@Param("name") String name);
+    @Query(value= "SELECT * FROM products p WHERE LOWER(p.name) LIKE LOWER(CONCAT('%', :name, '%'))", nativeQuery = true)
+    List<Product> searchProductsByName(@Param("name") String name);
 }
