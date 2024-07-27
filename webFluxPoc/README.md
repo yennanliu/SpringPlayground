c458dc4c4768c8b6690c04d540057c35e5283db2
yennanliu <f339339@gmail.com>
2024-07-27 15:17:12 +0800

    add api ref

--- webFluxPoc/README.md
+++ webFluxPoc/README.md
@@ -2,5 +2,11 @@
 
 ## End points
 
+- http://localhost:8888/books (GET)
+- http://localhost:8888/books/{id} (GET)
+  - http://localhost:8888/books/1
+
+- http://localhost:8888/books (POST)
+
 ## Ref
 - https://juejin.cn/post/7129076913951211557
--- webFluxPoc/src/main/java/com/yen/webFluxPoc/webFluxPoc/WebFluxPocApplication.java
+++ webFluxPoc/src/main/java/com/yen/webFluxPoc/webFluxPoc/WebFluxPocApplication.java
@@ -4,8 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.reactive.config.EnableWebFlux;
 
-@SpringBootApplication
+
 @EnableWebFlux
+@SpringBootApplication
 public class WebFluxPocApplication {
 
 	public static void main(String[] args) {
