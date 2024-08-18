e8cb43fa064f72e6b0deeed2b1bf4a8f029ca539
yennanliu <f339339@gmail.com>
2024-08-18 16:33:52 +0800

    add code

--- webFluxPoc/src/main/java/com/yen/webFluxPoc/WebFluxPocApplication.java
+++ webFluxPoc/src/main/java/com/yen/webFluxPoc/WebFluxPocApplication.java
@@ -2,7 +2,15 @@ package com.yen.webFluxPoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
 import org.springframework.web.reactive.config.EnableWebFlux;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.server.HttpServer;
+
+// https://youtu.be/K1etTHjgJSQ?si=rwDnmu4gNeFJYX_3&t=143
 
 @EnableWebFlux
 @SpringBootApplication
@@ -12,5 +20,26 @@ public class WebFluxPocApplication {
 
     System.out.println("Web flux app start");
     SpringApplication.run(WebFluxPocApplication.class, args);
+
+    /** run webflux server from scratch
+     *
+     *  https://youtu.be/K1etTHjgJSQ?si=pKNDRWrklvcVAyI7&t=306
+     */
+    // TODO : fix below
+//    HttpHandler httpHandler = (ServerHttpRequest request, ServerHttpResponse response) -> {
+//      return Mono.empty();
+//    };
+//
+//    ReactorHttpHandlerAdapter adapter = new ReactorHttpHandlerAdapter(httpHandler);
+//
+//    // start Netty server
+//    HttpServer
+//            .create()
+//            .host("localhost")
+//            .port(7777)
+//            .handle(adapter)
+//            .bindNow();
+
   }
+
 }
---
+++ webFluxPoc/src/main/java/com/yen/webFluxPoc/handler/MyHttpHandler.java
@@ -0,0 +1,26 @@
+//package com.yen.webFluxPoc.handler;
+//
+//// https://youtu.be/K1etTHjgJSQ?si=n-l8MI3rI6UmNwC8&t=212
+//
+//import org.springframework.http.server.ServerHttpRequest;
+//import org.springframework.http.server.ServerHttpResponse;
+//import org.springframework.http.server.reactive.HttpHandler;
+//import reactor.core.publisher.Mono;
+//
+//public class MyHttpHandler {
+//
+//    // attr
+//    private HttpHandler httpHandler;
+//
+//    // constructor
+//    MyHttpHandler(){
+//
+//    }
+//
+//    // method
+//    public Mono<Object> run(ServerHttpRequest request){
+//        //return Mono.just(1,2,3);
+//        return Mono.empty();
+//    }
+//
+//}
