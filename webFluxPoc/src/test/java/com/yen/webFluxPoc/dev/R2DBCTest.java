ce83ce4f26900a4e5557f4acaca1ed68ec2d030c
yennanliu <f339339@gmail.com>
2024-08-21 21:43:04 +0800

    enable mysql dep, update r2dbc test

--- webFluxPoc/src/test/java/com/yen/webFluxPoc/dev/R2DBCTest.java
+++ webFluxPoc/src/test/java/com/yen/webFluxPoc/dev/R2DBCTest.java
@@ -1,50 +1,60 @@
-//package com.yen.webFluxPoc.dev;
-//
-//// https://youtu.be/anguDoWURus?si=soqM6UrLcqB3td_i&t=580
-//
-//import com.yen.webFluxPoc.model.Author;
-//import io.r2dbc.spi.ConnectionFactories;
-//import io.r2dbc.spi.ConnectionFactory;
-//import org.junit.jupiter.api.Test;
-//import reactor.core.publisher.Flux;
-//import reactor.core.publisher.Mono;
-//
-//public class R2DBCTest {
-//
-//  @Test
-//  public void test1() {
-//
-//    /**  r2dbc is a async DB connection library 2) CAN NOT raise concurrent amount */
-//
-//    // Step 1) mysql connection factory
-//    final String mysqlUrl = "r2dbc:mysql://localhost:3306/test";
-//    ConnectionFactory connectionFactory = ConnectionFactories.get(mysqlUrl);
-//
-//    // Step 2) get connection, send SQL
-//
-//    // Step 3) publish received data
-//    // https://youtu.be/anguDoWURus?si=pyqt50AvCmoezet6&t=1327
-//    Mono.from(connectionFactory.create())
-//        .flatMapMany(
-//            connection ->
-//                Flux.from(
-//                    connection
-//                        .createStatement("SELECT * FROM author WHERE id = ?")
-//                        .bind(0, 2L)
-//                        .execute()))
-//        .map(
-//            result -> {
-//              return Mono.from(
-//                  result.map(
-//                      readable -> {
-//                        Long id = readable.get("id", Long.class);
-//                        String name = readable.get("name", String.class);
-//                        return new Author(id, name);
-//                      }));
-//            })
-//        .subscribe(
-//            authorMono -> {
-//              System.out.println(authorMono);
-//            });
-//  }
-//}
+package com.yen.webFluxPoc.dev;
+
+// https://youtu.be/anguDoWURus?si=soqM6UrLcqB3td_i&t=580
+
+import com.yen.webFluxPoc.model.Author;
+import io.asyncer.r2dbc.mysql.MySqlConnectionConfiguration;
+import io.asyncer.r2dbc.mysql.MySqlConnectionFactory;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class R2DBCTest {
+
+  @Test
+  public void test1() {
+
+    /** r2dbc is a async DB connection library 2) CAN NOT raise concurrent amount */
+
+    // Step 0) mysql setting
+    final String mysqlUrl = "r2dbc:mysql://localhost:3306/test";
+    // ConnectionFactory connectionFactory = ConnectionFactories.get(mysqlUrl);
+    // https://youtu.be/anguDoWURus?si=kKvC_BpiWANXqdtT&t=2323
+    MySqlConnectionConfiguration conf =
+        MySqlConnectionConfiguration.builder()
+            .username("root")
+            .host("localhost")
+            .database("test")
+            .build();
+
+    // Step 1) mysql connection factory
+    MySqlConnectionFactory connectionFactory = MySqlConnectionFactory.from(conf);
+
+    // Step 2) get connection, send SQL
+
+    // Step 3) publish received data
+    // https://youtu.be/anguDoWURus?si=pyqt50AvCmoezet6&t=1327
+    Mono.from(connectionFactory.create())
+        .flatMapMany(
+            connection ->
+                Flux.from(
+                    connection
+                        .createStatement("SELECT * FROM author WHERE id = ?")
+                        .bind(0, 2L)
+                        .execute()))
+        .map(
+            result -> {
+              return Mono.from(
+                  result.map(
+                      readable -> {
+                        Long id = readable.get("id", Long.class);
+                        String name = readable.get("name", String.class);
+                        return new Author(id, name);
+                      }));
+            })
+        .subscribe(
+            authorMono -> {
+              System.out.println(authorMono);
+            });
+  }
+}
