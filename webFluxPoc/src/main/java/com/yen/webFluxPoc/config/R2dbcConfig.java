7991d1396838ec6dce4781f93f874fb17986e735
yennanliu <f339339@gmail.com>
2024-08-25 15:31:04 +0800

    add code comment, clean code

--- webFluxPoc/src/main/java/com/yen/webFluxPoc/config/R2dbcConfig.java
+++ webFluxPoc/src/main/java/com/yen/webFluxPoc/config/R2dbcConfig.java
@@ -5,6 +5,7 @@ package com.yen.webFluxPoc.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
+// TODO : check how it works / if is necessary
 @EnableR2dbcRepositories // enable r2dbc repo : similar as JPA
 @Configuration
 public class R2dbcConfig {}
