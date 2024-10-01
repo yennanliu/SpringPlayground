ff552fe9bfbd31778192fc8ab097a94a4dbdbf6a
yennanliu <f339339@gmail.com>
2024-10-01 15:26:23 +0800

    UI CreatePlayList populate error if create list failed

--- springSpotifyPlayList/frontend/spotify-playlist-ui/src/views/PlayList/CreatePlayList.vue
+++ springSpotifyPlayList/frontend/spotify-playlist-ui/src/views/PlayList/CreatePlayList.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <h1>Create New PlayList</h1>
-    <!-- <button @click="authorize">
-      Authorize with Spotify
-    </button> -->
+    <h1>Create New Playlist</h1>
     <div>
       <div>
         <label for="playlistName">New Playlist Name:</label>
@@ -14,19 +11,13 @@
           placeholder="Enter playlist name"
         />
         <button @click="createPlaylist">Create Playlist</button>
+        <!-- Success message -->
         <div v-if="playlistCreated">Playlist created successfully!</div>
+        <!-- Error message -->
+        <div v-if="playlistCreationError" style="color: red;">
+          {{ playlistCreationError }}
+        </div>
       </div>
-      <!-- <div>
-        <label for="playlistId">Playlist ID:</label>
-        <input
-          type="text"
-          id="playlistId"
-          v-model="newSongToList.playlistId"
-          placeholder="Enter playlist ID"
-        />
-        <button @click="addSongToPlayList">Add Song To Playlist</button>
-        <div v-if="newSongsAdded">Song added to Playlist successfully!</div>
-      </div> -->
     </div>
   </div>
 </template>
@@ -37,108 +28,36 @@ import axios from "axios";
 export default {
   data() {
     return {
-      //authorized: false,
       playlistCreated: false,
-      newSongsAdded: false,
-      accessToken: null,
-      newPlayList: { userId: "someId", name: "", authCode: "code" },
-      newSongToList: {
-        playlistId: "",
-        trackURIs: "",
-        authCode: "code",
-      },
+      playlistCreationError: null, // To track errors
+      newPlayList: { userId: "someId", name: "", authCode: "code" }
     };
   },
-  _methods: {
-    // async authorize() {
-    //   try {
-    //     const response=await fetch("http://localhost:8888/authorize");
-    //     if(!response.ok) {
-    //       throw new Error("Failed to authorize with Spotify");
-    //     }
-    //     const data=await response.json();
-    //     if(data.url) {
-    //       window.location.href=data.url; // Redirect to the Spotify authorization page
-    //     } else {
-    //       throw new Error("Redirect URI not found in response");
-    //     }
-    //   } catch(error) {
-    //     console.error(error);
-    //     // Handle error
-    //   }
-    // },
-
+  methods: {
     async createPlaylist() {
       try {
         console.log("createPlaylist start");
-        const urlParams=new URLSearchParams(window.location.search);
-        const code=urlParams.get("code");
-        if(!code) {
+        const urlParams = new URLSearchParams(window.location.search);
+        const code = urlParams.get("code");
+        if (!code) {
           throw new Error("Authorization code not found");
         }
-        this.newPlayList.authCode=code;
-        const response=await axios.post(
+        this.newPlayList.authCode = code;
+        const response = await axios.post(
           "http://localhost:8888/playlist/create",
           this.newPlayList
         );
-        if(response.status===200) {
-          this.playlistCreated=true;
+        if (response.status === 200) {
+          this.playlistCreated = true;
+          this.playlistCreationError = null; // Clear error if successful
         } else {
           throw new Error("Failed to create playlist");
         }
-      } catch(error) {
+      } catch (error) {
         console.error(error);
-        // Handle error
+        this.playlistCreationError = error.message; // Set error message
       }
-    },
-
-    // async addSongToPlayList() {
-    //   try {
-    //     console.log("addSongToPlayList start");
-    //     const urlParams=new URLSearchParams(window.location.search);
-    //     const code=urlParams.get("code");
-    //     if(!code) {
-    //       throw new Error("Authorization code not found");
-    //     }
-    //     this.newSongToList.authCode=code;
-    //     // TODO : instead of hardcode trace uris, get them from UI
-    //     this.newSongToList.trackURIs=
-    //       "spotify:track:1wHrcMSpzIbNk4CipbKft0,spotify:track:69TMjHuBaLNRtMPopKWbdC,spotify:track:2Ugyo7kjFFli9gHf9KfK5A,spotify:track:3xsFZOyd6mfrjZT1Sf4nXR,spotify:track:6gWRznlX7vaUW0r8KF9iMZ,spotify:track:4kuKGST6Pj4iMZBpO6BYl4,spotify:track:1dO38CsQliftngGVX2NwI2,spotify:track:4KU4UDuuZEjiFGP01OkF9H";
-
-    //     const response=await fetch("http://localhost:8888/playlist/addSong", {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //       },
-    //       body: JSON.stringify({
-    //         songUris: this.newSongToList.trackURIs,
-    //         authCode: code,
-    //         playlistId: this.newSongToList.playlistId,
-    //       }),
-    //     });
-    //     if(response.status===200) {
-    //       this.newSongsAdded=true;
-    //     } else {
-    //       throw new Error("Failed to add song to playlist");
-    //     }
-    //   } catch(error) {
-    //     console.error(error);
-    //     // Handle error
-    //   }
-    // },
-  },
-  get methods() {
-    return this._methods;
-  },
-  set methods(value) {
-    this._methods=value;
-  },
-  mounted() {
-    //const urlParams = new URLSearchParams(window.location.search);
-    // const code = urlParams.get("code");
-    // if (code) {
-    //   this.authorized = true;
-    // }
-  },
+    }
+  }
 };
 </script>
