f9d28929cd651f8c12210af166b79a1cfa8e27a8
yennanliu <f339339@gmail.com>
2024-10-06 14:08:19 +0800

    implement getRecommendationWithPlayList, move nb, update readme

--- springSpotifyPlayList/backend/SpotifyPlayList/src/main/java/com/yen/SpotifyPlayList/service/RecommendationsService.java
+++ springSpotifyPlayList/backend/SpotifyPlayList/src/main/java/com/yen/SpotifyPlayList/service/RecommendationsService.java
@@ -7,10 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import se.michaelthelin.spotify.SpotifyApi;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
+import se.michaelthelin.spotify.model_objects.specification.AudioFeatures;
 import se.michaelthelin.spotify.model_objects.specification.Recommendations;
 import se.michaelthelin.spotify.requests.data.browse.GetRecommendationsRequest;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -18,6 +21,9 @@ public class RecommendationsService {
 
     @Autowired
     private AuthService authService;
+
+    @Autowired
+    private PlayListService playListService;
 
     private SpotifyApi spotifyApi;
 
@@ -41,17 +47,35 @@ public class RecommendationsService {
 
     public Recommendations getRecommendationWithPlayList(String playListId) throws SpotifyWebApiException {
 
-//        Recommendations recommendations = null;
-//        try {
-//            this.spotifyApi = authService.initializeSpotifyApi();
-//            GetRecommendationsRequest getRecommendationsRequest = prepareRecommendationsRequest(getRecommendationsDto);
-//            recommendations = getRecommendationsRequest.execute();
-//            log.info("recommendations = " + recommendations);
-//        } catch (IOException | SpotifyWebApiException | ParseException e) {
-//            throw new SpotifyWebApiException("getRecommendation error: " + e.getMessage());
-//        }
-//        return recommendations;
-        return null;
+        GetRecommendationsDto getRecommendationsDto = new GetRecommendationsDto();
+        Recommendations recommendations = null;
+
+        try{
+            List<AudioFeatures> audioFeaturesList = playListService.getSongFeatureByPlayList(playListId);
+            // TODO : update `GetRecommendationsRequest` :  make seedAlbum, seedArtist not required
+            // or, implement the API call by ourself : https://developer.spotify.com/documentation/web-api/reference/get-recommendations
+            // https://api.spotify.com/v1/recommendations
+
+            // get avg value of features
+
+            // TODO : use functional way
+            Float energySum = null;
+            Float acousticness = null;
+            Float danceability = null;
+
+            for (AudioFeatures audioFeatures : audioFeaturesList){
+                energySum += audioFeatures.getEnergy();
+                acousticness += audioFeatures.getAcousticness();
+                danceability += audioFeatures.getDanceability();
+                // TODO : implement others
+            }
+
+            GetRecommendationsRequest recommendationsRequest =  spotifyApi.getRecommendations().build();
+            return recommendationsRequest.execute();
+        }catch (Exception e){
+
+        }
+        return recommendations;
     }
     private GetRecommendationsRequest prepareRecommendationsRequest(GetRecommendationsDto getRecommendationsDto){
         return  spotifyApi.getRecommendations()
