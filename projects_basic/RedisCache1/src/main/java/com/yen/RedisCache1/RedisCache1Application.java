090ec4173beb8c938117d6ff2d73d463b4a24f41
yennanliu <f339339@gmail.com>
2024-12-08 08:18:29 +0800

    add redis-cache-1 project

---
+++ projects_basic/RedisCache1/src/main/java/com/yen/RedisCache1/RedisCache1Application.java
@@ -0,0 +1,14 @@
+package com.yen.RedisCache1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisCache1Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(RedisCache1Application.class, args);
+	}
+
+}
---
+++ projects_basic/RedisCache1/src/test/java/com/yen/RedisCache1/RedisCache1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.RedisCache1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisCache1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
