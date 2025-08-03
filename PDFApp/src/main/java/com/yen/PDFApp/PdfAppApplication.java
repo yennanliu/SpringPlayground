f58689a0865fb39efcb3da942dc98fe67ef6a959
yennanliu <f339339@gmail.com>
2025-08-03 15:13:07 +0800

    add pdf app

---
+++ PDFApp/src/main/java/com/yen/PDFApp/PdfAppApplication.java
@@ -0,0 +1,14 @@
+package com.yen.PDFApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PdfAppApplication.class, args);
+	}
+
+}
---
+++ PDFApp/src/test/java/com/yen/PDFApp/PdfAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.PDFApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
