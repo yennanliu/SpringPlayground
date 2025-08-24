c793ac09df0180242c0d4f9613b445b5c9fc627e
yennanliu <f339339@gmail.com>
2025-08-24 17:02:29 +0800

    add hotel app

---
+++ HotelApp/src/main/java/com/yen/HotelApp/HotelAppApplication.java
@@ -0,0 +1,14 @@
+package com.yen.HotelApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelAppApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HotelAppApplication.class, args);
+	}
+
+}
---
+++ HotelApp/src/test/java/com/yen/HotelApp/HotelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.HotelApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
