728a5cb6d2a226313b543ce120235e976c1da000
jim <jim@uber.com>
2026-03-19 14:23:44 +0800

    add clusterAdmin

---
+++ clusterAdmin/src/main/java/com/yen/clusterAdmin/ClusterAdminApplication.java
@@ -0,0 +1,13 @@
+package com.yen.clusterAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClusterAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClusterAdminApplication.class, args);
+	}
+
+}
---
+++ clusterAdmin/src/test/java/com/yen/clusterAdmin/ClusterAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.clusterAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusterAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
