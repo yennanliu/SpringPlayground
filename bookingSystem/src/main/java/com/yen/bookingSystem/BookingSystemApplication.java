81d8c63fbaf1c271d0db20b79e5afd559c0536d9
jim <jim@uber.com>
2026-03-26 13:42:20 +0800

    add booking system

---
+++ bookingSystem/src/main/java/com/yen/bookingSystem/BookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.yen.bookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSystemApplication.class, args);
+	}
+
+}
---
+++ bookingSystem/src/test/java/com/yen/bookingSystem/BookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yen.bookingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
